6b0b83fe963f40fc3ba9392c44ea24827a9fdb26
Menny Even Danan <menny@evendanan.net>
2009-09-16 15:09:54 +0000

    GUI fixes

--- src/com/menny/android/anysoftkeyboard/MainForm.java
+++ src/com/menny/android/anysoftkeyboard/MainForm.java
@@ -1,13 +1,14 @@
 package com.menny.android.anysoftkeyboard;
 
-import android.app.Activity;
+import android.app.TabActivity;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TabHost;
 import android.widget.TextView;
 
-public class MainForm extends Activity {
+public class MainForm extends TabActivity {
 
 	@Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,5 +24,13 @@ public class MainForm extends Activity {
 		
 		TextView label = (TextView)super.findViewById(R.id.main_title_version);
 		label.setText(label.getText()+version);
+		
+		TabHost mTabHost = getTabHost();
+	    
+	    mTabHost.addTab(mTabHost.newTabSpec("tab_test1").setIndicator("Welcome").setContent(R.id.main_tab1));
+	    mTabHost.addTab(mTabHost.newTabSpec("tab_test2").setIndicator("Links").setContent(R.id.main_tab2));
+	    mTabHost.addTab(mTabHost.newTabSpec("tab_test3").setIndicator("Credits").setContent(R.id.main_tab3));
+	    
+	    mTabHost.setCurrentTab(0);
     }
 }
