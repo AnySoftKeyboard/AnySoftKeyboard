b72ca195d36de8f7a07d76e68fa36ae5d555b896
Menny Even Danan <menny@evendanan.net>
2010-05-03 12:19:06 +0000

    Even less code!

--- src/com/menny/android/anysoftkeyboard/keyboards/GenericKeyboard.java
+++ src/com/menny/android/anysoftkeyboard/keyboards/GenericKeyboard.java
@@ -2,10 +2,35 @@ package com.menny.android.anysoftkeyboard.keyboards;
 
 import com.menny.android.anysoftkeyboard.AnyKeyboardContextProvider;
 
-public class GenericKeyboard extends InternalAnyKeyboard 
+public class GenericKeyboard extends AnyKeyboard 
 {
-	public GenericKeyboard(AnyKeyboardContextProvider context, int xmlLayoutResId) 
+	private final int mNameResId;
+	private final String mPrefId;
+	
+	public GenericKeyboard(AnyKeyboardContextProvider context, int xmlLayoutResId, int nameResId, String prefKeyId) 
 	{
-		super(context, xmlLayoutResId);
+		super(context, context.getApplicationContext(), xmlLayoutResId);
+		mNameResId = nameResId;
+		mPrefId = prefKeyId;
+	}
+
+	@Override
+	public String getDefaultDictionaryLocale() {
+		return null;
+	}
+
+	@Override
+	public int getKeyboardIconResId() {
+		return -1;
+	}
+
+	@Override
+	protected int getKeyboardNameResId() {
+		return mNameResId;
+	}
+
+	@Override
+	public String getKeyboardPrefId() {
+		return mPrefId;
 	}
 }
