9c138042cb34446a19b9bf171e0f758551f15e44
Menny Even Danan <menny@evendanan.net>
2010-05-23 09:25:03 +0000

    Issue 386: Reset dictionary and keyboard only if the package in question implement ASK dictionary/keyboard interface.

--- src/com/menny/android/anysoftkeyboard/PackagesChangedReceiver.java
+++ src/com/menny/android/anysoftkeyboard/PackagesChangedReceiver.java
@@ -1,8 +1,8 @@
 package com.menny.android.anysoftkeyboard;
 
 import com.menny.android.anysoftkeyboard.dictionary.DictionaryFactory;
+import com.menny.android.anysoftkeyboard.dictionary.ExternalDictionaryFactory;
 import com.menny.android.anysoftkeyboard.keyboards.KeyboardBuildersFactory;
-import com.menny.android.anysoftkeyboard.keyboards.KeyboardFactory;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -15,20 +15,33 @@ public class PackagesChangedReceiver extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
-			Log.i(TAG, "Rebuilding Keyboards since packages have been changed.");
+		if(intent == null || intent.getData() == null || context == null)
+			return;
+
+
+		boolean isDictionary = (context.getPackageManager().queryBroadcastReceivers(
+				new Intent(ExternalDictionaryFactory.DictionaryBuilder.RECEIVER_INTERFACE), 0).size() > 0);
+		boolean isKeyboard = (context.getPackageManager().queryBroadcastReceivers(
+				new Intent(KeyboardBuildersFactory.KeyboardBuilder.RECEIVER_INTERFACE), 0).size() > 0);
+
+		if(isKeyboard){
+			Log.i(TAG, "Rebuilding Keyboards since (keyboard) packages have been changed.");
 			KeyboardSwitcher keyboardSwitcher = KeyboardSwitcher.getInstance();
 			if(keyboardSwitcher != null){
 				KeyboardBuildersFactory.resetBuildersCache();
 				KeyboardSwitcher.getInstance().makeKeyboards(true);
 			}
+		}
 
-			Log.i(TAG, "Refreshing dictionaries since packages have been changed.");
+		if(isDictionary){
+			Log.i(TAG, "Refreshing dictionaries since (dictionary) packages have been changed.");
 
 			DictionaryFactory.releaseAllDictionaries();
 			AnySoftKeyboard softKeyboard = AnySoftKeyboard.getInstance();
 			if(softKeyboard != null) {
 				softKeyboard.setMainDictionaryForCurrentKeyboard();
 			}
+		}
 	}
 
 }
