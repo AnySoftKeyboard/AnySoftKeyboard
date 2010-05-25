7a3981d307f987918fadc9b848c1330e54ebacb4
Menny Even Danan <menny@evendanan.net>
2010-05-25 09:34:06 +0000

    Issue 386: PackageChangedReceiver: intent was targeted for all the packages which is incorrect!
    See issue for new problems.

--- src/com/menny/android/anysoftkeyboard/PackagesChangedReceiver.java
+++ src/com/menny/android/anysoftkeyboard/PackagesChangedReceiver.java
@@ -5,8 +5,10 @@ import com.menny.android.anysoftkeyboard.dictionary.ExternalDictionaryFactory;
 import com.menny.android.anysoftkeyboard.keyboards.KeyboardBuildersFactory;
 
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.util.Log;
 
 public class PackagesChangedReceiver extends BroadcastReceiver {
@@ -15,33 +17,62 @@ public class PackagesChangedReceiver extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		if(intent == null || intent.getData() == null || context == null)
+		if (intent == null || intent.getData() == null || context == null)
 			return;
 
+		if (intent.getAction().equals(Intent.ACTION_PACKAGE_REMOVED)
+				&& intent.getExtras().getBoolean(Intent.EXTRA_REPLACING)) {
+			// Event should be followed by a install so we do nothing at
+			// this point
+			Log.d(TAG, "Replacing app; we wait for ADD intent");
+			return;
+		}
 
-		boolean isDictionary = (context.getPackageManager().queryBroadcastReceivers(
-				new Intent(ExternalDictionaryFactory.DictionaryBuilder.RECEIVER_INTERFACE), 0).size() > 0);
-		boolean isKeyboard = (context.getPackageManager().queryBroadcastReceivers(
-				new Intent(KeyboardBuildersFactory.KeyboardBuilder.RECEIVER_INTERFACE), 0).size() > 0);
+		Intent dictionaryIntent = new Intent(
+				ExternalDictionaryFactory.DictionaryBuilder.RECEIVER_INTERFACE);
+		Intent keyboardIntent = new Intent(
+				KeyboardBuildersFactory.KeyboardBuilder.RECEIVER_INTERFACE);
 
-		if(isKeyboard){
-			Log.i(TAG, "Rebuilding Keyboards since (keyboard) packages have been changed.");
+		dictionaryIntent.setPackage(intent.getPackage());
+		keyboardIntent.setPackage(intent.getPackage());
+		boolean isDictionary = (context.getPackageManager()
+				.queryBroadcastReceivers(dictionaryIntent,
+						PackageManager.GET_RECEIVERS).size() > 0);
+		boolean isKeyboard = (context.getPackageManager()
+				.queryBroadcastReceivers(keyboardIntent,
+						PackageManager.GET_RECEIVERS).size() > 0);
+		isDictionary = isDictionary
+				|| context.getPackageName().equals(intent.getPackage());
+
+		isKeyboard = isKeyboard
+				|| context.getPackageName().equals(intent.getPackage());
+
+		Log.d(TAG, new StringBuffer("Packages (").append(intent.getData())
+				.append(") have been changed. Is dictionary ").append(
+						isDictionary).append(", isKeyboard ")
+				.append(isKeyboard).toString());
+
+		if (isKeyboard || isDictionary) {
+			Log
+					.i(TAG,
+							"Rebuilding Keyboards since (keyboard) packages have been changed.");
 			KeyboardSwitcher keyboardSwitcher = KeyboardSwitcher.getInstance();
-			if(keyboardSwitcher != null){
+			if (keyboardSwitcher != null) {
 				KeyboardBuildersFactory.resetBuildersCache();
 				KeyboardSwitcher.getInstance().makeKeyboards(true);
 			}
 		}
 
-		if(isDictionary){
-			Log.i(TAG, "Refreshing dictionaries since (dictionary) packages have been changed.");
+		if (isDictionary || isDictionary) {
+			Log
+					.i(TAG,
+							"Refreshing dictionaries since (dictionary) packages have been changed.");
 
 			DictionaryFactory.releaseAllDictionaries();
 			AnySoftKeyboard softKeyboard = AnySoftKeyboard.getInstance();
-			if(softKeyboard != null) {
+			if (softKeyboard != null) {
 				softKeyboard.setMainDictionaryForCurrentKeyboard();
 			}
 		}
 	}
-
 }
