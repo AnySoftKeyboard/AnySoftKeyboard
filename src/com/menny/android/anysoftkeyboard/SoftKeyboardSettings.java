356a13d1a9a22fbeade39f2da715b568e1685d8b
Menny Even Danan <menny@evendanan.net>
2010-05-25 07:11:05 +0000

    Issue 385

--- src/com/menny/android/anysoftkeyboard/SoftKeyboardSettings.java
+++ src/com/menny/android/anysoftkeyboard/SoftKeyboardSettings.java
@@ -22,7 +22,7 @@ public class SoftKeyboardSettings extends PreferenceActivity {
     protected void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         addPreferencesFromResource(R.layout.prefs);
-        
+
         String version = "";
         try {
 			final PackageInfo info = getPackageInfo(getApplicationContext());
@@ -30,44 +30,11 @@ public class SoftKeyboardSettings extends PreferenceActivity {
 		} catch (final NameNotFoundException e) {
 			Log.e("AnySoftKeyboard", "Failed to locate package information! This is very weird... I'm installed.");
 		}
-		
+
 		final Preference label = super.findPreference("prefs_title_key");
 		label.setSummary(label.getSummary()+version);
-		//first resetting maybe the user has installed a new keyboard
-		KeyboardBuildersFactory.resetBuildersCache();
-		//getting all keyboards
-		final ArrayList<KeyboardBuilder> creators = KeyboardBuildersFactory.getAllBuilders(getApplicationContext());
-		final PreferenceCategory keyboards = (PreferenceCategory)super.findPreference("prefs_keyboards_screen");
-		
-		for(final KeyboardBuilder creator : creators)
-		{
-		    final Context creatorContext = creator.getPackageContext() == null?
-		            getApplicationContext() : creator.getPackageContext();
-		            
-			if (creatorContext == getApplicationContext() && creator.getKeyboardNameResId() == R.string.eng_keyboard) {
-                continue;//english is an internal keyboard, and is on by default.
-            }
-			final CheckBoxPreference checkBox = new CheckBoxPreference(getApplicationContext());
-			/*
-			 * <CheckBoxPreference
-				android:key="eng_keyboard"
-				android:title="@string/eng_keyboard"
-				android:persistent="true" 
-				android:defaultValue="true"
-				android:summaryOn="QWERTY Latin keyboard"
-				android:summaryOff="QWERTY Latin keyboard"
-				/>
-			 */
-			checkBox.setKey(creator.getId());
-			checkBox.setTitle(creatorContext.getText(creator.getKeyboardNameResId()));
-			checkBox.setPersistent(true);
-			checkBox.setDefaultValue(false);
-			checkBox.setSummaryOn(creator.getDescription());
-			checkBox.setSummaryOff(creator.getDescription());
-			
-			keyboards.addPreference(checkBox);
-		}
-		
+
+
 		final Preference searcher = (Preference)super.findPreference("search_for_addons");
 		searcher.setOnPreferenceClickListener(new OnPreferenceClickListener() {
 			public boolean onPreferenceClick(Preference preference) {
@@ -80,6 +47,46 @@ public class SoftKeyboardSettings extends PreferenceActivity {
 			}
 		});
     }
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		//first resetting maybe the user has installed a new keyboard
+		KeyboardBuildersFactory.resetBuildersCache();
+		//getting all keyboards
+		final ArrayList<KeyboardBuilder> creators = KeyboardBuildersFactory.getAllBuilders(getApplicationContext());
+		final PreferenceCategory keyboards = (PreferenceCategory)super.findPreference("prefs_keyboards_screen");
+
+		for(final KeyboardBuilder creator : creators)
+		{
+		    final Context creatorContext = creator.getPackageContext() == null?
+		            getApplicationContext() : creator.getPackageContext();
+
+			if (creatorContext == getApplicationContext() && creator.getKeyboardNameResId() == R.string.eng_keyboard) {
+                continue;//english is an internal keyboard, and is on by default.
+            }
+			final CheckBoxPreference checkBox = new CheckBoxPreference(getApplicationContext());
+			/*
+			 * <CheckBoxPreference
+				android:key="eng_keyboard"
+				android:title="@string/eng_keyboard"
+				android:persistent="true"
+				android:defaultValue="true"
+				android:summaryOn="QWERTY Latin keyboard"
+				android:summaryOff="QWERTY Latin keyboard"
+				/>
+			 */
+			checkBox.setKey(creator.getId());
+			checkBox.setTitle(creatorContext.getText(creator.getKeyboardNameResId()));
+			checkBox.setPersistent(true);
+			checkBox.setDefaultValue(false);
+			checkBox.setSummaryOn(creator.getDescription());
+			checkBox.setSummaryOff(creator.getDescription());
+
+			keyboards.addPreference(checkBox);
+		}
+	}
 
 	public static PackageInfo getPackageInfo(Context context) throws NameNotFoundException {
 		return context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
