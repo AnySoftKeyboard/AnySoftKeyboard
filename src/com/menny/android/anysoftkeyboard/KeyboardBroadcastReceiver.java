b75dd52e07c1038e07caab301dcb9e9eb1b175e6
Lado Kumsiashvili <herrlado@gmail.com>
2011-01-10 09:25:13 +0000

    +LayoutChangedBroadcastReceiver

---
+++ src/com/menny/android/anysoftkeyboard/KeyboardBroadcastReceiver.java
@@ -0,0 +1,7 @@
+package com.menny.android.anysoftkeyboard;
+
+import com.menny.android.anysoftkeyboard.api.LayoutChangedBroadcastReceiver;
+
+public class KeyboardBroadcastReceiver extends LayoutChangedBroadcastReceiver {
+
+}
