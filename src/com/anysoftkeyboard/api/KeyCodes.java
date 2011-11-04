63c0d496e9504cced6a3019e4b3a6fc96b776da5
Menny Even Danan <menny@evendanan.net>
2011-11-04 12:58:02 +0000

    refactored the KeyCodes values field Added gesture pinch and separate (no detection just yet)

--- src/com/anysoftkeyboard/api/KeyCodes.java
+++ src/com/anysoftkeyboard/api/KeyCodes.java
@@ -4,6 +4,7 @@ public class KeyCodes {
 	
 	public static final int SPACE = 32;
 	public static final int ENTER = 10;
+	public static final int TAB = 6;
 	public static final int DELETE = -5;
 	
 	public static final int QUICK_TEXT = -10;
@@ -16,6 +17,10 @@ public class KeyCodes {
 	
 	public static final int MODE_SYMOBLS = -2;
 	public static final int MODE_ALPHABET = -99;
+	public static final int KEYBOARD_CYCLE = -97;
+	public static final int KEYBOARD_REVERSE_CYCLE = -96;
+	public static final int KEYBOARD_CYCLE_INSIDE_MODE = -95;
+	public static final int KEYBOARD_MODE_CHANGE = -94;
 	
 	public static final int ARROW_LEFT = -20;
 	public static final int ARROW_RIGHT = -21;
@@ -25,7 +30,38 @@ public class KeyCodes {
 	public static final int SETTINGS = -100;
 	public static final int CLIPBOARD = -12;
 	public static final int CANCEL = -3;
+	public static final int CLEAR_INPUT = -13; 
+	public static final int DONE = -4;
 	
 	public static final int DISABLED = 0;
 	
+	public static final int SPLIT_LAYOUT = -110;
+	public static final int MERGE_LAYOUT = -111;
+	
+	
+	
+	//public final static int KEYCODE_LANG_CHANGE = -99;
+	//public final static int KEYCODE_KEYBOARD_CYCLE = -97;
+	//public final static int KEYCODE_KEYBOARD_REVERSE_CYCLE = -96;
+	//public final static int KEYCODE_KEYBOARD_CYCLE_INSIDE_MODE = -95;
+	//public final static int KEYCODE_KEYBOARD_MODE_CHANGE = -94;
+	
+	//public final static int KEYCODE_QUICK_TEXT = -10;
+	//public final static int KEYCODE_DOMAIN = -9;
+	
+	//public static final int KEYCODE_LEFT = -20;
+	//public static final int KEYCODE_RIGHT = -21;
+	//public static final int KEYCODE_UP = -22;
+	//public static final int KEYCODE_DOWN = -23;
+	
+	//public static final int	KEYCODE_CTRL = -11;
+	
+	//public static final int KEYCODE_CLIPBOARD = -12;
+	
+	/*public static final int KEYCODE_SHIFT = -1;
+    public static final int KEYCODE_MODE_CHANGE = -2;
+    public static final int KEYCODE_CANCEL = -3;
+    public static final int KEYCODE_DONE = -4;
+    public static final int KEYCODE_DELETE = -5;
+    public static final int KEYCODE_ALT = -6;*/
 }
