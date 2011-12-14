b0434d2d999476ab31db6abd75aada4c52493ffa
Menny Even Danan <menny@evendanan.net>
2011-12-14 10:43:09 +0000

    Isseu 833

--- src/com/anysoftkeyboard/api/KeyCodes.java
+++ src/com/anysoftkeyboard/api/KeyCodes.java
@@ -39,30 +39,5 @@ public class KeyCodes {
 	public static final int SPLIT_LAYOUT = -110;
 	public static final int MERGE_LAYOUT = -111;
 	
-	
-	
-	//public final static int KEYCODE_LANG_CHANGE = -99;
-	//public final static int KEYCODE_KEYBOARD_CYCLE = -97;
-	//public final static int KEYCODE_KEYBOARD_REVERSE_CYCLE = -96;
-	//public final static int KEYCODE_KEYBOARD_CYCLE_INSIDE_MODE = -95;
-	//public final static int KEYCODE_KEYBOARD_MODE_CHANGE = -94;
-	
-	//public final static int KEYCODE_QUICK_TEXT = -10;
-	//public final static int KEYCODE_DOMAIN = -9;
-	
-	//public static final int KEYCODE_LEFT = -20;
-	//public static final int KEYCODE_RIGHT = -21;
-	//public static final int KEYCODE_UP = -22;
-	//public static final int KEYCODE_DOWN = -23;
-	
-	//public static final int	KEYCODE_CTRL = -11;
-	
-	//public static final int KEYCODE_CLIPBOARD = -12;
-	
-	/*public static final int KEYCODE_SHIFT = -1;
-    public static final int KEYCODE_MODE_CHANGE = -2;
-    public static final int KEYCODE_CANCEL = -3;
-    public static final int KEYCODE_DONE = -4;
-    public static final int KEYCODE_DELETE = -5;
-    public static final int KEYCODE_ALT = -6;*/
+	public static final int UTILITY_KEYBOARD = -120;
 }
