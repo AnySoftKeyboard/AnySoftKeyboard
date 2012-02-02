2877ac1049bbe7d5efb1dc7a24d28b571adc6d84
Menny Even Danan <menny@evendanan.net>
2012-02-02 11:12:46 +0000

    tips from the main settings page

--- src/com/anysoftkeyboard/ui/settings/MainSettings.java
+++ src/com/anysoftkeyboard/ui/settings/MainSettings.java
@@ -1,19 +1,18 @@
 package com.anysoftkeyboard.ui.settings;
 
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.preference.Preference;
-import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
 import android.util.Log;
 
-import com.anysoftkeyboard.ui.MainForm;
 import com.menny.android.anysoftkeyboard.R;
 
 public class MainSettings extends PreferenceActivity {
+
+	private static final String TAG = "ASK_PREFS";
 
 	@Override
     protected void onCreate(Bundle icicle) {
@@ -25,28 +24,11 @@ public class MainSettings extends PreferenceActivity {
 			final PackageInfo info = getPackageInfo(getApplicationContext());
 			version = info.versionName + " (release "+info.versionCode+")";
 		} catch (final NameNotFoundException e) {
-			Log.e("AnySoftKeyboard", "Failed to locate package information! This is very weird... I'm installed.");
+			Log.e(TAG, "Failed to locate package information! This is very weird... I'm installed.");
 		}
 
 		final Preference label = super.findPreference("prefs_title_key");
 		label.setSummary(label.getSummary()+version);
-		
-		final Preference helper = (Preference)super.findPreference("prefs_help_key");
-		helper.setOnPreferenceClickListener(new OnPreferenceClickListener() {
-			public boolean onPreferenceClick(Preference preference) {
-				if (preference.getKey().equals("prefs_help_key"))
-				{
-						//http://s.evendanan.net/ask_settings
-						//Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://s.evendanan.net/ask_settings"));
-						//startActivity(browserIntent);
-						Intent main = new Intent(getApplicationContext(), MainForm.class);
-						startActivity(main);
-						finish();
-						return true;
-				}
-				return false;
-			}
-		});
 	}
 
 	public static PackageInfo getPackageInfo(Context context) throws NameNotFoundException {
