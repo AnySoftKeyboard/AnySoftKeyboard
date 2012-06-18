973ec188aa7726454d13a6ccbfbdfab75720bbae
Menny Even Danan <menny@evendanan.net>
2012-06-18 12:13:04 +0300

    language pack template files and ANT rules

--- src/com/anysoftkeyboard/languagepack/change_me/PackBroadcastReceiver.java
+++ src/com/anysoftkeyboard/languagepack/change_me/PackBroadcastReceiver.java
@@ -4,7 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-public class KeyboardBroadcastReceiver extends BroadcastReceiver {
+public class PackBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context arg0, Intent arg1) {
