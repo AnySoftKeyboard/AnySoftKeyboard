c6a5fe4e7e910e6d356f9f1e16c8323b688190c2
Menny Even Danan <menny@evendanan.net>
2012-09-11 00:24:36 -0400

    weird exception, probably a token got dead...

--- src/com/anysoftkeyboard/ui/settings/UserWordsEditorAsyncTask.java
+++ src/com/anysoftkeyboard/ui/settings/UserWordsEditorAsyncTask.java
@@ -5,42 +5,47 @@ import android.os.AsyncTask;
 
 import com.menny.android.anysoftkeyboard.R;
 
-abstract class UserWordsEditorAsyncTask extends AsyncTask<Void, Void, Void>
-{
-    /**
+abstract class UserWordsEditorAsyncTask extends AsyncTask<Void, Void, Void> {
+	/**
      * 
      */
-    private final UserDictionaryEditorActivity mUserWordsEditorActivity;
+	private final UserDictionaryEditorActivity mUserWordsEditorActivity;
 
-    /**
-     * @param userDictionaryEditorActivity
-     */
-    UserWordsEditorAsyncTask(UserDictionaryEditorActivity userDictionaryEditorActivity) {
-        mUserWordsEditorActivity = userDictionaryEditorActivity;
-    }
+	/**
+	 * @param userDictionaryEditorActivity
+	 */
+	UserWordsEditorAsyncTask(
+			UserDictionaryEditorActivity userDictionaryEditorActivity) {
+		mUserWordsEditorActivity = userDictionaryEditorActivity;
+	}
 
-    private ProgressDialog progresDialog;
+	private ProgressDialog progresDialog;
 
-    @Override
-    protected void onPreExecute() {
-        super.onPreExecute();
+	@Override
+	protected void onPreExecute() {
+		super.onPreExecute();
 
-        progresDialog = new ProgressDialog(mUserWordsEditorActivity);
-        progresDialog.setTitle("");
-        progresDialog.setMessage(mUserWordsEditorActivity.getText(R.string.user_dictionary_read_please_wait));
-        progresDialog.setCancelable(false);
-        progresDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+		progresDialog = new ProgressDialog(mUserWordsEditorActivity);
+		progresDialog.setTitle("");
+		progresDialog.setMessage(mUserWordsEditorActivity
+				.getText(R.string.user_dictionary_read_please_wait));
+		progresDialog.setCancelable(false);
+		progresDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
 
-        progresDialog.setOwnerActivity(mUserWordsEditorActivity);
+		progresDialog.setOwnerActivity(mUserWordsEditorActivity);
 
-        progresDialog.show();
-    }
+		progresDialog.show();
+	}
 
-    protected void onPostExecute(Void result) {
-        if (progresDialog.isShowing())
-            progresDialog.dismiss();
-        applyResults(result);
-    }
+	protected void onPostExecute(Void result) {
+		try {
+			if (progresDialog.isShowing())
+				progresDialog.dismiss();
+		} catch (IllegalArgumentException e) {
+			// just swallowing it.
+		}
+		applyResults(result);
+	}
 
-    protected abstract void applyResults(Void result);
+	protected abstract void applyResults(Void result);
 }
