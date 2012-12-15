1dc313ef85020f0a1b57351e7303460cedef5f6a
Menny Even Danan <menny@evendanan.net>
2012-12-15 00:09:53 -0500

    finalizing contacts things (including missing file), and updating changelog

--- src/com/menny/android/anysoftkeyboard/AnyApplication.java
+++ src/com/menny/android/anysoftkeyboard/AnyApplication.java
@@ -20,7 +20,7 @@ import android.view.LayoutInflater;
 
 public class AnyApplication extends Application implements OnSharedPreferenceChangeListener {
 
-	public static final boolean DEBUG = true;
+	public static final boolean DEBUG = false;
 	//public static final boolean BLEEDING_EDGE = true;
 	
 	private static final String TAG = "ASK_APP";
