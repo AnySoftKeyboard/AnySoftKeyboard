b8c7805006f9ce8cb0c12b49fcc2d46b622d93b7
Menny Even Danan <menny@evendanan.net>
2013-02-17 22:17:48 -0500

    another inner class fix. Release v94

--- src/com/anysoftkeyboard/dictionaries/Suggest.java
+++ src/com/anysoftkeyboard/dictionaries/Suggest.java
@@ -34,23 +34,18 @@ import com.menny.android.anysoftkeyboard.R;
 /**
  * This class loads a dictionary and provides a list of suggestions for a given
  * sequence of characters. This includes corrections and completions.
- * 
- * @hide pending API Council Approval
- */
+ * */
 public class Suggest implements Dictionary.WordCallback {
 
-//	public static final int CORRECTION_NONE = 0;
-//	public static final int CORRECTION_BASIC = 1;
-//	public static final int CORRECTION_FULL = 2;
 	private static final String TAG = "ASK Suggest";
 
 	private Dictionary mMainDict;
 	private AutoText mAutoText;
-	
+
 	private int mMinimumWordSizeToStartCorrecting = 2;
-	
+
 	private final DictionaryFactory mDictionaryFactory;
-	
+
 	private Dictionary mUserDictionary;
 
 	private Dictionary mAutoDictionary;
@@ -61,7 +56,7 @@ public class Suggest implements Dictionary.WordCallback {
 
 	private final List<CharSequence> mDefaultInitialSuggestions;
 	private List<CharSequence> mInitialSuggestions = new ArrayList<CharSequence>();
-	
+
 	private int[] mPriorities = new int[mPrefMaxSuggestions];
 	private List<CharSequence> mSuggestions = new ArrayList<CharSequence>();
 	// private boolean mIncludeTypedWordIfValid;
@@ -76,36 +71,36 @@ public class Suggest implements Dictionary.WordCallback {
 	private boolean mIsFirstCharCapitalized;
 	private boolean mIsAllUpperCase;
 
-	//private int mCorrectionMode = CORRECTION_FULL;
+	// private int mCorrectionMode = CORRECTION_FULL;
 	private boolean mAutoTextEnabled = true;
 	private boolean mMainDictioanryEnabled = true;
 
 	public Suggest(Context context) {
-		mDictionaryFactory = AnyApplication.getFrankenRobot().embody(new Diagram<DictionaryFactory>() {});
+		mDictionaryFactory = AnyApplication.getFrankenRobot().embody(
+				new Diagram<DictionaryFactory>() {
+				});
 		for (int i = 0; i < mPrefMaxSuggestions; i++) {
 			StringBuilder sb = new StringBuilder(32);
 			mStringPool.add(sb);
 		}
-		
-		String[] initialSuggestions = context.getResources().getStringArray(R.array.english_initial_suggestions);
-		if (initialSuggestions != null)
-		{
-			mDefaultInitialSuggestions = new ArrayList<CharSequence>(initialSuggestions.length);
-			for(String suggestion : initialSuggestions)
+
+		String[] initialSuggestions = context.getResources().getStringArray(
+				R.array.english_initial_suggestions);
+		if (initialSuggestions != null) {
+			mDefaultInitialSuggestions = new ArrayList<CharSequence>(
+					initialSuggestions.length);
+			for (String suggestion : initialSuggestions)
 				mDefaultInitialSuggestions.add(suggestion);
-		}
-		else
-		{
+		} else {
 			mDefaultInitialSuggestions = new ArrayList<CharSequence>(0);
 		}
 	}
-/*
-	public int getCorrectionMode() {
-		return mCorrectionMode;
-	}
-*/
+
+	/*
+	 * public int getCorrectionMode() { return mCorrectionMode; }
+	 */
 	public void setCorrectionMode(boolean autoText, boolean mainDictionary) {
-		//mCorrectionMode = mode;
+		// mCorrectionMode = mode;
 		mAutoTextEnabled = autoText;
 		mMainDictioanryEnabled = mainDictionary;
 	}
@@ -123,24 +118,22 @@ public class Suggest implements Dictionary.WordCallback {
 
 	public void setMainDictionary(DictionaryAddOnAndBuilder dictionaryBuilder) {
 		if (AnyApplication.DEBUG)
-			Log.d(TAG, "Suggest: Got main dictionary! Type: "
-					+ ((dictionaryBuilder == null) ? "NULL" : dictionaryBuilder.getName()));
+			Log.d(TAG,
+					"Suggest: Got main dictionary! Type: "
+							+ ((dictionaryBuilder == null) ? "NULL"
+									: dictionaryBuilder.getName()));
 		if (mMainDict != null) {
 			mMainDict.close();
 		}
-		
-		if (dictionaryBuilder == null)
-		{
+
+		if (dictionaryBuilder == null) {
 			mMainDict = null;
 			mAutoText = null;
 			mInitialSuggestions = mDefaultInitialSuggestions;
-		}
-		else
-		{
-			try
-			{
+		} else {
+			try {
 				System.gc();
-				
+
 				mMainDict = dictionaryBuilder.createDictionary();
 				mMainDict.loadDictionary();
 			} catch (Exception e) {
@@ -158,13 +151,15 @@ public class Suggest implements Dictionary.WordCallback {
 	 */
 	public void setContactsDictionary(Context context, boolean enabled) {
 		if (!enabled && mContactsDictionary != null) {
-			//had one, but now config says it should be off
-			Log.i(TAG, "Contacts dictionary has been disabled! Closing resources.");
+			// had one, but now config says it should be off
+			Log.i(TAG,
+					"Contacts dictionary has been disabled! Closing resources.");
 			mContactsDictionary.close();
 			mContactsDictionary = null;
-		} else if (enabled && mContactsDictionary == null){
-			//config says it should be on, but I have none.
-			mContactsDictionary = mDictionaryFactory.createContactsDictionary(context);
+		} else if (enabled && mContactsDictionary == null) {
+			// config says it should be on, but I have none.
+			mContactsDictionary = mDictionaryFactory
+					.createContactsDictionary(context);
 		}
 	}
 
@@ -195,7 +190,7 @@ public class Suggest implements Dictionary.WordCallback {
 			mStringPool.add(sb);
 		}
 	}
-	
+
 	private boolean haveSufficientCommonality(String original,
 			CharSequence suggestion) {
 		final int originalLength = original.length();
@@ -205,17 +200,16 @@ public class Suggest implements Dictionary.WordCallback {
 		if (lengthDiff == 0 || lengthDiff == 1) {
 			return true;
 		}
-		
+
 		final int distance = IMEUtil.editDistance(original, suggestion);
-		
+
 		return distance <= 1;
 	}
 
-	public List<CharSequence> getInitialSuggestions()
-	{
+	public List<CharSequence> getInitialSuggestions() {
 		return mInitialSuggestions;
 	}
-	
+
 	/**
 	 * Returns a list of words that match the list of character codes passed in.
 	 * This list will be overwritten the next time this function is called.
@@ -228,95 +222,98 @@ public class Suggest implements Dictionary.WordCallback {
 	 *            index 0 in the array has the highest probability.
 	 * @return list of suggestions.
 	 */
-	public List<CharSequence> getSuggestions(/*View view,*/ WordComposer wordComposer,
-            boolean includeTypedWordIfValid) {
-        mHaveCorrection = false;
-        mIsFirstCharCapitalized = wordComposer.isFirstCharCapitalized();
-        mIsAllUpperCase = wordComposer.isAllUpperCase();
-        collectGarbage();
-        Arrays.fill(mPriorities, 0);
-        // mIncludeTypedWordIfValid = includeTypedWordIfValid;
+	public List<CharSequence> getSuggestions(
+			/* View view, */WordComposer wordComposer,
+			boolean includeTypedWordIfValid) {
+		mHaveCorrection = false;
+		mIsFirstCharCapitalized = wordComposer.isFirstCharCapitalized();
+		mIsAllUpperCase = wordComposer.isAllUpperCase();
+		collectGarbage();
+		Arrays.fill(mPriorities, 0);
+		// mIncludeTypedWordIfValid = includeTypedWordIfValid;
 
-        // Save a lowercase version of the original word
-        mOriginalWord = wordComposer.getTypedWord();
-        if (mOriginalWord.length() > 0) {
-            mOriginalWord = mOriginalWord.toString();
-            mLowerOriginalWord = mOriginalWord.toString().toLowerCase();
-        } else {
-            mLowerOriginalWord = "";
-        }
-        // Search the dictionary only if there are at least 2 (configurable) characters
-         if (wordComposer.size() >= mMinimumWordSizeToStartCorrecting) {
-        	 if (mContactsDictionary != null) {
-        		 if (AnyApplication.DEBUG) 
-        			 Log.v(TAG, "getSuggestions from contacts-dictionary");
-        		 
-        		 mContactsDictionary.getWords(wordComposer, this);
-        	 }
-        	 
-        	 if (mUserDictionary != null) {
-        		 if (AnyApplication.DEBUG)
-        			 Log.v(TAG, "getSuggestions from user-dictionary");
-        		 
-        		 mUserDictionary.getWords(wordComposer, this);
-        	 }
-        	 
-             if (mSuggestions.size() > 0 && isValidWord(mOriginalWord)) {
-                  mHaveCorrection = true;
-             }
-             
-             if (mMainDict != null) {
-            	 if (AnyApplication.DEBUG)
-            		 Log.v(TAG, "getSuggestions from main-dictionary");
-            	 mMainDict.getWords(wordComposer, this);
-             }
+		// Save a lowercase version of the original word
+		mOriginalWord = wordComposer.getTypedWord();
+		if (mOriginalWord.length() > 0) {
+			mOriginalWord = mOriginalWord.toString();
+			mLowerOriginalWord = mOriginalWord.toString().toLowerCase();
+		} else {
+			mLowerOriginalWord = "";
+		}
+		// Search the dictionary only if there are at least 2 (configurable)
+		// characters
+		if (wordComposer.size() >= mMinimumWordSizeToStartCorrecting) {
+			if (mContactsDictionary != null) {
+				if (AnyApplication.DEBUG)
+					Log.v(TAG, "getSuggestions from contacts-dictionary");
 
-             if (mMainDictioanryEnabled && mSuggestions.size() > 0) {
-                 mHaveCorrection = true;
-             }
-         }
-        
-        if (mOriginalWord != null) {
-            mSuggestions.add(0, mOriginalWord.toString());
-        }
-        // Check if the first suggestion has a minimum number of characters in
-        // common
-        if (mMainDictioanryEnabled && mSuggestions.size() > 1) {
-        	
-            if (!haveSufficientCommonality(mLowerOriginalWord, mSuggestions.get(1))) {
-                mHaveCorrection = false;
-            }
-        }
+				mContactsDictionary.getWords(wordComposer, this);
+			}
 
-        int i = 0;
-        int max = 6;
-        // Don't autotext the suggestions from the dictionaries
-        if (!mMainDictioanryEnabled && mAutoTextEnabled)
-            max = 1;
-        while (i < mSuggestions.size() && i < max) {
-            String suggestedWord = mSuggestions.get(i).toString().toLowerCase();
-            
-            CharSequence autoText = mAutoTextEnabled && mAutoText != null?
-            		mAutoText.lookup(suggestedWord, 0, suggestedWord.length()) : null;
-            // Is there an AutoText correction?
-            boolean canAdd = autoText != null;
-            // Is that correction already the current prediction (or original
-            // word)?
-            canAdd &= !TextUtils.equals(autoText, mSuggestions.get(i));
-            // Is that correction already the next predicted word?
-            if (canAdd && i + 1 < mSuggestions.size() && mMainDictioanryEnabled) {
-                canAdd &= !TextUtils.equals(autoText, mSuggestions.get(i + 1));
-            }
-            if (canAdd) {
-                mHaveCorrection = true;
-                mSuggestions.add(i + 1, autoText);
-                i++;
-            }
-            i++;
-        }
+			if (mUserDictionary != null) {
+				if (AnyApplication.DEBUG)
+					Log.v(TAG, "getSuggestions from user-dictionary");
 
-        return mSuggestions;
-    }
+				mUserDictionary.getWords(wordComposer, this);
+			}
+
+			if (mSuggestions.size() > 0 && isValidWord(mOriginalWord)) {
+				mHaveCorrection = true;
+			}
+
+			if (mMainDict != null) {
+				if (AnyApplication.DEBUG)
+					Log.v(TAG, "getSuggestions from main-dictionary");
+				mMainDict.getWords(wordComposer, this);
+			}
+
+			if (mMainDictioanryEnabled && mSuggestions.size() > 0) {
+				mHaveCorrection = true;
+			}
+		}
+
+		if (mOriginalWord != null) {
+			mSuggestions.add(0, mOriginalWord.toString());
+		}
+		// Check if the first suggestion has a minimum number of characters in
+		// common
+		if (mMainDictioanryEnabled && mSuggestions.size() > 1) {
+
+			if (!haveSufficientCommonality(mLowerOriginalWord,
+					mSuggestions.get(1))) {
+				mHaveCorrection = false;
+			}
+		}
+
+		int i = 0;
+		int max = 6;
+		// Don't autotext the suggestions from the dictionaries
+		if (!mMainDictioanryEnabled && mAutoTextEnabled)
+			max = 1;
+		while (i < mSuggestions.size() && i < max) {
+			String suggestedWord = mSuggestions.get(i).toString().toLowerCase();
+
+			CharSequence autoText = mAutoTextEnabled && mAutoText != null ? mAutoText
+					.lookup(suggestedWord, 0, suggestedWord.length()) : null;
+			// Is there an AutoText correction?
+			boolean canAdd = autoText != null;
+			// Is that correction already the current prediction (or original
+			// word)?
+			canAdd &= !TextUtils.equals(autoText, mSuggestions.get(i));
+			// Is that correction already the next predicted word?
+			if (canAdd && i + 1 < mSuggestions.size() && mMainDictioanryEnabled) {
+				canAdd &= !TextUtils.equals(autoText, mSuggestions.get(i + 1));
+			}
+			if (canAdd) {
+				mHaveCorrection = true;
+				mSuggestions.add(i + 1, autoText);
+				i++;
+			}
+			i++;
+		}
+
+		return mSuggestions;
+	}
 
 	public boolean hasMinimalCorrection() {
 		return mHaveCorrection;
@@ -403,20 +400,25 @@ public class Suggest implements Dictionary.WordCallback {
 		}
 
 		if (mMainDictioanryEnabled || mAutoTextEnabled) {
-			final boolean validFromMain = (mMainDictioanryEnabled && mMainDict != null && mMainDict.isValidWord(word));
-			final boolean validFromUser = (mUserDictionary != null && mUserDictionary.isValidWord(word));
-			//final boolean validFromAuto = (mAutoDictionary != null && mAutoDictionary.isValidWord(word));
-			final boolean validFromContacts = (mContactsDictionary != null && mContactsDictionary.isValidWord(word));
+			final boolean validFromMain = (mMainDictioanryEnabled
+					&& mMainDict != null && mMainDict.isValidWord(word));
+			final boolean validFromUser = (mUserDictionary != null && mUserDictionary
+					.isValidWord(word));
+			// final boolean validFromAuto = (mAutoDictionary != null &&
+			// mAutoDictionary.isValidWord(word));
+			final boolean validFromContacts = (mContactsDictionary != null && mContactsDictionary
+					.isValidWord(word));
 
 			if (AnyApplication.DEBUG)
 				Log.v(TAG, "Suggest::isValidWord(" + word
-						+ ") mMainDictioanryEnabled:" + mMainDictioanryEnabled+" mAutoTextEnabled:"+mAutoTextEnabled
-						+ " validFromMain:" + validFromMain 
-                                                + " validFromUser:"+ validFromUser
-						//+" validFromAuto:"+validFromAuto
+						+ ") mMainDictioanryEnabled:" + mMainDictioanryEnabled
+						+ " mAutoTextEnabled:" + mAutoTextEnabled
+						+ " validFromMain:" + validFromMain + " validFromUser:"
+						+ validFromUser
+						// +" validFromAuto:"+validFromAuto
 						+ " validFromContacts:" + validFromContacts);
 			return validFromMain || validFromUser
-					|| /*validFromAuto ||*/ validFromContacts;
+					|| /* validFromAuto || */validFromContacts;
 		} else {
 			return false;
 		}
@@ -438,12 +440,12 @@ public class Suggest implements Dictionary.WordCallback {
 		}
 		mSuggestions.clear();
 	}
-	
-	public void setMinimumWordLengthForCorrection(int minLength)
-	{
-		//making sure it is not negative or zero
+
+	public void setMinimumWordLengthForCorrection(int minLength) {
+		// making sure it is not negative or zero
 		mMinimumWordSizeToStartCorrecting = Math.max(1, minLength);
 	}
+
 	public DictionaryFactory getDictionaryFactory() {
 		return mDictionaryFactory;
 	}
