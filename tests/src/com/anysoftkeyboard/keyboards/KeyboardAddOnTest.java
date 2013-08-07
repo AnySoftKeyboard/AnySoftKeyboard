f781af171101fd20abf843d6d7803bbb61a87287
Menny Even Danan <menny@evendanan.net>
2013-08-06 21:42:56 -0400

    more tests. this time no defects found.

---
+++ tests/src/com/anysoftkeyboard/keyboards/KeyboardAddOnTest.java
@@ -0,0 +1,148 @@
+package com.anysoftkeyboard.keyboards;
+
+import android.content.Context;
+import android.test.AndroidTestCase;
+import android.text.TextUtils;
+import android.util.Log;
+import com.menny.android.anysoftkeyboard.R;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+
+public class KeyboardAddOnTest extends AndroidTestCase {
+
+    private Context mTesterContext;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        try {
+            Method m = AndroidTestCase.class.getMethod("getTestContext", new Class[] {});
+            mTesterContext = (Context) m.invoke(this, (Object[]) null);
+        } catch (Exception x) {
+            throw x;
+        }
+    }
+
+    public void testKeyboardAddOneApiAttributeValues() throws Exception {
+        //since I suppose to be backward compatible, these attributes values MUST NOT change!
+        assertEquals(R.styleable.KeyboardLayout_android_horizontalGap, 2);
+        assertEquals(R.styleable.KeyboardLayout_android_verticalGap, 3);
+        assertEquals(R.styleable.KeyboardLayout_android_keyHeight, 1);
+        assertEquals(R.styleable.KeyboardLayout_android_keyWidth, 0);
+        assertEquals(R.styleable.KeyboardLayout_Key_android_codes, 0);
+        assertEquals(R.styleable.KeyboardLayout_Key_android_iconPreview, 7);
+        assertEquals(R.styleable.KeyboardLayout_Key_android_isModifier, 4);
+        assertEquals(R.styleable.KeyboardLayout_Key_android_isRepeatable, 6);
+        assertEquals(R.styleable.KeyboardLayout_Key_android_isSticky, 5);
+        assertEquals(R.styleable.KeyboardLayout_Key_android_keyboardMode, 11);
+        assertEquals(R.styleable.KeyboardLayout_Key_android_keyEdgeFlags, 3);
+        assertEquals(R.styleable.KeyboardLayout_Key_android_keyIcon, 10);
+        assertEquals(R.styleable.KeyboardLayout_Key_android_keyLabel, 9);
+        assertEquals(R.styleable.KeyboardLayout_Key_android_keyOutputText, 8);
+        assertEquals(R.styleable.KeyboardLayout_Key_android_popupCharacters, 2);
+        assertEquals(R.styleable.KeyboardLayout_Key_android_popupKeyboard, 1);
+        assertEquals(R.styleable.KeyboardLayout_Key_hintLabel, 16);
+        assertEquals(R.styleable.KeyboardLayout_Key_isFunctional, 13);
+        assertEquals(R.styleable.KeyboardLayout_Key_keyDynamicEmblem, 18);
+        assertEquals(R.styleable.KeyboardLayout_Key_longPressCode, 12);
+        assertEquals(R.styleable.KeyboardLayout_Key_shiftedCodes, 14);
+        assertEquals(R.styleable.KeyboardLayout_Key_showPreview, 17);
+        assertEquals(R.styleable.KeyboardLayout_Key_shiftedKeyLabel, 15);
+        assertEquals(R.styleable.KeyboardLayout_Row_android_keyboardMode, 1);
+        assertEquals(R.styleable.KeyboardLayout_Row_android_rowEdgeFlags, 0);
+    }
+
+    public void testGetKeyboardDefaultEnabled() throws Exception {
+        ArrayList<KeyboardAddOnAndBuilder> enabledKeyboards =  KeyboardFactory.getEnabledKeyboards(getContext());
+        //checking that ASK English is enabled
+        boolean askEnglishEnabled = false;
+        for(KeyboardAddOnAndBuilder addOnAndBuilder : enabledKeyboards) {
+            if (addOnAndBuilder.getId().contains("c7535083-4fe6-49dc-81aa-c5438a1a343a")) {
+                assertTrue(addOnAndBuilder.getKeyboardDefaultEnabled());
+                assertEquals(addOnAndBuilder.getPackageName(), getContext().getPackageName());
+                askEnglishEnabled = true;
+            }
+        }
+        assertTrue(askEnglishEnabled);
+
+        //now checking my tester keyboard
+        boolean testerEnglishEnabled = false;
+        for(KeyboardAddOnAndBuilder addOnAndBuilder : enabledKeyboards) {
+            if (addOnAndBuilder.getId().contains("aef7f690-f485-11e2-b778-0800200c9a60")) {
+                assertTrue(addOnAndBuilder.getKeyboardDefaultEnabled());
+                assertEquals(addOnAndBuilder.getPackageName(), mTesterContext.getPackageName());
+                testerEnglishEnabled = true;
+            }
+        }
+        assertTrue(testerEnglishEnabled);
+
+        //now checking my tester keyboard 2
+        boolean tester2EnglishEnabled = false;
+        for(KeyboardAddOnAndBuilder addOnAndBuilder : enabledKeyboards) {
+            if (addOnAndBuilder.getId().contains("aef7f690-f485-11e2-b778-0800200c9a61")) {
+                tester2EnglishEnabled = true;
+            }
+        }
+        assertFalse(tester2EnglishEnabled);
+    }
+
+    private KeyboardAddOnAndBuilder getKeyboardFromFactory(String id) {
+        ArrayList<KeyboardAddOnAndBuilder> keyboards =  KeyboardFactory.getAllAvailableKeyboards(getContext());
+
+        for(KeyboardAddOnAndBuilder addOnAndBuilder : keyboards) {
+            if (addOnAndBuilder.getId().equals(KeyboardAddOnAndBuilder.KEYBOARD_PREF_PREFIX + id)) {
+                return addOnAndBuilder;
+            }
+        }
+
+        return null;
+    }
+
+    public void testGetKeyboardLocale() throws Exception {
+        KeyboardAddOnAndBuilder askEnglish = getKeyboardFromFactory("c7535083-4fe6-49dc-81aa-c5438a1a343a");
+        assertNotNull(askEnglish);
+        assertEquals(askEnglish.getKeyboardLocale(), "en");
+
+        KeyboardAddOnAndBuilder testerEnglish = getKeyboardFromFactory("aef7f690-f485-11e2-b778-0800200c9a60");
+        assertNotNull(testerEnglish);
+        assertEquals(testerEnglish.getKeyboardLocale(), "en");
+
+        KeyboardAddOnAndBuilder tester2Hebrew = getKeyboardFromFactory("aef7f690-f485-11e2-b778-0800200c9a61");
+        assertNotNull(tester2Hebrew);
+        assertEquals(tester2Hebrew.getKeyboardLocale(), "iw");
+
+        KeyboardAddOnAndBuilder tester3Console = getKeyboardFromFactory("aef7f690-f485-11e2-b778-0800200c9a62");
+        assertNotNull(tester3Console);
+        assertTrue(TextUtils.isEmpty(tester3Console.getKeyboardLocale()));
+    }
+
+
+
+    public void testGetIcon() throws Exception {
+
+    }
+
+    public void testHasScreenshot() throws Exception {
+        KeyboardAddOnAndBuilder askEnglish = getKeyboardFromFactory("c7535083-4fe6-49dc-81aa-c5438a1a343a");
+        assertNotNull(askEnglish);
+        assertTrue(askEnglish.hasScreenshot());
+
+        KeyboardAddOnAndBuilder testerEnglish = getKeyboardFromFactory("aef7f690-f485-11e2-b778-0800200c9a60");
+        assertNotNull(testerEnglish);
+        assertTrue(askEnglish.hasScreenshot());
+
+        KeyboardAddOnAndBuilder tester3Console = getKeyboardFromFactory("aef7f690-f485-11e2-b778-0800200c9a62");
+        assertNotNull(tester3Console);
+        assertFalse(tester3Console.hasScreenshot());
+    }
+
+    public void testGetScreenshot() throws Exception {
+
+    }
+
+    public void testCreateKeyboard() throws Exception {
+
+    }
+}
