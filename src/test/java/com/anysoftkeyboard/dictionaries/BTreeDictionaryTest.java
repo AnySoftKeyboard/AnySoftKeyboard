39f4162d1827135e050410bc2c695a5b1b49952c
Menny Even Danan <menny@evendanan.net>
2015-01-09 22:35:38 -0500

    re-enabling Robolectric tests. Issue #377
    Change-Id: I36b75220ddb99fe8f4ef55ccf172c50cca0c5b65

--- src/test/java/com/anysoftkeyboard/dictionaries/BTreeDictionaryTest.java
+++ src/test/java/com/anysoftkeyboard/dictionaries/BTreeDictionaryTest.java
@@ -16,7 +16,6 @@
 
 package com.anysoftkeyboard.dictionaries;
 
-import com.anysoftkeyboard.RobolectricAPI18TestRunner;
 import com.anysoftkeyboard.ui.settings.MainSettingsActivity;
 
 import org.junit.Assert;
@@ -24,11 +23,12 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-@RunWith(RobolectricAPI18TestRunner.class)
+@RunWith(RobolectricTestRunner.class)
 public class BTreeDictionaryTest {
 
 	TestableBTreeDictionary mDictionaryUnderTest;
@@ -38,184 +38,185 @@ public class BTreeDictionaryTest {
 		MainSettingsActivity activity = Robolectric.buildActivity(MainSettingsActivity.class).create().resume().get();
 		mDictionaryUnderTest = new TestableBTreeDictionary("TEST", activity);
 	}
-	@Test
-    public void testLoadDictionary() throws Exception {
-        //no words now
-        Assert.assertFalse(mDictionaryUnderTest.isValidWord((String) TestableBTreeDictionary.STORAGE[0][1]));
 
-        //ok, now yes words
-        mDictionaryUnderTest.loadDictionary();
-        for(int row=0; row<TestableBTreeDictionary.STORAGE.length; row++) {
-            final String word = (String)TestableBTreeDictionary.STORAGE[row][1];
-            final int freq = ((Integer)TestableBTreeDictionary.STORAGE[row][2]).intValue();
-            assertTrue("Word at row " + row + " (" + word + ") should be valid.", mDictionaryUnderTest.isValidWord(word));
-            Assert.assertEquals(mDictionaryUnderTest.getWordFrequency(word), freq);
-        }
-        //checking validity of the internal structure
-        assetNodeArrayIsValid(mDictionaryUnderTest.getRoot());
-    }
+	@Test
+	public void testLoadDictionary() throws Exception {
+		//no words now
+		Assert.assertFalse(mDictionaryUnderTest.isValidWord((String) TestableBTreeDictionary.STORAGE[0][1]));
+
+		//ok, now yes words
+		mDictionaryUnderTest.loadDictionary();
+		for (int row = 0; row < TestableBTreeDictionary.STORAGE.length; row++) {
+			final String word = (String) TestableBTreeDictionary.STORAGE[row][1];
+			final int freq = ((Integer) TestableBTreeDictionary.STORAGE[row][2]).intValue();
+			assertTrue("Word at row " + row + " (" + word + ") should be valid.", mDictionaryUnderTest.isValidWord(word));
+			Assert.assertEquals(mDictionaryUnderTest.getWordFrequency(word), freq);
+		}
+		//checking validity of the internal structure
+		assetNodeArrayIsValid(mDictionaryUnderTest.getRoot());
+	}
 
 	@Test
 	public void testAddWord() throws Exception {
-        mDictionaryUnderTest.loadDictionary();
+		mDictionaryUnderTest.loadDictionary();
 
-        assertTrue(mDictionaryUnderTest.addWord("new", 23));
-        Assert.assertEquals("new", mDictionaryUnderTest.wordRequestedToAddedToStorage);
-        Assert.assertEquals(23, mDictionaryUnderTest.wordFrequencyRequestedToAddedToStorage);
-        assertTrue(mDictionaryUnderTest.isValidWord("new"));
-        Assert.assertEquals(mDictionaryUnderTest.getWordFrequency("new"), 23);
-        //checking validity of the internal structure
-        assetNodeArrayIsValid(mDictionaryUnderTest.getRoot());
+		assertTrue(mDictionaryUnderTest.addWord("new", 23));
+		Assert.assertEquals("new", mDictionaryUnderTest.wordRequestedToAddedToStorage);
+		Assert.assertEquals(23, mDictionaryUnderTest.wordFrequencyRequestedToAddedToStorage);
+		assertTrue(mDictionaryUnderTest.isValidWord("new"));
+		Assert.assertEquals(mDictionaryUnderTest.getWordFrequency("new"), 23);
+		//checking validity of the internal structure
+		assetNodeArrayIsValid(mDictionaryUnderTest.getRoot());
 
-        assertTrue(mDictionaryUnderTest.addWord("new", 34));
-        Assert.assertEquals("new", mDictionaryUnderTest.wordRequestedToAddedToStorage);
-        Assert.assertEquals(34, mDictionaryUnderTest.wordFrequencyRequestedToAddedToStorage);
-        assertTrue(mDictionaryUnderTest.isValidWord("new"));
-        Assert.assertEquals(mDictionaryUnderTest.getWordFrequency("new"), 34);
-        //checking validity of the internal structure
-        assetNodeArrayIsValid(mDictionaryUnderTest.getRoot());
+		assertTrue(mDictionaryUnderTest.addWord("new", 34));
+		Assert.assertEquals("new", mDictionaryUnderTest.wordRequestedToAddedToStorage);
+		Assert.assertEquals(34, mDictionaryUnderTest.wordFrequencyRequestedToAddedToStorage);
+		assertTrue(mDictionaryUnderTest.isValidWord("new"));
+		Assert.assertEquals(mDictionaryUnderTest.getWordFrequency("new"), 34);
+		//checking validity of the internal structure
+		assetNodeArrayIsValid(mDictionaryUnderTest.getRoot());
 
-        assertTrue(mDictionaryUnderTest.addWord("newa", 45));
-        assertTrue(mDictionaryUnderTest.isValidWord("newa"));
-        Assert.assertEquals(mDictionaryUnderTest.getWordFrequency("new"), 34);
-        Assert.assertEquals(mDictionaryUnderTest.getWordFrequency("newa"), 45);
-        //checking validity of the internal structure
-        assetNodeArrayIsValid(mDictionaryUnderTest.getRoot());
+		assertTrue(mDictionaryUnderTest.addWord("newa", 45));
+		assertTrue(mDictionaryUnderTest.isValidWord("newa"));
+		Assert.assertEquals(mDictionaryUnderTest.getWordFrequency("new"), 34);
+		Assert.assertEquals(mDictionaryUnderTest.getWordFrequency("newa"), 45);
+		//checking validity of the internal structure
+		assetNodeArrayIsValid(mDictionaryUnderTest.getRoot());
 
-        assertTrue(mDictionaryUnderTest.addWord("nea", 47));
-        Assert.assertEquals("nea", mDictionaryUnderTest.wordRequestedToAddedToStorage);
-        Assert.assertEquals(47, mDictionaryUnderTest.wordFrequencyRequestedToAddedToStorage);
-        assertTrue(mDictionaryUnderTest.isValidWord("nea"));
-        Assert.assertEquals(mDictionaryUnderTest.getWordFrequency("new"), 34);
-        Assert.assertEquals(mDictionaryUnderTest.getWordFrequency("newa"), 45);
-        Assert.assertEquals(mDictionaryUnderTest.getWordFrequency("nea"), 47);
-        //checking validity of the internal structure
-        assetNodeArrayIsValid(mDictionaryUnderTest.getRoot());
+		assertTrue(mDictionaryUnderTest.addWord("nea", 47));
+		Assert.assertEquals("nea", mDictionaryUnderTest.wordRequestedToAddedToStorage);
+		Assert.assertEquals(47, mDictionaryUnderTest.wordFrequencyRequestedToAddedToStorage);
+		assertTrue(mDictionaryUnderTest.isValidWord("nea"));
+		Assert.assertEquals(mDictionaryUnderTest.getWordFrequency("new"), 34);
+		Assert.assertEquals(mDictionaryUnderTest.getWordFrequency("newa"), 45);
+		Assert.assertEquals(mDictionaryUnderTest.getWordFrequency("nea"), 47);
+		//checking validity of the internal structure
+		assetNodeArrayIsValid(mDictionaryUnderTest.getRoot());
 
-        assertTrue(mDictionaryUnderTest.addWord("neabb", 50));
-        Assert.assertEquals("neabb", mDictionaryUnderTest.wordRequestedToAddedToStorage);
-        Assert.assertEquals(50, mDictionaryUnderTest.wordFrequencyRequestedToAddedToStorage);
-        assertTrue(mDictionaryUnderTest.isValidWord("neabb"));
-        Assert.assertFalse(mDictionaryUnderTest.isValidWord("neab"));
-        Assert.assertEquals(mDictionaryUnderTest.getWordFrequency("new"), 34);
-        Assert.assertEquals(mDictionaryUnderTest.getWordFrequency("newa"), 45);
-        Assert.assertEquals(mDictionaryUnderTest.getWordFrequency("nea"), 47);
-        Assert.assertEquals(mDictionaryUnderTest.getWordFrequency("neabb"), 50);
-        Assert.assertEquals(mDictionaryUnderTest.getWordFrequency("neab"), 0);
-        //checking validity of the internal structure
-        assetNodeArrayIsValid(mDictionaryUnderTest.getRoot());
-    }
+		assertTrue(mDictionaryUnderTest.addWord("neabb", 50));
+		Assert.assertEquals("neabb", mDictionaryUnderTest.wordRequestedToAddedToStorage);
+		Assert.assertEquals(50, mDictionaryUnderTest.wordFrequencyRequestedToAddedToStorage);
+		assertTrue(mDictionaryUnderTest.isValidWord("neabb"));
+		Assert.assertFalse(mDictionaryUnderTest.isValidWord("neab"));
+		Assert.assertEquals(mDictionaryUnderTest.getWordFrequency("new"), 34);
+		Assert.assertEquals(mDictionaryUnderTest.getWordFrequency("newa"), 45);
+		Assert.assertEquals(mDictionaryUnderTest.getWordFrequency("nea"), 47);
+		Assert.assertEquals(mDictionaryUnderTest.getWordFrequency("neabb"), 50);
+		Assert.assertEquals(mDictionaryUnderTest.getWordFrequency("neab"), 0);
+		//checking validity of the internal structure
+		assetNodeArrayIsValid(mDictionaryUnderTest.getRoot());
+	}
 
 	@Test
 	public void testOnStorageChanged() throws Exception {
 
-    }
+	}
 
 	@Test
 	public void testDeleteWord() throws Exception {
-        mDictionaryUnderTest.loadDictionary();
-        //from read storage
-        String word = (String)TestableBTreeDictionary.STORAGE[0][1];
-        int wordFreq = ((Integer)TestableBTreeDictionary.STORAGE[0][2]).intValue();
-        assertTrue(mDictionaryUnderTest.isValidWord(word));
-        mDictionaryUnderTest.deleteWord(word);
-        Assert.assertFalse(mDictionaryUnderTest.isValidWord(word));
-        Assert.assertEquals(mDictionaryUnderTest.wordRequestedToBeDeletedFromStorage, word);
-        //checking validity of the internal structure
-        assetNodeArrayIsValid(mDictionaryUnderTest.getRoot());
+		mDictionaryUnderTest.loadDictionary();
+		//from read storage
+		String word = (String) TestableBTreeDictionary.STORAGE[0][1];
+		int wordFreq = ((Integer) TestableBTreeDictionary.STORAGE[0][2]).intValue();
+		assertTrue(mDictionaryUnderTest.isValidWord(word));
+		mDictionaryUnderTest.deleteWord(word);
+		Assert.assertFalse(mDictionaryUnderTest.isValidWord(word));
+		Assert.assertEquals(mDictionaryUnderTest.wordRequestedToBeDeletedFromStorage, word);
+		//checking validity of the internal structure
+		assetNodeArrayIsValid(mDictionaryUnderTest.getRoot());
 
-        //re-adding
-        assertTrue(mDictionaryUnderTest.addWord(word, wordFreq + 1));
-        assertTrue(mDictionaryUnderTest.isValidWord(word));
-        Assert.assertEquals(wordFreq+1, mDictionaryUnderTest.getWordFrequency(word));
-        mDictionaryUnderTest.wordRequestedToBeDeletedFromStorage = null;
-        mDictionaryUnderTest.deleteWord(word);
-        Assert.assertFalse(mDictionaryUnderTest.isValidWord(word));
-        Assert.assertEquals(mDictionaryUnderTest.wordRequestedToBeDeletedFromStorage, word);
-        //checking validity of the internal structure
-        assetNodeArrayIsValid(mDictionaryUnderTest.getRoot());
+		//re-adding
+		assertTrue(mDictionaryUnderTest.addWord(word, wordFreq + 1));
+		assertTrue(mDictionaryUnderTest.isValidWord(word));
+		Assert.assertEquals(wordFreq + 1, mDictionaryUnderTest.getWordFrequency(word));
+		mDictionaryUnderTest.wordRequestedToBeDeletedFromStorage = null;
+		mDictionaryUnderTest.deleteWord(word);
+		Assert.assertFalse(mDictionaryUnderTest.isValidWord(word));
+		Assert.assertEquals(mDictionaryUnderTest.wordRequestedToBeDeletedFromStorage, word);
+		//checking validity of the internal structure
+		assetNodeArrayIsValid(mDictionaryUnderTest.getRoot());
 
-        //a new one
-        word = "new";
-        assertTrue(mDictionaryUnderTest.addWord(word, wordFreq));
-        assertTrue(mDictionaryUnderTest.isValidWord(word));
-        Assert.assertEquals(wordFreq, mDictionaryUnderTest.getWordFrequency(word));
-        mDictionaryUnderTest.wordRequestedToBeDeletedFromStorage = null;
-        mDictionaryUnderTest.deleteWord(word);
-        Assert.assertFalse(mDictionaryUnderTest.isValidWord(word));
-        Assert.assertEquals(mDictionaryUnderTest.wordRequestedToBeDeletedFromStorage, word);
-        //checking validity of the internal structure
-        assetNodeArrayIsValid(mDictionaryUnderTest.getRoot());
+		//a new one
+		word = "new";
+		assertTrue(mDictionaryUnderTest.addWord(word, wordFreq));
+		assertTrue(mDictionaryUnderTest.isValidWord(word));
+		Assert.assertEquals(wordFreq, mDictionaryUnderTest.getWordFrequency(word));
+		mDictionaryUnderTest.wordRequestedToBeDeletedFromStorage = null;
+		mDictionaryUnderTest.deleteWord(word);
+		Assert.assertFalse(mDictionaryUnderTest.isValidWord(word));
+		Assert.assertEquals(mDictionaryUnderTest.wordRequestedToBeDeletedFromStorage, word);
+		//checking validity of the internal structure
+		assetNodeArrayIsValid(mDictionaryUnderTest.getRoot());
 
-        //none existing
-        Assert.assertFalse(mDictionaryUnderTest.isValidWord("fail"));
-        mDictionaryUnderTest.deleteWord("fail");
-        Assert.assertFalse(mDictionaryUnderTest.isValidWord("fail"));
-        //checking validity of the internal structure
-        assetNodeArrayIsValid(mDictionaryUnderTest.getRoot());
+		//none existing
+		Assert.assertFalse(mDictionaryUnderTest.isValidWord("fail"));
+		mDictionaryUnderTest.deleteWord("fail");
+		Assert.assertFalse(mDictionaryUnderTest.isValidWord("fail"));
+		//checking validity of the internal structure
+		assetNodeArrayIsValid(mDictionaryUnderTest.getRoot());
 
-        //deleting part of the root
-        mDictionaryUnderTest.addWord("root", 1);
-        mDictionaryUnderTest.addWord("rooting", 2);
-        mDictionaryUnderTest.addWord("rootina", 2);
-        Assert.assertFalse(mDictionaryUnderTest.isValidWord("roo"));
-        Assert.assertFalse(mDictionaryUnderTest.isValidWord("rooti"));
-        //checking validity of the internal structure
-        assetNodeArrayIsValid(mDictionaryUnderTest.getRoot());
+		//deleting part of the root
+		mDictionaryUnderTest.addWord("root", 1);
+		mDictionaryUnderTest.addWord("rooting", 2);
+		mDictionaryUnderTest.addWord("rootina", 2);
+		Assert.assertFalse(mDictionaryUnderTest.isValidWord("roo"));
+		Assert.assertFalse(mDictionaryUnderTest.isValidWord("rooti"));
+		//checking validity of the internal structure
+		assetNodeArrayIsValid(mDictionaryUnderTest.getRoot());
 
-        mDictionaryUnderTest.deleteWord("root");
-        Assert.assertFalse(mDictionaryUnderTest.isValidWord("roo"));
-        Assert.assertFalse(mDictionaryUnderTest.isValidWord("root"));
-        Assert.assertFalse(mDictionaryUnderTest.isValidWord("rooti"));
-        assertTrue(mDictionaryUnderTest.isValidWord("rooting"));
-        assertTrue(mDictionaryUnderTest.isValidWord("rootina"));
-        //checking validity of the internal structure
-        assetNodeArrayIsValid(mDictionaryUnderTest.getRoot());
+		mDictionaryUnderTest.deleteWord("root");
+		Assert.assertFalse(mDictionaryUnderTest.isValidWord("roo"));
+		Assert.assertFalse(mDictionaryUnderTest.isValidWord("root"));
+		Assert.assertFalse(mDictionaryUnderTest.isValidWord("rooti"));
+		assertTrue(mDictionaryUnderTest.isValidWord("rooting"));
+		assertTrue(mDictionaryUnderTest.isValidWord("rootina"));
+		//checking validity of the internal structure
+		assetNodeArrayIsValid(mDictionaryUnderTest.getRoot());
 
-        mDictionaryUnderTest.deleteWord("rooting");
-        Assert.assertFalse(mDictionaryUnderTest.isValidWord("root"));
-        Assert.assertFalse(mDictionaryUnderTest.isValidWord("rooti"));
-        Assert.assertFalse(mDictionaryUnderTest.isValidWord("rooting"));
-        assertTrue(mDictionaryUnderTest.isValidWord("rootina"));
-        //checking validity of the internal structure
-        assetNodeArrayIsValid(mDictionaryUnderTest.getRoot());
+		mDictionaryUnderTest.deleteWord("rooting");
+		Assert.assertFalse(mDictionaryUnderTest.isValidWord("root"));
+		Assert.assertFalse(mDictionaryUnderTest.isValidWord("rooti"));
+		Assert.assertFalse(mDictionaryUnderTest.isValidWord("rooting"));
+		assertTrue(mDictionaryUnderTest.isValidWord("rootina"));
+		//checking validity of the internal structure
+		assetNodeArrayIsValid(mDictionaryUnderTest.getRoot());
 
-        mDictionaryUnderTest.addWord("root", 1);
-        assertTrue(mDictionaryUnderTest.isValidWord("root"));
-        Assert.assertFalse(mDictionaryUnderTest.isValidWord("rooting"));
-        Assert.assertFalse(mDictionaryUnderTest.isValidWord("rooti"));
-        assertTrue(mDictionaryUnderTest.isValidWord("rootina"));
-        //checking validity of the internal structure
-        assetNodeArrayIsValid(mDictionaryUnderTest.getRoot());
+		mDictionaryUnderTest.addWord("root", 1);
+		assertTrue(mDictionaryUnderTest.isValidWord("root"));
+		Assert.assertFalse(mDictionaryUnderTest.isValidWord("rooting"));
+		Assert.assertFalse(mDictionaryUnderTest.isValidWord("rooti"));
+		assertTrue(mDictionaryUnderTest.isValidWord("rootina"));
+		//checking validity of the internal structure
+		assetNodeArrayIsValid(mDictionaryUnderTest.getRoot());
 
-        mDictionaryUnderTest.deleteWord("rootina");
-        assertTrue(mDictionaryUnderTest.isValidWord("root"));
-        Assert.assertFalse(mDictionaryUnderTest.isValidWord("rooting"));
-        Assert.assertFalse(mDictionaryUnderTest.isValidWord("rooti"));
-        Assert.assertFalse(mDictionaryUnderTest.isValidWord("rootina"));
-        //checking validity of the internal structure
-        assetNodeArrayIsValid(mDictionaryUnderTest.getRoot());
-    }
+		mDictionaryUnderTest.deleteWord("rootina");
+		assertTrue(mDictionaryUnderTest.isValidWord("root"));
+		Assert.assertFalse(mDictionaryUnderTest.isValidWord("rooting"));
+		Assert.assertFalse(mDictionaryUnderTest.isValidWord("rooti"));
+		Assert.assertFalse(mDictionaryUnderTest.isValidWord("rootina"));
+		//checking validity of the internal structure
+		assetNodeArrayIsValid(mDictionaryUnderTest.getRoot());
+	}
 
-    private void assetNodeArrayIsValid(BTreeDictionary.NodeArray root) {
-        assertTrue(root.length >=0);
-        assertTrue(root.length <= root.data.length);
-        for (int i=0;i<root.length;i++) {
-            assertNotNull(root.data[i]);
-            if (root.data[i].children != null)//it may be null.
-                assetNodeArrayIsValid(root.data[i].children);
-        }
-    }
+	private void assetNodeArrayIsValid(BTreeDictionary.NodeArray root) {
+		assertTrue(root.length >= 0);
+		assertTrue(root.length <= root.data.length);
+		for (int i = 0; i < root.length; i++) {
+			assertNotNull(root.data[i]);
+			if (root.data[i].children != null)//it may be null.
+				assetNodeArrayIsValid(root.data[i].children);
+		}
+	}
 
 	@Test
 	public void testClose() throws Exception {
-        mDictionaryUnderTest.loadDictionary();
+		mDictionaryUnderTest.loadDictionary();
 
-        assertTrue(mDictionaryUnderTest.isValidWord((String) TestableBTreeDictionary.STORAGE[0][1]));
-        mDictionaryUnderTest.close();
-        assertTrue(mDictionaryUnderTest.storageIsClosed);
-        Assert.assertFalse(mDictionaryUnderTest.isValidWord((String)TestableBTreeDictionary.STORAGE[0][1]));
-        Assert.assertEquals(mDictionaryUnderTest.getWordFrequency((String)TestableBTreeDictionary.STORAGE[0][1]), 0);
-        Assert.assertFalse(mDictionaryUnderTest.addWord("fail", 1));
-    }
+		assertTrue(mDictionaryUnderTest.isValidWord((String) TestableBTreeDictionary.STORAGE[0][1]));
+		mDictionaryUnderTest.close();
+		assertTrue(mDictionaryUnderTest.storageIsClosed);
+		Assert.assertFalse(mDictionaryUnderTest.isValidWord((String) TestableBTreeDictionary.STORAGE[0][1]));
+		Assert.assertEquals(mDictionaryUnderTest.getWordFrequency((String) TestableBTreeDictionary.STORAGE[0][1]), 0);
+		Assert.assertFalse(mDictionaryUnderTest.addWord("fail", 1));
+	}
 }
--- src/test/java/com/anysoftkeyboard/keyboards/KeyboardAddOnTest.java
+++ src/test/java/com/anysoftkeyboard/keyboards/KeyboardAddOnTest.java
@@ -2,12 +2,11 @@ package com.anysoftkeyboard.keyboards;
 
 import android.text.TextUtils;
 
-import com.anysoftkeyboard.RobolectricAPI18TestRunner;
-
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
 
 import java.util.ArrayList;
 
@@ -16,110 +15,110 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-@RunWith(RobolectricAPI18TestRunner.class)
+@RunWith(RobolectricTestRunner.class)
 public class KeyboardAddOnTest {
 
-    public static final String ASK_ENGLISH_1 = "c7535083-4fe6-49dc-81aa-c5438a1a343a";
-    public static final String TESTER_KEYBOARD_1 = "aef7f690-f485-11e2-b778-0800200c9a60";
-    public static final String TESTER_KEYBOARD_2 = "aef7f690-f485-11e2-b778-0800200c9a61";
-    public static final String TESTER_KEYBOARD_3 = "aef7f690-f485-11e2-b778-0800200c9a62";
+	public static final String ASK_ENGLISH_1 = "c7535083-4fe6-49dc-81aa-c5438a1a343a";
+	public static final String TESTER_KEYBOARD_1 = "aef7f690-f485-11e2-b778-0800200c9a60";
+	public static final String TESTER_KEYBOARD_2 = "aef7f690-f485-11e2-b778-0800200c9a61";
+	public static final String TESTER_KEYBOARD_3 = "aef7f690-f485-11e2-b778-0800200c9a62";
 
 	@Ignore("For some reason Robolectric(?) doesn't like missing attributes, although Android is fine with that")
 	@Test
 	public void testGetKeyboardDefaultEnabled() throws Exception {
-        ArrayList<KeyboardAddOnAndBuilder> enabledKeyboards =  KeyboardFactory.getEnabledKeyboards(Robolectric.application);
-        //checking that ASK English is enabled
-        boolean askEnglishEnabled = false;
-        for(KeyboardAddOnAndBuilder addOnAndBuilder : enabledKeyboards) {
-            if (addOnAndBuilder.getId().contains(ASK_ENGLISH_1)) {
-                assertTrue(addOnAndBuilder.getKeyboardDefaultEnabled());
-                assertEquals(addOnAndBuilder.getPackageName(), Robolectric.application.getPackageName());
-                askEnglishEnabled = true;
-            }
-        }
-        assertTrue(askEnglishEnabled);
+		ArrayList<KeyboardAddOnAndBuilder> enabledKeyboards = KeyboardFactory.getEnabledKeyboards(Robolectric.application);
+		//checking that ASK English is enabled
+		boolean askEnglishEnabled = false;
+		for (KeyboardAddOnAndBuilder addOnAndBuilder : enabledKeyboards) {
+			if (addOnAndBuilder.getId().contains(ASK_ENGLISH_1)) {
+				assertTrue(addOnAndBuilder.getKeyboardDefaultEnabled());
+				assertEquals(addOnAndBuilder.getPackageName(), Robolectric.application.getPackageName());
+				askEnglishEnabled = true;
+			}
+		}
+		assertTrue(askEnglishEnabled);
 
-        //now checking my tester keyboard
-        boolean testerEnglishEnabled = false;
-        for(KeyboardAddOnAndBuilder addOnAndBuilder : enabledKeyboards) {
-            if (addOnAndBuilder.getId().contains(TESTER_KEYBOARD_1)) {
-                assertTrue(addOnAndBuilder.getKeyboardDefaultEnabled());
-                assertEquals(addOnAndBuilder.getPackageName(), Robolectric.application.getPackageName());
-                testerEnglishEnabled = true;
-            }
-        }
-        assertTrue(testerEnglishEnabled);
+		//now checking my tester keyboard
+		boolean testerEnglishEnabled = false;
+		for (KeyboardAddOnAndBuilder addOnAndBuilder : enabledKeyboards) {
+			if (addOnAndBuilder.getId().contains(TESTER_KEYBOARD_1)) {
+				assertTrue(addOnAndBuilder.getKeyboardDefaultEnabled());
+				assertEquals(addOnAndBuilder.getPackageName(), Robolectric.application.getPackageName());
+				testerEnglishEnabled = true;
+			}
+		}
+		assertTrue(testerEnglishEnabled);
 
-        //now checking my tester keyboard 2
-        boolean tester2EnglishEnabled = false;
-        for(KeyboardAddOnAndBuilder addOnAndBuilder : enabledKeyboards) {
-            if (addOnAndBuilder.getId().contains(TESTER_KEYBOARD_2)) {
-                tester2EnglishEnabled = true;
-            }
-        }
-        assertFalse(tester2EnglishEnabled);
-    }
+		//now checking my tester keyboard 2
+		boolean tester2EnglishEnabled = false;
+		for (KeyboardAddOnAndBuilder addOnAndBuilder : enabledKeyboards) {
+			if (addOnAndBuilder.getId().contains(TESTER_KEYBOARD_2)) {
+				tester2EnglishEnabled = true;
+			}
+		}
+		assertFalse(tester2EnglishEnabled);
+	}
 
-    private KeyboardAddOnAndBuilder getKeyboardFromFactory(String id) {
-        ArrayList<KeyboardAddOnAndBuilder> keyboards =  KeyboardFactory.getAllAvailableKeyboards(Robolectric.application);
+	private KeyboardAddOnAndBuilder getKeyboardFromFactory(String id) {
+		ArrayList<KeyboardAddOnAndBuilder> keyboards = KeyboardFactory.getAllAvailableKeyboards(Robolectric.application);
 
-        for(KeyboardAddOnAndBuilder addOnAndBuilder : keyboards) {
-            if (addOnAndBuilder.getId().equals(KeyboardAddOnAndBuilder.KEYBOARD_PREF_PREFIX + id)) {
-                return addOnAndBuilder;
-            }
-        }
+		for (KeyboardAddOnAndBuilder addOnAndBuilder : keyboards) {
+			if (addOnAndBuilder.getId().equals(KeyboardAddOnAndBuilder.KEYBOARD_PREF_PREFIX + id)) {
+				return addOnAndBuilder;
+			}
+		}
 
-        return null;
-    }
+		return null;
+	}
 
 	@Ignore("For some reason Robolectric(?) doesn't like missing attributes, although Android is fine with that")
 	@Test
 	public void testGetKeyboardLocale() throws Exception {
-        KeyboardAddOnAndBuilder askEnglish = getKeyboardFromFactory(ASK_ENGLISH_1);
-        assertNotNull(askEnglish);
-        assertEquals(askEnglish.getKeyboardLocale(), "en");
+		KeyboardAddOnAndBuilder askEnglish = getKeyboardFromFactory(ASK_ENGLISH_1);
+		assertNotNull(askEnglish);
+		assertEquals(askEnglish.getKeyboardLocale(), "en");
 
-        KeyboardAddOnAndBuilder testerEnglish = getKeyboardFromFactory(TESTER_KEYBOARD_1);
-        assertNotNull(testerEnglish);
-        assertEquals(testerEnglish.getKeyboardLocale(), "en");
+		KeyboardAddOnAndBuilder testerEnglish = getKeyboardFromFactory(TESTER_KEYBOARD_1);
+		assertNotNull(testerEnglish);
+		assertEquals(testerEnglish.getKeyboardLocale(), "en");
 
-        KeyboardAddOnAndBuilder tester2Hebrew = getKeyboardFromFactory(TESTER_KEYBOARD_2);
-        assertNotNull(tester2Hebrew);
-        assertEquals(tester2Hebrew.getKeyboardLocale(), "iw");
+		KeyboardAddOnAndBuilder tester2Hebrew = getKeyboardFromFactory(TESTER_KEYBOARD_2);
+		assertNotNull(tester2Hebrew);
+		assertEquals(tester2Hebrew.getKeyboardLocale(), "iw");
 
-        KeyboardAddOnAndBuilder tester3Console = getKeyboardFromFactory(TESTER_KEYBOARD_3);
-        assertNotNull(tester3Console);
-        assertTrue(TextUtils.isEmpty(tester3Console.getKeyboardLocale()));
-    }
+		KeyboardAddOnAndBuilder tester3Console = getKeyboardFromFactory(TESTER_KEYBOARD_3);
+		assertNotNull(tester3Console);
+		assertTrue(TextUtils.isEmpty(tester3Console.getKeyboardLocale()));
+	}
 
 	@Test
 	public void testGetIcon() throws Exception {
 
-    }
+	}
 
 	@Ignore("For some reason Robolectric(?) doesn't like missing attributes, although Android is fine with that")
 	@Test
 	public void testHasScreenshot() throws Exception {
-        KeyboardAddOnAndBuilder askEnglish = getKeyboardFromFactory(ASK_ENGLISH_1);
-        assertNotNull(askEnglish);
-        assertTrue(askEnglish.hasScreenshot());
+		KeyboardAddOnAndBuilder askEnglish = getKeyboardFromFactory(ASK_ENGLISH_1);
+		assertNotNull(askEnglish);
+		assertTrue(askEnglish.hasScreenshot());
 
-        KeyboardAddOnAndBuilder testerEnglish = getKeyboardFromFactory(TESTER_KEYBOARD_1);
-        assertNotNull(testerEnglish);
-        assertTrue(askEnglish.hasScreenshot());
+		KeyboardAddOnAndBuilder testerEnglish = getKeyboardFromFactory(TESTER_KEYBOARD_1);
+		assertNotNull(testerEnglish);
+		assertTrue(askEnglish.hasScreenshot());
 
-        KeyboardAddOnAndBuilder tester3Console = getKeyboardFromFactory(TESTER_KEYBOARD_3);
-        assertNotNull(tester3Console);
-        assertFalse(tester3Console.hasScreenshot());
-    }
+		KeyboardAddOnAndBuilder tester3Console = getKeyboardFromFactory(TESTER_KEYBOARD_3);
+		assertNotNull(tester3Console);
+		assertFalse(tester3Console.hasScreenshot());
+	}
 
 	@Test
 	public void testGetScreenshot() throws Exception {
 
-    }
+	}
 
 	@Test
 	public void testCreateKeyboard() throws Exception {
 
-    }
+	}
 }
