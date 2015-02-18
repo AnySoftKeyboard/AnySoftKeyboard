8d2cc4134ffb2bbe8590c4f418a5176fa041ec21
Menny Even Danan <menny@evendanan.net>
2015-02-17 21:31:54 -0500

    adding settings key to the quick-text popup. Issue #390

--- src/main/java/com/anysoftkeyboard/quicktextkeys/ui/FrameKeyboardViewClickListener.java
+++ src/main/java/com/anysoftkeyboard/quicktextkeys/ui/FrameKeyboardViewClickListener.java
@@ -1,10 +1,15 @@
 package com.anysoftkeyboard.quicktextkeys.ui;
 
+import android.content.Intent;
 import android.view.View;
 
 import com.anysoftkeyboard.api.KeyCodes;
 import com.anysoftkeyboard.keyboards.views.OnKeyboardActionListener;
+import com.anysoftkeyboard.ui.settings.AdditionalUiSettingsFragment;
+import com.anysoftkeyboard.ui.settings.MainSettingsActivity;
 import com.menny.android.anysoftkeyboard.R;
+
+import net.evendanan.pushingpixels.FragmentChauffeurActivity;
 
 public class FrameKeyboardViewClickListener implements View.OnClickListener {
 	private final OnKeyboardActionListener mKeyboardActionListener;
@@ -25,6 +30,21 @@ public class FrameKeyboardViewClickListener implements View.OnClickListener {
 			case R.id.quick_keys_popup_return:
 				mKeyboardActionListener.onKey(KeyCodes.ENTER, null, 0, null, true);
 				break;
+			case R.id.quick_keys_popup_quick_keys_settings:
+				//starting the MainSettings activity, with TipsFragment
+				Intent i = new Intent(v.getContext(), MainSettingsActivity.class);
+				i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+				FragmentChauffeurActivity.addIntentArgsForAddingFragmentToUi(i, AdditionalUiSettingsFragment.class, AdditionalUiSettingsFragment.createArgsForConfiguringQuickTextKeys());
+				v.getContext().startActivity(i);
+				mKeyboardActionListener.onCancel();
+				break;
 		}
+	}
+
+	public void registerOnViews(View rootView) {
+		rootView.findViewById(R.id.quick_keys_popup_close).setOnClickListener(this);
+		rootView.findViewById(R.id.quick_keys_popup_backspace).setOnClickListener(this);
+		rootView.findViewById(R.id.quick_keys_popup_return).setOnClickListener(this);
+		rootView.findViewById(R.id.quick_keys_popup_quick_keys_settings).setOnClickListener(this);
 	}
 }
