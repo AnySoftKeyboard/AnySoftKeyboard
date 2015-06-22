70f552d9d56350e08a1f2e12177b56359aa11828
Menny Even Danan <menny@evendanan.net>
2015-06-21 21:44:05 -0400

    reporting v1.6 to build

--- buildSrc/src/main/groovy/versionbuilder/VersionBuilder.groovy
+++ buildSrc/src/main/groovy/versionbuilder/VersionBuilder.groovy
@@ -2,14 +2,14 @@ package versionbuilder
 
 class VersionBuilder {
     static final int GIT_COMMIT_COUNT_NORMALIZE = 2320;
-    static final int GIT_COMMIT_COUNT_MINOR_NORMALIZE = 140+50;
+    static final int GIT_COMMIT_COUNT_MINOR_NORMALIZE = 140+50+38;
 
     static def buildGitVersionNumber() {
         return Integer.parseInt('git rev-list --count HEAD'.execute().text.trim()) - GIT_COMMIT_COUNT_NORMALIZE;
     }
 
     static def buildGitVersionName() {
-        return String.format("%d.%d.%d", 1, 5, buildGitVersionNumber() - GIT_COMMIT_COUNT_MINOR_NORMALIZE);
+        return String.format("%d.%d.%d", 1, 6, buildGitVersionNumber() - GIT_COMMIT_COUNT_MINOR_NORMALIZE);
     }
 
 }
