e197c6274ea725c00a7a02e7903d0da70e309984
Menny Even Danan <menny@evendanan.net>
2015-11-09 23:38:04 -0500

    updating change-log for #457

--- src/main/java/com/anysoftkeyboard/ui/tutorials/VersionChangeLogs.java
+++ src/main/java/com/anysoftkeyboard/ui/tutorials/VersionChangeLogs.java
@@ -12,6 +12,7 @@ public class VersionChangeLogs {
         List<VersionChangeLog> log = new ArrayList<>();
 
         log.add(new VersionChangeLog(1, 6, "r1", Uri.parse("https://github.com/AnySoftKeyboard/AnySoftKeyboard/milestones/1.6-r1"),
+                "Clipboard actions! Checkout the utility keyboard (swipe up from the space-bar).",
                 "Small UI changes, too small to notice. But I'm happier.",
                 "YABTU."));
 
