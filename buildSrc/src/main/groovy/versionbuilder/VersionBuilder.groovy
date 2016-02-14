b6721ca607d54f1499aa6bc7cbf907f9eca82cba
Menny Even Danan <menny@evendanan.net>
2016-02-13 21:57:09 -0500

    too much stuff here, it is no longer 1.6, it is 1.7

--- buildSrc/src/main/groovy/versionbuilder/VersionBuilder.groovy
+++ buildSrc/src/main/groovy/versionbuilder/VersionBuilder.groovy
@@ -1,15 +1,22 @@
 package versionbuilder
 
-class VersionBuilder {
-    static final int GIT_COMMIT_COUNT_NORMALIZE = 2320;
-    static final int GIT_COMMIT_COUNT_MINOR_NORMALIZE = 140+50+38;
+public class VersionBuilder {
+    static final int GIT_COMMIT_COUNT_NORMALIZE = 2320
+    static final int GIT_COMMIT_COUNT_MINOR_NORMALIZE = 140+50+38+167
 
-    static def buildGitVersionNumber() {
-        return Integer.parseInt('git rev-list --count HEAD'.execute().text.trim()) - GIT_COMMIT_COUNT_NORMALIZE;
+    public static def buildGitVersionNumber() {
+        try {
+            return Integer.parseInt('git rev-list --count HEAD'.execute().text.trim()) - GIT_COMMIT_COUNT_NORMALIZE
+        } catch (Exception e) {
+            println("Failed to get version from git data. Error: "+e.message);
+            return 1
+        }
     }
 
-    static def buildGitVersionName() {
-        return String.format("%d.%d.%d", 1, 6, buildGitVersionNumber() - GIT_COMMIT_COUNT_MINOR_NORMALIZE);
+    public static def buildGitVersionName() {
+        int gitVersion = buildGitVersionNumber()
+        if (gitVersion < GIT_COMMIT_COUNT_MINOR_NORMALIZE) gitVersion = GIT_COMMIT_COUNT_MINOR_NORMALIZE + 1
+        return String.format("%d.%d.%d", 1, 7, gitVersion - GIT_COMMIT_COUNT_MINOR_NORMALIZE)
     }
 
 }
--- src/main/java/com/anysoftkeyboard/ui/tutorials/VersionChangeLogs.java
+++ src/main/java/com/anysoftkeyboard/ui/tutorials/VersionChangeLogs.java
@@ -11,9 +11,10 @@ public class VersionChangeLogs {
     static List<VersionChangeLog> createChangeLog() {
         List<VersionChangeLog> log = new ArrayList<>();
 
-        log.add(new VersionChangeLog(1, 6, "r4", Uri.parse("https://github.com/AnySoftKeyboard/AnySoftKeyboard/milestones/1.6-r4"),
+        log.add(new VersionChangeLog(1, 7, "", Uri.parse("https://github.com/AnySoftKeyboard/AnySoftKeyboard/milestones/1.7"),
                 "Printing-out speed ups. This is very helpful to you 150-words-a-minute wizards.",
                 "Physical-Keyboard interaction enhancements. This has Wife-Seal-of-Approval stamp on it.",
+                "Automatically switch to previously used layout in an App. Probably good for multilingual users.",
                 "Localization update: PL, CA, MY, UK, DE.",
                 "YABTU"));
 
