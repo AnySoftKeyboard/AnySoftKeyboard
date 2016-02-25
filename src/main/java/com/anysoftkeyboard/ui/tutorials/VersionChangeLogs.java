0f1bf1c2b7ee79f3fa5dfb282958a97c8f2128f5
Menny Even Danan <menny@evendanan.net>
2016-02-25 17:22:21 -0500

    starting development on 1.7-r1, updating libraries

--- src/main/java/com/anysoftkeyboard/ui/tutorials/VersionChangeLogs.java
+++ src/main/java/com/anysoftkeyboard/ui/tutorials/VersionChangeLogs.java
@@ -11,6 +11,10 @@ public class VersionChangeLogs {
     static List<VersionChangeLog> createChangeLog() {
         List<VersionChangeLog> log = new ArrayList<>();
 
+        log.add(new VersionChangeLog(1, 7, "r1", Uri.parse("https://github.com/AnySoftKeyboard/AnySoftKeyboard/milestones/1.7-r1"),
+                "Localization update: ",
+                "YABTU"));
+
         log.add(new VersionChangeLog(1, 7, "", Uri.parse("https://github.com/AnySoftKeyboard/AnySoftKeyboard/milestones/1.7"),
                 "Printing-out speed ups. This is very helpful to you 150-words-a-minute wizards.",
                 "Physical-Keyboard interaction enhancements. This has Wife-Seal-of-Approval stamp on it.",
