6d6b76f154f3b7025d5875aed728ead238b14995
Menny Even Danan <menny@evendanan.net>
2016-03-25 08:36:53 -0400

    Correctly setting secure env

--- download_signing_files.sh
+++ download_signing_files.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
-wget ${KEYSTORE_FILE_URL} -q -O stalker.keystore
-wget ${PUBLISH_CERT_FILE_URL} -q -O apk_upload_key.p12
+echo "Downloading certs for key $1"
+echo "Downloading certs for test $2"
+echo "ENV Key $ANYSOFTKEYBOARD_KEYSTORE_ALIAS"
+echo "ENV Test $MY_TEST_ENV_VAL"
+
+wget $KEYSTORE_FILE_URL -q -O /tmp/anysoftkeyboard.keystore
+wget $PUBLISH_CERT_FILE_URL -q -O /tmp/apk_upload_key.p12
