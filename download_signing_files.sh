c1e3799e754816cd5b791c73cc333b49ea05d135
Menny Even Danan <menny@evendanan.net>
2016-03-26 18:14:47 -0400

    fixing tmp path for keystore

--- download_signing_files.sh
+++ download_signing_files.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
-echo "Downloading certs for key $1"
-echo "Downloading certs for test $2"
-echo "ENV Key $ANYSOFTKEYBOARD_KEYSTORE_ALIAS"
-echo "ENV Test $MY_TEST_ENV_VAL"
 
 wget $KEYSTORE_FILE_URL -q -O /tmp/anysoftkeyboard.keystore
 wget $PUBLISH_CERT_FILE_URL -q -O /tmp/apk_upload_key.p12
