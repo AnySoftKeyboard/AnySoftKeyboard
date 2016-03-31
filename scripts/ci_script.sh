2cbb83d4534a6c7dd5b5e3c6ede9edde315e67d8
Menny Even Danan <menny@evendanan.net>
2016-03-31 10:33:39 -0400

    updating ci-script to support deploy to RELEASE

--- scripts/ci_script.sh
+++ scripts/ci_script.sh
@@ -9,12 +9,21 @@ fi
 
 IS_POST_MERGE=0
 if [ "${COMMIT_BRANCH}" == 'master' ]; then
-    if [ ! -z $KEYSTORE_FILE_URL ]; then
+    if [ ! -z ${KEYSTORE_FILE_URL} ]; then
         IS_POST_MERGE=1
     fi
 fi
 
-if [ $IS_POST_MERGE -eq 1 ]; then
+REQUEST_TO_DEPLOY_RELEASE=$(git log -1 --pretty=%s | grep -e "^DEPLOY-RELEASE")
+REQUEST_TO_DEPLOY_DISABLE=$(git log -1 --pretty=%s | grep -e "^DEPLOY-DISABLE")
+DEPLOY_METHOD=1
+if [ ! -z ${REQUEST_TO_DEPLOY_RELEASE} ]; then
+    DEPLOY_METHOD=2
+elif [ ! -z ${REQUEST_TO_DEPLOY_DISABLE} ]; then
+    DEPLOY_METHOD=0
+fi
+
+if [ ${IS_POST_MERGE} -eq 1 ]; then
     echo "[POST MERGE] Downloading signature files"
     scripts/download_signing_files.sh
 fi
@@ -24,9 +33,16 @@ echo "Doing commit checks..."
 ./gradlew jacocoTestReport
 
 
-if [ $IS_POST_MERGE -eq 1 ]; then
-    echo "[POST MERGE] Building and deploying CANARY..."
-    ./gradlew assembleCanary publishCanary
-    echo "[POST MERGE] Building release and debug..."
-    ./gradlew assembleDebug assembleRelease
+if [ ${IS_POST_MERGE} -eq 1 ]; then
+    if [ ${DEPLOY_METHOD} -eq 0 ]; then
+        echo "[POST MERGE] deploy was disable for this commit"
+    elif [ ${DEPLOY_METHOD} -eq 1 ]; then
+        echo "[POST MERGE] Building and deploying CANARY (to beta channel)..."
+        ./gradlew assembleCanary publishCanary
+    elif [ ${DEPLOY_METHOD} -eq 1 ]; then
+        echo "[POST MERGE] Building and deploying RELEASE (to beta channel)..."
+        ./gradlew assembleRelease publishRelease
+    else
+        echo "[POST MERGE] unknown DEPLOY_METHOD '${DEPLOY_METHOD}'! Not deploying."
+    fi
 fi
