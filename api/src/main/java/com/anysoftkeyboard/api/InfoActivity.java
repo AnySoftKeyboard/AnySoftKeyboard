126c6fe928b456c56b901710142dbe2fb9804e5a
Menny Even Danan <menny@evendanan.net>
2016-04-02 20:43:45 -0400

    code clean up and typo fix
    Change-Id: Iabf6ebdaec14d73cbf997b80d785fcd28d739c70

--- api/src/main/java/com/anysoftkeyboard/api/InfoActivity.java
+++
@@ -1,8 +0,0 @@
-package com.anysoftkeyboard.api;
-
-import android.app.Activity;
-
-
-public class InfoActivity extends Activity {
-
-}
--- api/src/main/java/com/anysoftkeyboard/api/LanguageChangedBroadcastReceiver.java
+++ api/src/main/java/com/anysoftkeyboard/api/LanguageChangedBroadcastReceiver.java
@@ -8,15 +8,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 
+public abstract class LanguageChangedBroadcastReceiver extends BroadcastReceiver {
 
-/**
- * @author lado
- */
-public abstract class LanguageChangeddBroadcastReceiver extends BroadcastReceiver {
-
-    /* (non-Javadoc)
-     * @see android.content.BroadcastReceiver#onReceive(android.content.Context, android.content.Intent)
-     */
     @Override
     public final void onReceive(Context context, Intent intent) {
         final String action = intent.getAction();
@@ -25,26 +18,6 @@ public abstract class LanguageChangeddBroadcastReceiver extends BroadcastReceive
         } else if (action.equals(".NOTIFY_OFF")) {
             cancelNotification(context, intent);
         }
-        //        if(action.equals("notifylanguage")){
-//            Notification notification = new Notification();
-//
-//            Intent notificationIntent = new Intent();
-//            PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
-//                    notificationIntent, 0);
-//
-//            notification.setLatestEventInfo(getApplicationContext(),
-//                    getText("ka"), "ka",
-//                    contentIntent);
-//            //this will not work. Need to find a way to show notification as a different package.
-//            //notification.icon = current.getKeyboardIconResId();
-//            notification.icon = R.drawable.ka;
-//
-//            notification.defaults = 0;// no sound, vibrate, etc.
-//            // notifying
-//            NotificationManager    mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE); 
-//            mNotificationManager.notify(KEYBOARD_NOTIFICATION_ID, notification);
-
-        //}
     }
 
     private void sendNotification(Context ctx, Intent i) {
