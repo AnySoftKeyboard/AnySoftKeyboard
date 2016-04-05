1ac709f209bba4074e4a750f7ba2a6de56a92371
Menny Even Danan <menny@evendanan.net>
2016-04-05 13:43:03 -0400

    change log related to #614

--- src/main/java/com/anysoftkeyboard/ui/tutorials/VersionChangeLogs.java
+++ src/main/java/com/anysoftkeyboard/ui/tutorials/VersionChangeLogs.java
@@ -13,7 +13,8 @@ public class VersionChangeLogs {
 
         log.add(new VersionChangeLog(1, 7, "r3", Uri.parse("https://github.com/AnySoftKeyboard/AnySoftKeyboard/milestones/1.7-r3"),
                 "Even more bugs crashed!",
-                "The utility box (swipe up from space-bar) now has SELECT-ALL key. Pro tip: long-press that key and then use left&right arrows to precisely select characters."));
+                "The utility box (swipe up from space-bar) now has SELECT-ALL key. Pro tip: long-press that key and then use left&right arrows to precisely select characters.",
+                "Talking about space-bar. Try double-spacing."));
 
         log.add(new VersionChangeLog(1, 7, "r2", Uri.parse("https://github.com/AnySoftKeyboard/AnySoftKeyboard/milestones/1.7-r2"),
                 "Bugs, bugs, bugs. Squashed.",
