4ef1a6bf5006a8083cf0527125f48698087af663
Menny Even Danan <menny@evendanan.net>
2016-06-09 15:54:33 -0400

    removing unused import

--- src/main/java/com/anysoftkeyboard/keyboards/views/preview/PositionCalculator.java
+++ src/main/java/com/anysoftkeyboard/keyboards/views/preview/PositionCalculator.java
@@ -4,7 +4,6 @@ import android.graphics.Point;
 import android.view.View;
 
 import com.anysoftkeyboard.keyboards.Keyboard;
-import com.anysoftkeyboard.keyboards.views.AnyKeyboardBaseView;
 
 public interface PositionCalculator {
 	Point calculatePositionForPreview(Keyboard.Key key, View keyboardView, PreviewPopupTheme theme, int[] windowOffset);
