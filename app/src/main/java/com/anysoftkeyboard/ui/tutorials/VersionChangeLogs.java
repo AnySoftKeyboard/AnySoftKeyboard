32a2f5d396b313658d81cb672b97ad128661b5e1
Menny Even Danan <menny@evendanan.net>
2016-12-11 20:18:08 -0500

    DEPLOY-CANARY: updating localization for BE, UK, and SL.

--- app/src/main/java/com/anysoftkeyboard/ui/tutorials/VersionChangeLogs.java
+++ app/src/main/java/com/anysoftkeyboard/ui/tutorials/VersionChangeLogs.java
@@ -18,7 +18,7 @@ public class VersionChangeLogs {
                 "Some bug fixes.",
                 "Emojis are no longer popups, but their own keyboard. Plus, long press to get various skin tones (if available).",
                 "YABTU",
-                "Localization update: "));
+                "Localization update: BE, UK, and SL."));
 
         log.add(new VersionChangeLog(1, 8, "r6", Uri.parse("https://github.com/AnySoftKeyboard/AnySoftKeyboard/milestones/1.8-r6"),
                 "Quite a few bug fixes, some related to shift states.",
