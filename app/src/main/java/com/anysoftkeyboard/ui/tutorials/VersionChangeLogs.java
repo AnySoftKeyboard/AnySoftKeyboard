0bad1441a15d59b844905ebf506294bca62e08e7
Menny Even Danan <menny@evendanan.net>
2017-02-04 00:20:24 +0200

    DEPLOY-CANARY: Change log for canary release

--- app/src/main/java/com/anysoftkeyboard/ui/tutorials/VersionChangeLogs.java
+++ app/src/main/java/com/anysoftkeyboard/ui/tutorials/VersionChangeLogs.java
@@ -13,6 +13,7 @@ public class VersionChangeLogs {
 
         log.add(new VersionChangeLog(1, 8, "r9", Uri.parse("https://github.com/AnySoftKeyboard/AnySoftKeyboard/milestones/1.8-r9"),
                 "New Initial Setup Wizard. For clearer activation.",
+                "Seems like we did not include words with accents in suggestions. We do now!",
                 "Yet more bugs squashed.",
                 "Localization update: BE, NL, FR, SL, DE."));
 
