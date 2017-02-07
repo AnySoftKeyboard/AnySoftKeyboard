174f1cf6fde152a8fbbf15001962d6df4d93fa1a
Menny Even Danan <menny@evendanan.net>
2017-02-07 07:29:24 +0100

    DEPLOY-CANARY: releasing improved tag search

--- app/src/main/java/com/anysoftkeyboard/ui/tutorials/VersionChangeLogs.java
+++ app/src/main/java/com/anysoftkeyboard/ui/tutorials/VersionChangeLogs.java
@@ -14,6 +14,7 @@ public class VersionChangeLogs {
         log.add(new VersionChangeLog(1, 8, "r9", Uri.parse("https://github.com/AnySoftKeyboard/AnySoftKeyboard/milestones/1.8-r9"),
                 "New Initial Setup Wizard. For clearer activation.",
                 "Seems like we did not include words with accents in suggestions. We do now!",
+                "Better Emoji-Search experience. Type a : to start searching.",
                 "Yet more bugs squashed.",
                 "Localization update: BE, NL, FR, SL, DE."));
 
