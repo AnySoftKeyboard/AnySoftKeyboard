d235a55051b164319c91a84d4fe20006eee5efd1
Menny Even Danan <menny@evendanan.net>
2017-04-13 13:19:25 -0400

    fixing unit-tests after rebase

--- app/src/test/java/com/anysoftkeyboard/keyboards/KeyboardFactoryTest.java
+++ app/src/test/java/com/anysoftkeyboard/keyboards/KeyboardFactoryTest.java
@@ -5,6 +5,7 @@ import com.anysoftkeyboard.SharedPrefsHelper;
 import com.menny.android.anysoftkeyboard.AnyApplication;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RuntimeEnvironment;
@@ -14,24 +15,34 @@ import java.util.List;
 @RunWith(AnySoftKeyboardTestRunner.class)
 public class KeyboardFactoryTest {
 
+    private KeyboardFactory mKeyboardFactory;
+
+    @Before
+    public void setUp() throws Exception {
+        mKeyboardFactory = AnyApplication.getKeyboardFactory(RuntimeEnvironment.application);
+    }
+
     @Test
     public void hasMultipleAlphabets() throws Exception {
-        Assert.assertFalse(AnyApplication.getKeyboardFactory(RuntimeEnvironment.application).hasMultipleAlphabets());
+        Assert.assertFalse(mKeyboardFactory.hasMultipleAlphabets());
 
         SharedPrefsHelper.ensureKeyboardAtIndexEnabled(1, true);
-        Assert.assertTrue(AnyApplication.getKeyboardFactory(RuntimeEnvironment.application).hasMultipleAlphabets());
+        Assert.assertTrue(mKeyboardFactory.hasMultipleAlphabets());
 
         SharedPrefsHelper.ensureKeyboardAtIndexEnabled(0, false);
-        Assert.assertFalse(AnyApplication.getKeyboardFactory(RuntimeEnvironment.application).hasMultipleAlphabets());
+        Assert.assertFalse(mKeyboardFactory.hasMultipleAlphabets());
     }
 
     @Test
     public void testDefaultKeyboardId() {
-        final List<KeyboardAddOnAndBuilder> allAddOns = AnyApplication.getKeyboardFactory(RuntimeEnvironment.application).getAllAddOns();
-        Assert.assertEquals(3, allAddOns.size());
-        KeyboardAddOnAndBuilder addon = AnyApplication.getKeyboardFactory(RuntimeEnvironment.application).getEnabledAddOn();
+        final List<KeyboardAddOnAndBuilder> allAddOns = mKeyboardFactory.getAllAddOns();
+        Assert.assertEquals(7, allAddOns.size());
+        KeyboardAddOnAndBuilder addon = mKeyboardFactory.getEnabledAddOn();
         Assert.assertNotNull(addon);
         Assert.assertEquals("c7535083-4fe6-49dc-81aa-c5438a1a343a", addon.getId());
+
+        Assert.assertTrue(mKeyboardFactory.isAddOnEnabledByDefault("c7535083-4fe6-49dc-81aa-c5438a1a343a"));
+        Assert.assertFalse(mKeyboardFactory.isAddOnEnabledByDefault("c7535083-4fe6-49dc-81aa-c5438a1a343b"));
     }
 
 }
