16d917fc21d1c13c53658fbf58699b8f6389fa1e
Menny Even Danan <menny@evendanan.net>
2017-08-22 12:25:52 -0400

    update crowdin script to be more explicit

--- scripts/upload_strings_to_crowdin_api.sh
+++ scripts/upload_strings_to_crowdin_api.sh
@@ -5,6 +5,18 @@ if [ -z "${CROWDIN_API}" ]; then
     exit 1
 fi
 
+UPDATE_OPTION=""
+if [ "$1" == "keep" ]; then
+    echo "Updating string and keeping previous translations..."
+    UPDATE_OPTION="&update_option=update_as_unapproved"
+elif [ "$1" == "remove" ]; then
+    echo "Updating string and removing previous translations..."
+    UPDATE_OPTION=""
+else
+  echo "Please specify update option ('keep', 'remove') for translations"
+  exit 1
+fi
+
 curl \
   -F "files[strings.xml]=@app/src/main/res/values/strings.xml" \
-  https://api.crowdin.com/api/project/anysoftkeyboard/update-file?key=${CROWDIN_API}&update_option=update_as_unapproved
+  https://api.crowdin.com/api/project/anysoftkeyboard/update-file?key=${CROWDIN_API}${UPDATE_OPTION}
