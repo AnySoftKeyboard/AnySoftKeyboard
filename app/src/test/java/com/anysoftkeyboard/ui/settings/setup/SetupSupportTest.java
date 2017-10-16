61a8d7578c2b2b4623b1987567b1c9b1afe4ec62
Menny Even-Danan <menny@evendanan.net>
2017-10-16 02:29:47 +0000

    Adding popup-link animation

--- app/src/test/java/com/anysoftkeyboard/ui/settings/setup/SetupSupportTest.java
+++ app/src/test/java/com/anysoftkeyboard/ui/settings/setup/SetupSupportTest.java
@@ -1,6 +1,8 @@
 package com.anysoftkeyboard.ui.settings.setup;
 
 import android.content.ComponentName;
+import android.view.View;
+import android.view.animation.Animation;
 
 import com.anysoftkeyboard.AnySoftKeyboardTestRunner;
 import com.anysoftkeyboard.keyboards.KeyboardAddOnAndBuilder;
@@ -12,6 +14,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 import org.robolectric.RuntimeEnvironment;
 
@@ -125,5 +128,51 @@ public class SetupSupportTest {
 
         Mockito.doReturn("ru").when(hebrewBuilder).getKeyboardLocale();
         Assert.assertTrue(SetupSupport.hasLanguagePackForCurrentLocale(mockResponse));
+    }
+
+    @Test
+    public void testPopupAnimation() {
+        View v1 = Mockito.mock(View.class);
+        View v2 = Mockito.mock(View.class);
+        Mockito.doReturn(mApplication).when(v1).getContext();
+        Mockito.doReturn(mApplication).when(v2).getContext();
+
+        SetupSupport.popupViewAnimation(v1, v2);
+
+        ArgumentCaptor<Animation> animation1Captor = ArgumentCaptor.forClass(Animation.class);
+        ArgumentCaptor<Animation> animation2Captor = ArgumentCaptor.forClass(Animation.class);
+        Mockito.verify(v1).startAnimation(animation1Captor.capture());
+        Mockito.verify(v2).startAnimation(animation2Captor.capture());
+
+        Animation animation1 = animation1Captor.getValue();
+        Animation animation2 = animation2Captor.getValue();
+
+        Assert.assertEquals(500, animation1.getStartOffset());
+        Assert.assertEquals(700, animation2.getStartOffset());
+    }
+
+    @Test
+    public void testPopupViewAnimationWithIds() {
+        View v1 = Mockito.mock(View.class);
+        View v2 = Mockito.mock(View.class);
+        Mockito.doReturn(mApplication).when(v1).getContext();
+        Mockito.doReturn(mApplication).when(v2).getContext();
+
+        View rootView = Mockito.mock(View.class);
+        Mockito.doReturn(v1).when(rootView).findViewById(1);
+        Mockito.doReturn(v2).when(rootView).findViewById(2);
+
+        SetupSupport.popupViewAnimationWithIds(rootView, 1, 2);
+
+        ArgumentCaptor<Animation> animation1Captor = ArgumentCaptor.forClass(Animation.class);
+        ArgumentCaptor<Animation> animation2Captor = ArgumentCaptor.forClass(Animation.class);
+        Mockito.verify(v1).startAnimation(animation1Captor.capture());
+        Mockito.verify(v2).startAnimation(animation2Captor.capture());
+
+        Animation animation1 = animation1Captor.getValue();
+        Animation animation2 = animation2Captor.getValue();
+
+        Assert.assertEquals(500, animation1.getStartOffset());
+        Assert.assertEquals(700, animation2.getStartOffset());
     }
 }
