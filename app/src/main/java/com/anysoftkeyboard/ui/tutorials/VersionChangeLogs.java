83a5eb42799a8f6eb96ad3270b3a1c8c1f2375b3
Menny Even Danan <menny@evendanan.net>
2017-12-28 21:17:56 -0500

    Fixing typo in change-log

--- app/src/main/java/com/anysoftkeyboard/ui/tutorials/VersionChangeLogs.java
+++ app/src/main/java/com/anysoftkeyboard/ui/tutorials/VersionChangeLogs.java
@@ -16,7 +16,7 @@ public class VersionChangeLogs {
         log.add(new VersionChangeLog(1, 9, "r1", Uri.parse("https://github.com/AnySoftKeyboard/AnySoftKeyboard/milestone/83"),
                 "New Settings UI bottom navigation.",
                 "Also, new Setup Wizard UI.",
-                "And many new settings al around.",
+                "And many new settings all around.",
                 "Localization update: AR, BE, pt-rBR, ES, EU, FR, FIL, HU, LT, MY, SC, TR, RU, UK.",
                 "YABTU and also will only work with Android 2.2 \uD83C\uDF6A or newer."));
 
