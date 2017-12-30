9ff9eccab45c307fed1c0c2a45802bde0e40a485
Menny Even Danan <menny@evendanan.net>
2017-12-29 21:45:23 -0500

    Grouping Config annotated test together

--- app/src/test/java/com/anysoftkeyboard/AnySoftKeyboardTestRunner.java
+++ app/src/test/java/com/anysoftkeyboard/AnySoftKeyboardTestRunner.java
@@ -1,9 +1,13 @@
 package com.anysoftkeyboard;
 
+import net.evendanan.testgrouping.HashingStrategy;
+import net.evendanan.testgrouping.TestClassHashingStrategy;
 import net.evendanan.testgrouping.TestsGroupingFilter;
 
+import org.junit.runner.Description;
 import org.junit.runners.model.InitializationError;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
 /**
  * Just a way to add general things on-top RobolectricTestRunner.
@@ -12,6 +16,23 @@ import org.robolectric.RobolectricTestRunner;
 public class AnySoftKeyboardTestRunner extends RobolectricTestRunner {
     public AnySoftKeyboardTestRunner(Class<?> testClass) throws InitializationError {
         super(testClass);
-        TestsGroupingFilter.addTestsGroupingFilterWithSystemPropertiesData(this, false/*so running from AS will work*/);
+        TestsGroupingFilter.addTestsGroupingFilterWithSystemPropertiesData(this, new TestWeightHashingStrategy(), false/*so running from AS will work*/);
+    }
+
+    private static class TestWeightHashingStrategy implements HashingStrategy {
+        private final TestClassHashingStrategy mFallbackHashing = new TestClassHashingStrategy();
+
+        @Override
+        public int calculateHashFromDescription(Description description, int groupsCount) {
+            if (groupsCount < 2) {
+                return 0;
+            }
+
+            if (description.getAnnotation(Config.class) != null || description.getTestClass().getAnnotation(Config.class) != null) {
+                return groupsCount - 1;
+            } else {
+                return mFallbackHashing.calculateHashFromDescription(description, groupsCount - 1);
+            }
+        }
     }
 }
