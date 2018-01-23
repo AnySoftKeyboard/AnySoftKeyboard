895ed6d2a5135fc3ed46c61d824604945487866c
Menny Even Danan <menny@evendanan.net>
2018-01-23 15:02:35 -0500

    Enabling large machine for CI

--- scripts/ci/ci_check.sh
+++ scripts/ci/ci_check.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
 
-./gradlew --stacktrace lintDebug
-./gradlew --stacktrace checkstyleMain
-./gradlew --stacktrace checkDebug
-#Not running findbugs in CI, since it takes too much memory and ends the process with exit-code 137
-if [ "${CI}" == "" ]; then
-    ./gradlew findbugsMain
-fi
+./gradlew --stacktrace lintDebug findbugsMain checkstyleMain checkDebug
--- scripts/ci/ci_test.sh
+++ scripts/ci/ci_test.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 
-GRADLE_ARGS=""
-if [ "${CI}" == "true" ]; then
-    GRADLE_ARGS="-DmaxTestForks=${TEST_FORKS}"
-fi
-
-./gradlew --stacktrace ${GRADLE_ARGS} testDebugUnitTest testDebugUnitTestCoverage
+./gradlew --stacktrace testDebugUnitTest testDebugUnitTestCoverage
