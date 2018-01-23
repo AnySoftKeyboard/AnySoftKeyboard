d4470c5a2bdbd7f2301a18d004715e120cfa2d7e
Menny Even Danan <menny@evendanan.net>
2018-01-23 13:51:54 -0500

    Using GRADLE_OPT where we can

--- scripts/ci/ci_setup.sh
+++ scripts/ci/ci_setup.sh
@@ -14,4 +14,4 @@ rm local.properties || true
 echo -e "sdk.dir=${ANDROID_HOME}\nndk.dir=${ANDROID_NDK}" > local.properties
 
 #workaround for plugin error https://code.google.com/p/android/issues/detail?id=212309
-./gradlew --no-daemon --stacktrace dependencies || true
+./gradlew --stacktrace dependencies || true
--- scripts/ci/ci_test.sh
+++ scripts/ci/ci_test.sh
@@ -2,7 +2,7 @@
 
 GRADLE_ARGS=""
 if [ "${CI}" == "true" ]; then
-    GRADLE_ARGS="--no-daemon --stacktrace --max-workers=2 -DmaxTestForks=${TEST_FORKS}"
+    GRADLE_ARGS="-DmaxTestForks=${TEST_FORKS}"
 fi
 
-./gradlew ${GRADLE_ARGS} testDebugUnitTest testDebugUnitTestCoverage
+./gradlew --stacktrace ${GRADLE_ARGS} testDebugUnitTest testDebugUnitTestCoverage
