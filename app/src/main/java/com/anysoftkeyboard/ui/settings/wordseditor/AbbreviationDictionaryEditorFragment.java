c0ce111a0a2cf0766669343fa41faf47e65dbf6a
Menny Even Danan <menny@evendanan.net>
2018-01-29 13:15:31 -0500

    Allowing backup and restore of abbr dict

--- app/src/main/java/com/anysoftkeyboard/ui/settings/wordseditor/AbbreviationDictionaryEditorFragment.java
+++ app/src/main/java/com/anysoftkeyboard/ui/settings/wordseditor/AbbreviationDictionaryEditorFragment.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.util.Pair;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,18 +14,85 @@ import android.widget.TextView;
 
 import com.anysoftkeyboard.dictionaries.EditableDictionary;
 import com.anysoftkeyboard.dictionaries.sqlite.AbbreviationsDictionary;
+import com.anysoftkeyboard.dictionaries.sqlite.WordsSQLiteConnectionPrefsProvider;
+import com.anysoftkeyboard.prefs.backup.PrefsRoot;
+import com.anysoftkeyboard.prefs.backup.PrefsXmlStorage;
+import com.anysoftkeyboard.rx.RxSchedulers;
 import com.anysoftkeyboard.ui.settings.MainSettingsActivity;
+import com.menny.android.anysoftkeyboard.AnyApplication;
 import com.menny.android.anysoftkeyboard.R;
+
+import net.evendanan.pushingpixels.RxProgressDialog;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import io.reactivex.disposables.CompositeDisposable;
+
 public class AbbreviationDictionaryEditorFragment extends UserDictionaryEditorFragment {
+
+    private static final String ASK_ABBR_WORDS_SDCARD_FILENAME = "AbbrUserWords.xml";
+
+    @NonNull
+    private CompositeDisposable mDisposable = new CompositeDisposable();
 
     @Override
     public void onStart() {
         super.onStart();
         MainSettingsActivity.setActivityTitle(this, getString(R.string.abbreviation_dict_settings_titlebar));
+    }
+
+    @Override
+    public void onDestroy() {
+        mDisposable.dispose();
+        super.onDestroy();
+    }
+
+    @Override
+    protected void restoreFromStorage() {
+        //not calling base, since we have a different way of storing data
+        mDisposable.dispose();
+        mDisposable = new CompositeDisposable();
+
+        PrefsXmlStorage storage = new PrefsXmlStorage(AnyApplication.getBackupFile(ASK_ABBR_WORDS_SDCARD_FILENAME));
+        WordsSQLiteConnectionPrefsProvider provider = new WordsSQLiteConnectionPrefsProvider(getContext(), AbbreviationsDictionary.ABBREVIATIONS_DB);
+
+        mDisposable.add(RxProgressDialog.create(Pair.create(storage, provider), getActivity())
+                .subscribeOn(RxSchedulers.background())
+                .map(pair -> {
+                    final PrefsRoot prefsRoot = pair.first.load();
+                    pair.second.storePrefsRoot(prefsRoot);
+                    return Boolean.TRUE;
+                })
+                .observeOn(RxSchedulers.mainThread())
+                .subscribe(
+                        o -> mDialogController.showDialog(UserDictionaryEditorFragment.DIALOG_LOAD_SUCCESS),
+                        throwable -> mDialogController.showDialog(UserDictionaryEditorFragment.DIALOG_LOAD_FAILED, throwable.getMessage()),
+                        this::fillWordsList));
+    }
+
+    @Override
+    protected void backupToStorage() {
+        //not calling base, since we have a different way of storing data
+        mDisposable.dispose();
+        mDisposable = new CompositeDisposable();
+
+        PrefsXmlStorage storage = new PrefsXmlStorage(AnyApplication.getBackupFile(ASK_ABBR_WORDS_SDCARD_FILENAME));
+        WordsSQLiteConnectionPrefsProvider provider = new WordsSQLiteConnectionPrefsProvider(getContext(), AbbreviationsDictionary.ABBREVIATIONS_DB);
+
+        mDisposable.add(RxProgressDialog.create(Pair.create(storage, provider), getActivity())
+                .subscribeOn(RxSchedulers.background())
+                .map(pair -> {
+                    final PrefsRoot prefsRoot = pair.second.getPrefsRoot();
+                    pair.first.store(prefsRoot);
+
+                    return Boolean.TRUE;
+                })
+                .observeOn(RxSchedulers.mainThread())
+                .subscribe(
+                        o -> mDialogController.showDialog(UserDictionaryEditorFragment.DIALOG_SAVE_SUCCESS),
+                        throwable -> mDialogController.showDialog(UserDictionaryEditorFragment.DIALOG_SAVE_FAILED, throwable.getMessage()),
+                        this::fillWordsList));
     }
 
     @Override
@@ -67,13 +135,13 @@ public class AbbreviationDictionaryEditorFragment extends UserDictionaryEditorFr
         @Override
         protected void bindEditingWordViewText(EditText wordView, LoadedWord editorWord) {
             wordView.setText(getAbbreviation(editorWord));
-            EditText explodedSentence = (EditText) ((View) wordView.getParent()).findViewById(R.id.word_target_view);
+            EditText explodedSentence = ((View) wordView.getParent()).findViewById(R.id.word_target_view);
             explodedSentence.setText(getExplodedSentence(editorWord));
         }
 
         @Override
         protected LoadedWord createNewEditorWord(EditText wordView, LoadedWord oldEditorWord) {
-            EditText explodedSentenceView = (EditText) ((View) wordView.getParent()).findViewById(R.id.word_target_view);
+            EditText explodedSentenceView = ((View) wordView.getParent()).findViewById(R.id.word_target_view);
             final String newAbbreviation = wordView.getText().toString();
             final String newExplodedSentence = explodedSentenceView.getText().toString();
             if (TextUtils.isEmpty(newAbbreviation) || TextUtils.isEmpty(newExplodedSentence)) {
