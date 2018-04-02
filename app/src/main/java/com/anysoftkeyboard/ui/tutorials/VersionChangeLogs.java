2a69f14a41ba019057a0bb51fd0bd75d05a170e9
Menny Even Danan <menny@evendanan.net>
2018-04-02 13:13:00 -0400

    Update localization and remove NO in favor of NB

--- app/src/main/java/com/anysoftkeyboard/ui/tutorials/VersionChangeLogs.java
+++ app/src/main/java/com/anysoftkeyboard/ui/tutorials/VersionChangeLogs.java
@@ -18,7 +18,7 @@ public class VersionChangeLogs {
                 "New Kaomoji emoji group.",
                 "Pressing shift will change caps of selected text.",
                 "A few bug fixes.",
-                "Updated localization: AR, BE, BG, DU, EU, FI, FIL, FR, HU, KA, KMR, KU, LT, NO, PT-rBR, RU, SC, TR, UK."));
+                "Updated localization: AR, BE, BG, DU, EU, FI, FIL, FR, HU, KA, KMR, KU, LT, NB, PT-rBR, RU, SC, TR, UK."));
 
         log.add(new VersionChangeLog(1, 9, "r1", Uri.parse("https://github.com/AnySoftKeyboard/AnySoftKeyboard/milestone/83"),
                 "New Settings UI bottom navigation.",
