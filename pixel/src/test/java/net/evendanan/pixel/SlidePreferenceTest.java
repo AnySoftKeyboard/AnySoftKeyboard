69ddadb3915e536a0fb5713043a0679d326c4c2e
Menny Even Danan <menny@evendanan.net>
2018-05-31 20:03:03 -0400

    Demo sound-effect custom volume and vibration

--- pixel/src/test/java/net/evendanan/pixel/SlidePreferenceTest.java
+++ pixel/src/test/java/net/evendanan/pixel/SlidePreferenceTest.java
@@ -3,7 +3,6 @@ package net.evendanan.pixel;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.preference.PreferenceManager;
 import android.widget.SeekBar;
