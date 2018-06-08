d9c715e1448f779d77ad9ba3c911f520de49c244
Menny Even Danan <menny@evendanan.net>
2018-06-08 15:14:27 -0400

    Observing device Power-Saver state

--- app/src/main/java/com/anysoftkeyboard/powersave/PowerSaving.java
+++ app/src/main/java/com/anysoftkeyboard/powersave/PowerSaving.java
@@ -1,9 +1,13 @@
 package com.anysoftkeyboard.powersave;
 
+import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Build;
+import android.os.PowerManager;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 
 import com.github.karczews.rxbroadcastreceiver.RxBroadcastReceivers;
 import com.menny.android.anysoftkeyboard.AnyApplication;
@@ -16,21 +20,40 @@ public class PowerSaving {
     public static Observable<Boolean> observePowerSavingState(@NonNull Context context) {
         return Observable.combineLatest(
                 AnyApplication.prefs(context).getString(R.string.settings_key_power_save_mode, R.string.settings_default_power_save_mode_value).asObservable(),
-                RxBroadcastReceivers.fromIntentFilter(context.getApplicationContext(), getPowerSavingIntentFilter()),
-                (powerSavingPref, batteryIntent) -> {
+                RxBroadcastReceivers.fromIntentFilter(context.getApplicationContext(), getBatteryStateIntentFilter()).startWith(new Intent(Intent.ACTION_BATTERY_OKAY)),
+                getOsPowerSavingStateObservable(context),
+                (powerSavingPref, batteryIntent, osPowerSavingState) -> {
                     switch (powerSavingPref) {
                         case "never":
                             return false;
                         case "always":
                             return true;
                         default:
-                            return Intent.ACTION_BATTERY_LOW.equals(batteryIntent.getAction());
+                            return osPowerSavingState || Intent.ACTION_BATTERY_LOW.equals(batteryIntent.getAction());
                     }
-                }).startWith(false);
+                });
     }
 
+    private static Observable<Boolean> getOsPowerSavingStateObservable(Context context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            final PowerManager powerManager = (PowerManager) context.getSystemService(Service.POWER_SERVICE);
+            return RxBroadcastReceivers.fromIntentFilter(context, getPowerSavingIntentFilter())
+                    .map(i -> powerManager.isPowerSaveMode())
+                    .startWith(false);
+        } else {
+            return Observable.just(false);
+        }
+    }
 
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     private static IntentFilter getPowerSavingIntentFilter() {
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(PowerManager.ACTION_POWER_SAVE_MODE_CHANGED);
+
+        return filter;
+    }
+
+    private static IntentFilter getBatteryStateIntentFilter() {
         IntentFilter filter = new IntentFilter();
         filter.addAction(Intent.ACTION_BATTERY_LOW);
         filter.addAction(Intent.ACTION_BATTERY_OKAY);
