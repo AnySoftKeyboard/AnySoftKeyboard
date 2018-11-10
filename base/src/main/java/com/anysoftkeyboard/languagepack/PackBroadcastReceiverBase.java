b721e7f18aa12a60aeb4543757d29b6a0006a12e
Menny Even Danan <menny@evendanan.net>
2018-11-10 17:39:47 -0500

    Initial mono-repo
    Building English dictionary

---
+++ base/src/main/java/com/anysoftkeyboard/languagepack/PackBroadcastReceiverBase.java
@@ -0,0 +1,14 @@
+package com.anysoftkeyboard.languagepack;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class PackBroadcastReceiverBase extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context arg0, Intent arg1) {
+
+    }
+
+}
---
+++ english/pack/src/main/java/com/anysoftkeyboard/languagepack/english/PackBroadcastReceiver.java
@@ -0,0 +1,7 @@
+package com.anysoftkeyboard.languagepack.english;
+
+import com.anysoftkeyboard.languagepack.PackBroadcastReceiverBase;
+
+public class PackBroadcastReceiver extends PackBroadcastReceiverBase {
+
+}
