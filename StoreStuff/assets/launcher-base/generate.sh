8c8cc04558ef7ad1c7c8138b5db9351567fc9a4c
Menny Even Danan <menny@evendanan.net>
2018-11-12 22:52:21 -0500

    Support for generating language-pack icon

---
+++ StoreStuff/assets/launcher-base/generate.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+PROJECT_DIR=$1
+BUILD_DIR=$2
+DIMEN=$3
+HEIGHT=$4
+OFFSET=$5
+ASSETS_DIR=$6
+
+mkdir -p ${BUILD_DIR}/image_temp
+mkdir -p ${PROJECT_DIR}/src/main/res/mipmap-${DIMEN}
+convert ${PROJECT_DIR}/flag/flag.png -adaptive-resize x${HEIGHT} ${BUILD_DIR}/image_temp/flag-${DIMEN}.png
+convert ${ASSETS_DIR}/${DIMEN}.png ${BUILD_DIR}/image_temp/flag-${DIMEN}.png -gravity south -geometry +0+${OFFSET} -composite ${PROJECT_DIR}/src/main/res/mipmap-${DIMEN}/ic_launcher.png
---
+++ StoreStuff/assets/logo-base/generate.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+PROJECT_DIR=$1
+BUILD_DIR=$2
+ASSETS_DIR=$3
+
+mkdir -p ${BUILD_DIR}/image_web_temp
+mkdir -p ${PROJECT_DIR}/store_logo
+convert ${PROJECT_DIR}/flag/flag.png -adaptive-resize x128 ${BUILD_DIR}/image_web_temp/flag.png
+convert ${ASSETS_DIR}/web.png ${BUILD_DIR}/image_web_temp/flag.png -gravity south -geometry +0+64 -composite ${PROJECT_DIR}/store_logo/logo.png
