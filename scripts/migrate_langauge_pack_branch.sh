18b9127ee4c680704788ec6b941db51d61840bea
Menny Even-Danan <menny@spotify.com>
2018-11-15 16:43:01 -0500

    Moving language-packs to :languages: subfolder

--- scripts/migrate_langauge_pack_branch.sh
+++ scripts/migrate_langauge_pack_branch.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 BRACNH_TO_MIGRATE=$1
-TARGET_FOLDER=`echo ${1} | tr '[:upper:]' '[:lower:]'`
+TARGET_MODULE=`echo ${1} | tr '[:upper:]' '[:lower:]'`
+TARGET_FOLDER="languages/${TARGET_MODULE}"
 COPYBARA_BIN=$2
 TEMP_DIR=${TMPDIR}/LANGUAGE_PACK_MIGRATE/${BRACNH_TO_MIGRATE}
 
@@ -23,9 +24,9 @@ echo "${file_contents//REPLACEMENT_BRANCH_TO_MIGRATE/$BRACNH_TO_MIGRATE}" > ${TE
 
 ${COPYBARA_BIN} --folder-dir ${TEMP_DIR}/files/ ${TEMP_DIR}/copy.bara.sky
 
-mv ${TEMP_DIR}/files/${TARGET_FOLDER} .
+mv ${TEMP_DIR}/files/${TARGET_MODULE} languages/
 
-echo "include ':${TARGET_FOLDER}', ':${TARGET_FOLDER}:pack', ':${TARGET_FOLDER}:apk'" >> settings.gradle
+echo "include ':languages:${TARGET_MODULE}:pack', ':languages:${TARGET_MODULE}:apk'" >> settings.gradle
 
 mv ${TARGET_FOLDER}/build.gradle ${TARGET_FOLDER}/build.gradle.old
 echo "// Language pack ${BRACNH_TO_MIGRATE}" > ${TARGET_FOLDER}/build.gradle
@@ -34,7 +35,7 @@ mkdir ${TARGET_FOLDER}/apk
 echo "apply plugin: 'com.android.application'" > ${TARGET_FOLDER}/apk/build.gradle
 echo "ext.shouldBePublished = false" >> ${TARGET_FOLDER}/apk/build.gradle
 echo "dependencies {" >> ${TARGET_FOLDER}/apk/build.gradle
-echo "    implementation project(path: ':${TARGET_FOLDER}:pack')" >> ${TARGET_FOLDER}/apk/build.gradle
+echo "    implementation project(path: ':languages:${TARGET_MODULE}:pack')" >> ${TARGET_FOLDER}/apk/build.gradle
 echo "}" >> ${TARGET_FOLDER}/apk/build.gradle
 
 mkdir ${TARGET_FOLDER}/pack
@@ -43,5 +44,5 @@ echo "ext.status_icon_text = 'en'" >> ${TARGET_FOLDER}/pack/build.gradle
 echo "ext.dictionaryInputPossibleCharacters = null//"".toCharArray()" >> ${TARGET_FOLDER}/pack/build.gradle
 echo "ext.dictionaryInputAdditionalInnerCharacters = null//"".toCharArray()" >> ${TARGET_FOLDER}/pack/build.gradle
 echo "dependencies {" >> ${TARGET_FOLDER}/pack/build.gradle
-echo "    implementation project(path: ':base')" >> ${TARGET_FOLDER}/pack/build.gradle
+echo "    implementation project(path: ':languages:base')" >> ${TARGET_FOLDER}/pack/build.gradle
 echo "}" >> ${TARGET_FOLDER}/pack/build.gradle
