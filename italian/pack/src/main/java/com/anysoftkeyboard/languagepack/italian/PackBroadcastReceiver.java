e17808d5b4fa7b3030e84ab72173d8636a363885
Menny Even-Danan <menny@spotify.com>
2018-11-15 15:52:46 -0500

    Migrating Italian pack

---
+++ italian/pack/src/main/java/com/anysoftkeyboard/languagepack/italian/PackBroadcastReceiver.java
@@ -0,0 +1,7 @@
+package com.anysoftkeyboard.languagepack.italian;
+
+import com.anysoftkeyboard.languagepack.PackBroadcastReceiverBase;
+
+public class PackBroadcastReceiver extends PackBroadcastReceiverBase {
+
+}
