1648e132e0cbd7257d715e36f35de54a8f6aff4a
Menny Even-Danan <menny@spotify.com>
2018-11-15 13:39:44 -0500

    Migrating Afrikaans to mono-repo

---
+++ afrikaans/pack/src/main/java/com/anysoftkeyboard/languagepack/afrikaans/PackBroadcastReceiver.java
@@ -0,0 +1,7 @@
+package com.anysoftkeyboard.languagepack.afrikaans;
+
+import com.anysoftkeyboard.languagepack.PackBroadcastReceiverBase;
+
+public class PackBroadcastReceiver extends PackBroadcastReceiverBase {
+
+}
