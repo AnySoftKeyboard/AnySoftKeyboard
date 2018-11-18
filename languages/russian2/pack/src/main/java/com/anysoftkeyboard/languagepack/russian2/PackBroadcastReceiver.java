c3c6bf526017cda966349359e952975d44a9c25e
Menny Even-Danan <menny@evendanan.net>
2018-11-17 21:41:03 -0500

    Migrating Russian branch

---
+++ languages/russian2/pack/src/main/java/com/anysoftkeyboard/languagepack/russian2/PackBroadcastReceiver.java
@@ -0,0 +1,7 @@
+package com.anysoftkeyboard.languagepack.russian2;
+
+import com.anysoftkeyboard.languagepack.PackBroadcastReceiverBase;
+
+public class PackBroadcastReceiver extends PackBroadcastReceiverBase {
+
+}
