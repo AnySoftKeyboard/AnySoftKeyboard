a061e133f7dbe2275a2643eb35c56dc1f72c520c
Menny Even-Danan <menny@evendanan.net>
2018-11-27 15:49:34 -0500

    Removing long-press tips. They are not needed anymore

--- app/src/main/java/com/anysoftkeyboard/ime/AnySoftKeyboardClipboard.java
+++ app/src/main/java/com/anysoftkeyboard/ime/AnySoftKeyboardClipboard.java
@@ -11,24 +11,17 @@ import android.view.inputmethod.InputConnection;
 import com.anysoftkeyboard.api.KeyCodes;
 import com.anysoftkeyboard.devicespecific.Clipboard;
 import com.anysoftkeyboard.keyboards.Keyboard;
-import com.f2prateek.rx.preferences2.Preference;
 import com.menny.android.anysoftkeyboard.AnyApplication;
 import com.menny.android.anysoftkeyboard.R;
 
 public abstract class AnySoftKeyboardClipboard extends AnySoftKeyboardSwipeListener {
 
-    private static final int MAX_TIMES_TO_SHOW_LONG_PRESS_TIP = 5;
-
     private boolean mArrowSelectionState;
-    private Preference<Integer> mLongPressPref;
-    private Preference<Integer> mFineSelectTipPref;
     private Clipboard mClipboard;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        mLongPressPref = prefs().getInteger(R.string.settings_key_clipboard_tip_for_long_press, R.integer.settings_default_zero_value);
-        mFineSelectTipPref = prefs().getInteger(R.string.settings_key_clipboard_tip_for_fine_select, R.integer.settings_default_zero_value);
         mClipboard = AnyApplication.getDeviceSpecific().createClipboard(getApplicationContext());
     }
 
@@ -66,15 +59,7 @@ public abstract class AnySoftKeyboardClipboard extends AnySoftKeyboardSwipeListe
                             sendDownUpKeyEvents(KeyEvent.KEYCODE_DEL);
                         } else {
                             //showing toast, since there isn't any other UI feedback
-                            final int toastTextToShow;
-                            final int timesTipShown = mLongPressPref.get();
-                            if (timesTipShown < MAX_TIMES_TO_SHOW_LONG_PRESS_TIP) {
-                                toastTextToShow = R.string.clipboard_copy_done_toast_with_long_press_tip;
-                                mLongPressPref.set(timesTipShown + 1);
-                            } else {
-                                toastTextToShow = R.string.clipboard_copy_done_toast;
-                            }
-                            showToastMessage(toastTextToShow, true);
+                            showToastMessage(R.string.clipboard_copy_done_toast, true);
                         }
                     }
                 }
@@ -93,10 +78,8 @@ public abstract class AnySoftKeyboardClipboard extends AnySoftKeyboardSwipeListe
                 break;
             case KeyCodes.CLIPBOARD_SELECT:
                 mArrowSelectionState = !mArrowSelectionState;
-                final int timesTipShown = mFineSelectTipPref.get();
-                if (mArrowSelectionState && timesTipShown < MAX_TIMES_TO_SHOW_LONG_PRESS_TIP) {
+                if (mArrowSelectionState) {
                     showToastMessage(R.string.clipboard_fine_select_enabled_toast, true);
-                    mFineSelectTipPref.set(timesTipShown + 1);
                 }
                 break;
             case KeyCodes.UNDO:
