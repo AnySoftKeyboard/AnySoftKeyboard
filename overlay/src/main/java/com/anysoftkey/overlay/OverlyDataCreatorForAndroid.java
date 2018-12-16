0875b69992b67adede864276bd7509ccfc9fb4ea
Menny Even Danan <menny@evendanan.net>
2018-12-15 21:34:12 -0500

    Overlay creator that can normalizer dark color
    This will be used in cases where the remote app did not provide
    a dark-primary

--- overlay/src/main/java/com/anysoftkey/overlay/OverlyDataCreatorForAndroid.java
+++ overlay/src/main/java/com/anysoftkey/overlay/OverlyDataCreatorForAndroid.java
@@ -52,20 +52,20 @@ public class OverlyDataCreatorForAndroid implements OverlyDataCreator {
         TypedArray a = context.obtainStyledAttributes(themeId, APP_COLORS_ATTRS);
         try {
             final Resources resources = context.getResources();
-            data.setPrimaryColor(getColorFromStyledAttribute(a, 0, resources));
-            data.setPrimaryDarkColor(getColorFromStyledAttribute(a, 1, resources));
-            data.setPrimaryTextColor(getColorFromStyledAttribute(a, 2, resources));
+            data.setPrimaryColor(getColorFromStyledAttribute(a, 0, resources, 0));
+            data.setPrimaryDarkColor(getColorFromStyledAttribute(a, 1, resources, data.getPrimaryColor()));
+            data.setPrimaryTextColor(getColorFromStyledAttribute(a, 2, resources, 0));
         } finally {
             a.recycle();
         }
     }
 
-    private static int getColorFromStyledAttribute(TypedArray a, int index, Resources resources) {
+    private static int getColorFromStyledAttribute(TypedArray a, int index, Resources resources, int defaultColor) {
         final TypedValue typedValue = a.peekValue(index);
         if (typedValue.type == TypedValue.TYPE_REFERENCE) {
             return resources.getColor(typedValue.resourceId);
         } else {
-            return typedValue.data;
+            return a.getColor(index, defaultColor);
         }
     }
 
