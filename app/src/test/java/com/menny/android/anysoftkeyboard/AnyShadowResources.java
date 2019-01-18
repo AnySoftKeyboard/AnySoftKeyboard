e112213d27731b264eae56a3636e001a6f2df43b
Menny Even Danan <menny@evendanan.net>
2019-01-18 13:10:06 -0500

    Removing unneeded shadow

--- app/src/test/java/com/menny/android/anysoftkeyboard/AnyShadowResources.java
+++ app/src/test/java/com/menny/android/anysoftkeyboard/AnyShadowResources.java
@@ -1,24 +1,24 @@
-package com.menny.android.anysoftkeyboard;
-
-import android.content.res.Resources;
-import android.graphics.drawable.Drawable;
-import android.util.TypedValue;
-
-import org.mockito.Mockito;
-import org.robolectric.annotation.Implementation;
-import org.robolectric.annotation.Implements;
-import org.robolectric.shadows.ShadowResources;
-
-@Implements(value = Resources.class)
-public class AnyShadowResources extends ShadowResources {
-
-    @Implementation
-    @Override
-    public Drawable loadDrawable(TypedValue value, int id, Resources.Theme theme) throws Resources.NotFoundException {
-        if (id == R.mipmap.ic_launcher) {
-            return Mockito.mock(Drawable.class);
-        } else {
-            return super.loadDrawable(value, id, theme);
-        }
-    }
-}
+//package com.menny.android.anysoftkeyboard;
+//
+//import android.content.res.Resources;
+//import android.graphics.drawable.Drawable;
+//import android.util.TypedValue;
+//
+//import org.mockito.Mockito;
+//import org.robolectric.annotation.Implementation;
+//import org.robolectric.annotation.Implements;
+//import org.robolectric.shadows.ShadowResources;
+//
+//@Implements(value = Resources.class)
+//public class AnyShadowResources extends ShadowResources {
+//
+//    @Implementation
+//    @Override
+//    public Drawable loadDrawable(TypedValue value, int id, Resources.Theme theme) throws Resources.NotFoundException {
+//        if (id == R.mipmap.ic_launcher) {
+//            return Mockito.mock(Drawable.class);
+//        } else {
+//            return super.loadDrawable(value, id, theme);
+//        }
+//    }
+//}
