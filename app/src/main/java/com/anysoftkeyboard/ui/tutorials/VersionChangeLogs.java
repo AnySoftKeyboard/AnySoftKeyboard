b4dfe8669ed21c03ec79841aa0add40380c894a3
Menny Even Danan <menny@evendanan.net>
2019-02-04 14:49:52 -0500

    Localization update

--- app/src/main/java/com/anysoftkeyboard/ui/tutorials/VersionChangeLogs.java
+++ app/src/main/java/com/anysoftkeyboard/ui/tutorials/VersionChangeLogs.java
@@ -16,7 +16,8 @@ public class VersionChangeLogs {
         log.add(new VersionChangeLog(1, 10, "r1", Uri.parse("https://github.com/AnySoftKeyboard/AnySoftKeyboard/milestone/89"),
                 "Keyboard colors will adapt to used app. Enable this in Themes settings.",
                 "Updated build-tools (smaller binary now).",
-                "Updated localization: AR, BE, DE, EO, EU, ES, ES-MX, FR, IT, KMR, KU, PT, SC."));
+                "Halmak layout for English.",
+                "Updated localization: AR, BE, BG, DE, EO, EU, ES, ES-MX, FR, IT, KMR, KU, PT, SC, SK, TR."));
 
         log.add(new VersionChangeLog(1, 9, "r6", Uri.parse("https://github.com/AnySoftKeyboard/AnySoftKeyboard/milestone/88"),
                 "➿ Gesture-Typing is here! As BETA. You'll need to enable it in Settings if you want to try it out.",
