ab42b46b39e81ff8535cb66f5091f973ec0305d4
Lauri Lähteinen <lauri.lahteinen@gmail.com>
2019-02-04 19:04:52 +0200

    Finnish: Initial migration to mono-repo.

---
+++ languages/finnish/pack/src/main/java/com/anysoftkeyboard/languagepack/finnish/PackBroadcastReceiver.java
@@ -0,0 +1,7 @@
+package com.anysoftkeyboard.languagepack.finnish;
+
+import com.anysoftkeyboard.languagepack.PackBroadcastReceiverBase;
+
+public class PackBroadcastReceiver extends PackBroadcastReceiverBase {
+
+}
