343b82d09d1c36a139a91dffbecb23e92ca59e6d
Menny Even Danan <menny@evendanan.net>
2019-02-05 21:34:30 -0500

    Night-Mode observable

--- app/src/main/java/com/anysoftkeyboard/ui/settings/NightModeSettingsFragment.java
+++ app/src/main/java/com/anysoftkeyboard/ui/settings/NightModeSettingsFragment.java
@@ -17,11 +17,19 @@
 package com.anysoftkeyboard.ui.settings;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.preference.PreferenceFragmentCompat;
 
+import com.anysoftkeyboard.android.NightMode;
+import com.anysoftkeyboard.rx.GenericOnError;
 import com.menny.android.anysoftkeyboard.R;
 
+import io.reactivex.disposables.Disposable;
+import io.reactivex.disposables.Disposables;
+
 public class NightModeSettingsFragment extends PreferenceFragmentCompat {
+
+    private Disposable mAppNightModeDisposable = Disposables.empty();
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
@@ -32,5 +40,13 @@ public class NightModeSettingsFragment extends PreferenceFragmentCompat {
     public void onStart() {
         super.onStart();
         MainSettingsActivity.setActivityTitle(this, getString(R.string.night_mode_screen));
+        mAppNightModeDisposable = NightMode.observeNightModeState(getContext(), R.string.settings_key_night_mode_app_theme_control, R.bool.settings_default_true)
+                .subscribe(enabled -> ((AppCompatActivity) getActivity()).getDelegate().applyDayNight(), GenericOnError.onError("NightModeSettingsFragment"));
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mAppNightModeDisposable.dispose();
     }
 }
