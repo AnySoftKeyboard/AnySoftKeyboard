ab3bc6186c566834b14481b03e3fa2fe52218e9c
Menny Even Danan <menny@evendanan.net>
2019-02-05 22:33:00 -0500

    Night-mode theme

--- app/src/main/java/com/anysoftkeyboard/ime/AnySoftKeyboardPowerSaving.java
+++ app/src/main/java/com/anysoftkeyboard/ime/AnySoftKeyboardPowerSaving.java
@@ -4,30 +4,20 @@ import android.content.ComponentName;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
-import android.support.annotation.VisibleForTesting;
 import android.support.v4.content.ContextCompat;
 import android.view.inputmethod.EditorInfo;
 
+import com.anysoftkeyboard.android.PowerSaving;
 import com.anysoftkeyboard.overlay.OverlayData;
 import com.anysoftkeyboard.overlay.OverlyDataCreator;
-import com.anysoftkeyboard.android.PowerSaving;
 import com.anysoftkeyboard.rx.GenericOnError;
 import com.menny.android.anysoftkeyboard.R;
 
 import java.util.List;
 
-public abstract class AnySoftKeyboardPowerSaving extends AnySoftKeyboardThemeOverlay {
-
-    private static final OverlayData POWER_SAVING_OVERLAY = new OverlayData(
-            Color.BLACK,
-            Color.BLACK,
-            Color.DKGRAY,
-            Color.GRAY,
-            Color.DKGRAY
-    );
-
+public abstract class AnySoftKeyboardPowerSaving extends AnySoftKeyboardNightMode {
     private boolean mPowerState;
-    private boolean mThemeInPowerSaving;
+    private ToggleOverlayCreator mToggleOverlayCreator;
 
     @Override
     public void onCreate() {
@@ -42,14 +32,7 @@ public abstract class AnySoftKeyboardPowerSaving extends AnySoftKeyboardThemeOve
         ));
 
         addDisposable(PowerSaving.observePowerSavingState(getApplicationContext(), R.string.settings_key_power_save_mode_theme_control, R.bool.settings_default_true)
-                .subscribe(themePowerState -> {
-                    mThemeInPowerSaving = themePowerState;
-                    final EditorInfo currentInputEditorInfo = getCurrentInputEditorInfo();
-                    if (currentInputEditorInfo != null) {
-                        applyThemeOverlay(currentInputEditorInfo);
-                                //mThemeInPowerSaving/*only forcing calculation in power-state, else let the system decide if to calculate or apply invalid*/);
-                    }
-                }, GenericOnError.onError("Power-Saving theme")));
+                .subscribe(mToggleOverlayCreator::setToggle, GenericOnError.onError("Power-Saving theme")));
     }
 
     @NonNull
@@ -64,21 +47,41 @@ public abstract class AnySoftKeyboardPowerSaving extends AnySoftKeyboardThemeOve
 
     @Override
     protected OverlyDataCreator createOverlayDataCreator() {
-        return new PowerSavingOverlayCreator(super.createOverlayDataCreator());
+        return mToggleOverlayCreator = new ToggleOverlayCreator(super.createOverlayDataCreator(), this,
+                new OverlayData(
+                        Color.BLACK,
+                        Color.BLACK,
+                        Color.DKGRAY,
+                        Color.GRAY,
+                        Color.DKGRAY
+                ));
     }
 
-    @VisibleForTesting
-    class PowerSavingOverlayCreator implements OverlyDataCreator {
+    static class ToggleOverlayCreator implements OverlyDataCreator {
         private final OverlyDataCreator mOriginalCreator;
+        private final OverlayData mOverrideData;
+        private final AnySoftKeyboardThemeOverlay mOverlayController;
+        private boolean mUseOverride;
 
-        private PowerSavingOverlayCreator(OverlyDataCreator originalCreator) {
+        ToggleOverlayCreator(OverlyDataCreator originalCreator, AnySoftKeyboardThemeOverlay overlayController, OverlayData overrideData) {
             mOriginalCreator = originalCreator;
+            mOverlayController = overlayController;
+            mOverrideData = overrideData;
+        }
+
+        void setToggle(boolean useOverride) {
+            mUseOverride = useOverride;
+
+            final EditorInfo currentInputEditorInfo = mOverlayController.getCurrentInputEditorInfo();
+            if (currentInputEditorInfo != null) {
+                mOverlayController.applyThemeOverlay(currentInputEditorInfo);
+            }
         }
 
         @Override
         public OverlayData createOverlayData(ComponentName remoteApp) {
-            if (mThemeInPowerSaving) {
-                return POWER_SAVING_OVERLAY;
+            if (mUseOverride) {
+                return mOverrideData;
             } else {
                 return mOriginalCreator.createOverlayData(remoteApp);
             }
