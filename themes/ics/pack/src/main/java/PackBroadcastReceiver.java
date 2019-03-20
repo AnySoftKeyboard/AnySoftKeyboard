a15284666ac933cdb931ac5e46f4b3653632e92c
Menny Even Danan <menny@evendanan.net>
2019-03-20 13:39:48 -0400

    Migrating themes

---
+++ themes/ics/pack/src/main/java/PackBroadcastReceiver.java
@@ -0,0 +1,6 @@
+package com.anysoftkeyboard.themes.ics.pack;
+
+import com.anysoftkeyboard.addon.PackBroadcastReceiverBase;
+
+public class PackBroadcastReceiver extends PackBroadcastReceiverBase {
+}
