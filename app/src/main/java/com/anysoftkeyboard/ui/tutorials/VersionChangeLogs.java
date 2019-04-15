6ba72a5a47810698c59881109f81fcc501f5abec
Menny Even Danan <menny@evendanan.net>
2019-04-15 12:02:58 -0400

    Inial support for external media-insertion providers

--- app/src/main/java/com/anysoftkeyboard/ui/tutorials/VersionChangeLogs.java
+++ app/src/main/java/com/anysoftkeyboard/ui/tutorials/VersionChangeLogs.java
@@ -15,6 +15,7 @@ public class VersionChangeLogs {
 
         log.add(new VersionChangeLog(1, 10, "r2", Uri.parse("https://github.com/AnySoftKeyboard/AnySoftKeyboard/milestone/90"),
                 "Support for image/gif insertion. This is an option in the Emoji popup.",
+                "Improvements for Gesture-Typing. Thanks to Philipp Fischbeck.",
                 "Updated localization: "));
 
         log.add(new VersionChangeLog(1, 10, "r1", Uri.parse("https://github.com/AnySoftKeyboard/AnySoftKeyboard/milestone/89"),
