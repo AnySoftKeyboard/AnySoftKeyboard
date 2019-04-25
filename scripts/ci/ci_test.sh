83f8bd7770193394167d68172f533d09d8f0fdc8
Menny Even Danan <menny@evendanan.net>
2019-04-25 15:41:56 -0400

    Ensuring check scripts fail on errors

--- scripts/ci/ci_test.sh
+++ scripts/ci/ci_test.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+set -e
 
 ./gradlew --stacktrace testDebugUnitTest testDebugUnitTestCoverage -PexcludeTestClasses="**/*AllSdkTest*"
--- scripts/ci/ci_test_heavy.sh
+++ scripts/ci/ci_test_heavy.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+set -e
 
 ./gradlew --stacktrace -DmaxTestForks=1 -DTEST_FORK_EVERY=1 :app:testDebugUnitTest --tests="*AllSdkTest*" :app:testDebugUnitTestCoverage
