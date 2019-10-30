e75129b5fd628e5292139ca1db66e1cb4137fe60
Menny Even Danan <menny@evendanan.net>
2019-10-30 15:32:19 -0400

    Deploy support with GitHub Actions

--- scripts/ci/ci_deploy.sh
+++ scripts/ci/ci_deploy.sh
@@ -1,41 +1,40 @@
 #!/usr/bin/env bash
 set -e
 
-## Assuming the following environment variables
-# BUILD_TYPE
-# KEYSTORE_FILE_URL
-# PUBLISH_CERT_FILE_URL
+RELEASE_BUILD=$1
+KEYSTORE_FILE_URL=$2
+PUBLISH_CERT_FILE_URL=$3
 
-if [[ "${BUILD_TYPE}" == "canary" ]]; then
+if [[ "${RELEASE_BUILD}" == "false" ]]; then
     echo "Deploy build-type CANARY from master."
     GRADLE_TASKS="-DdeployChannel=alpha assembleCanary publishCanary"
-elif [[ "${BUILD_TYPE}" == "release" ]]; then
+elif [[ "${RELEASE_BUILD}" == "true" ]]; then
     echo "Deploy build-type RELEASE from 'release-branch'."
     cp app/src/main/play/release-notes/en-US/alpha.txt app/src/main/play/release-notes/en-US/beta.txt
     GRADLE_TASKS="-DdeployChannel=beta assembleRelease publishRelease"
-elif [[ "${BUILD_TYPE}" == "dry-run-release" ]]; then
+elif [[ "${RELEASE_BUILD}" == "dry-run" ]]; then
     echo "Dry Run Deploy build-type RELEASE."
-    GRADLE_TASKS="-DdeployChannel=alpha assembleRelease"
+    GRADLE_TASKS="-DdeployChannel=alpha assembleRelease assembleCanary"
 else
-    echo "Invalid build type '${BUILD_TYPE}'. Can not deploy."
+    echo "Invalid build type '${RELEASE_BUILD}'. Can not deploy."
     exit 1
 fi
 
-# from this point, we fail with error when stuff missing, since we want to deploy.
+if [[ "${RELEASE_BUILD}" != "dry-run" ]]; then
+    echo "Downloading signature files..."
 
-if [[ -z "${KEYSTORE_FILE_URL}" ]]; then
-    echo "Could not find secure env variable KEYSTORE_FILE_URL. Can not deploy."
-    exit 1
+    if [[ -z "${KEYSTORE_FILE_URL}" ]]; then
+        echo "Could not find secure env variable KEYSTORE_FILE_URL. Can not deploy."
+        exit 1
+    fi
+
+    if [[ -z "${PUBLISH_CERT_FILE_URL}" ]]; then
+        echo "Could not find secure env variable PUBLISH_CERT_FILE_URL. Can not deploy."
+        exit 1
+    fi
+
+    wget ${KEYSTORE_FILE_URL} -q -O /tmp/anysoftkeyboard.keystore
+    wget ${PUBLISH_CERT_FILE_URL} -q -O /tmp/apk_upload_key.p12
 fi
 
-if [[ -z "${PUBLISH_CERT_FILE_URL}" ]]; then
-    echo "Could not find secure env variable PUBLISH_CERT_FILE_URL. Can not deploy."
-    exit 1
-fi
-
-echo "Downloading signature files..."
-wget ${KEYSTORE_FILE_URL} -q -O /tmp/anysoftkeyboard.keystore
-wget ${PUBLISH_CERT_FILE_URL} -q -O /tmp/apk_upload_key.p12
 ./gradlew --stacktrace -PwithAutoVersioning ${GRADLE_TASKS} generateFdroidYamls
-
-cat outputs/fdroid.yaml
