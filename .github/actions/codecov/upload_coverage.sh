a7cf4eef086b70372d35878950d9425ca9c193ce
Menny Even Danan <menny@evendanan.net>
2019-11-06 16:16:35 -0500

    fail with exit-code if http replies other than 2xx

--- .github/actions/codecov/upload_coverage.sh
+++ .github/actions/codecov/upload_coverage.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-./scripts/retry.sh 5 curl https://codecov.io/bash -o codecov.sh
+./scripts/retry.sh 5 curl  --fail https://codecov.io/bash -o codecov.sh
 chmod +x codecov.sh
 COV_FILES=$(find . -name "test*UnitTestCoverage.xml" | xargs -n 1 echo -n " -f ")
 ./scripts/retry.sh 5 ./codecov.sh -X gcov -X coveragepy -X xcode ${COV_FILES}
