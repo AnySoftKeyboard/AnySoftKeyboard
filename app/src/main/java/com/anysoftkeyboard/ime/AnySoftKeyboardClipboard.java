d762b4d1374d6ea9133d7bf5ae5aa494c1971a95
ArenaL5 <arenal5@gmx.com>
2019-11-24 03:25:40 +0100

    #1636 fixed in fine selection mode too

--- app/src/main/java/com/anysoftkeyboard/ime/AnySoftKeyboardClipboard.java
+++ app/src/main/java/com/anysoftkeyboard/ime/AnySoftKeyboardClipboard.java
@@ -116,11 +116,28 @@ public abstract class AnySoftKeyboardClipboard extends AnySoftKeyboardSwipeListe
         if (mArrowSelectionState && ic != null) {
             switch (keyEventKeyCode) {
                 case KeyEvent.KEYCODE_DPAD_LEFT:
-                    ic.setSelection(
-                            Math.max(0, globalSelectionStartPosition - 1), globalCursorPosition);
+                    // A Unicode codepoint can be made up of two Java chars.
+                    // We check if that's what happening before the cursor:
+                    final String toLeft = ic.getTextBeforeCursor(8, 0).toString();
+                    if (toLeft.length() == 0) {
+                        ic.setSelection(globalSelectionStartPosition, globalCursorPosition);
+                    } else {
+                        ic.setSelection(
+                                globalSelectionStartPosition -
+                                    Character.charCount(toLeft.codePointBefore(toLeft.length())),
+                                    globalCursorPosition);
+                    }
                     return true;
                 case KeyEvent.KEYCODE_DPAD_RIGHT:
-                    ic.setSelection(globalSelectionStartPosition, globalCursorPosition + 1);
+                    final String toRight = ic.getTextAfterCursor(8, 0).toString();
+                    if (toRight.length() == 0) {
+                        ic.setSelection(globalSelectionStartPosition, globalCursorPosition);
+                    } else {
+                        ic.setSelection(
+                                globalSelectionStartPosition,
+                                globalCursorPosition +
+                                    Character.charCount(toRight.codePointAt(0)));
+                    }
                     return true;
                 default:
                     mArrowSelectionState = false;
