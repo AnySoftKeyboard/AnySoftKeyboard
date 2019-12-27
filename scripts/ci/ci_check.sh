be45446350f70e9c89783abe3fe80ce82eb62948
Menny Even Danan <menny@evendanan.net>
2019-12-26 23:22:33 -0500

    unit-tests for inner-word-character

--- scripts/ci/ci_check.sh
+++ scripts/ci/ci_check.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-./gradlew verifyGoogleJavaFormat
+./gradlew verifyGoogleJavaFormat || {
+    echo "code is not formatted."
+    echo "run './gradlew googleJavaFormat' to fix."
+    exit 1
+}
 ./gradlew --stacktrace lintDebug checkstyleMain pmdMain pmdTest
 ./gradlew --stacktrace verifyReleaseResources
 ./gradlew --stacktrace generateReleasePlayResources
