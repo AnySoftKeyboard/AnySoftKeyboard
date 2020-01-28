50a0afd55f54bdd49682563071f9b6eb90b7fdd6
Menny Even Danan <menny@evendanan.net>
2020-01-27 23:32:14 -0500

    workaround for issue 133 in cache-action

--- .github/actions/test-shard-run/run_tests.sh
+++ .github/actions/test-shard-run/run_tests.sh
@@ -8,3 +8,6 @@ EXTRA_ARGS=${4}
 echo "Will run tests for module '${MODULE}' with extra args '${EXTRA_ARGS}' for group-index ${TEST_GROUP_INDEX} out of ${TEST_GROUPS_COUNT} groups:"
 
 ./gradlew --stacktrace -Dorg.gradle.daemon=false ${MODULE}testDebugUnitTest ${MODULE}testDebugUnitTestCoverage ${EXTRA_ARGS}
+
+#see https://github.com/actions/cache/issues/133
+chmod -R a+rwx .
