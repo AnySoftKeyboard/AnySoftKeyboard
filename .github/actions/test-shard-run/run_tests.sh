d60c4822d5790bd41ac4f1c151596bc45288ab12
Menny Even Danan <menny@evendanan.net>
2020-01-31 09:55:05 -0500

    pre-load Robolectric jars, for a more stable and hermetic run

--- .github/actions/test-shard-run/run_tests.sh
+++ .github/actions/test-shard-run/run_tests.sh
@@ -8,7 +8,9 @@ EXTRA_ARGS=${4}
 
 echo "Will run tests for module '${MODULE}' with extra args '${EXTRA_ARGS}' for group-index ${TEST_GROUP_INDEX} out of ${TEST_GROUPS_COUNT} groups:"
 
-./gradlew --stacktrace -Dorg.gradle.daemon=false ${MODULE}testDebugUnitTest ${MODULE}testDebugUnitTestCoverage ${EXTRA_ARGS}
+./scripts/download_robolectric_jars_to_machine.sh
+
+./gradlew ${MODULE}testDebugUnitTest ${MODULE}testDebugUnitTestCoverage ${EXTRA_ARGS}
 
 #see https://github.com/actions/cache/issues/133
 [[ -n "${GITHUB_ACTIONS}" ]] && chmod -R a+rwx .
