eddb8988297c312df8b1675a56fd4838fd42f27c
Menny Even Danan <menny@evendanan.net>
2020-01-31 22:32:33 -0500

    jacoco fixes and wget retries

--- .github/actions/codecov/upload_coverage.sh
+++ .github/actions/codecov/upload_coverage.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-./scripts/retry.sh 5 curl  --fail https://codecov.io/bash -o codecov.sh
+wget --tries=5 --waitretry=5 --progress=dot:mega --output-document=codecov.sh https://codecov.io/bash
 chmod +x codecov.sh
+
 COV_FILES=$(find . -name "test*UnitTestCoverage.xml" | xargs -n 1 echo -n " -f ")
 ./scripts/retry.sh 5 ./codecov.sh -X gcov -X coveragepy -X xcode ${COV_FILES}
