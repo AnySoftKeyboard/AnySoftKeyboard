6fe2f33b59ff6de07e9d2b246acb9e1438d5a8f1
Menny Even Danan <menny@evendanan.net>
2020-02-01 23:45:44 -0500

    Find duplicate add-on IDs

--- scripts/ci/ci_check.sh
+++ scripts/ci/ci_check.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
+DUPLICATE_ID=$(find . -type f -name "*dictionaries.xml" -o -name "*keyboards.xml" -exec grep -Eo "\\sid=\"([A-Za-z0-9\\-]+)\"" {} \; | sort | uniq -d)
+
+if [[ -n "${DUPLICATE_ID}" ]]; then
+  echo "Found duplicate add-ons ID:"
+  echo "${DUPLICATE_ID}"
+  exit 1
+fi
+
 ./gradlew verifyGoogleJavaFormat || {
     echo "code is not formatted."
     echo "run './gradlew googleJavaFormat' to fix."
