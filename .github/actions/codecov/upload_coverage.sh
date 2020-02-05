c56b6cc003971944a267d2cb360b34b2d15fe400
Menny Even Danan <menny@evendanan.net>
2020-02-05 12:24:38 -0500

    directly passing token and config file path

--- .github/actions/codecov/upload_coverage.sh
+++ .github/actions/codecov/upload_coverage.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+CODECOV_TOKEN="${1}"
+
 wget --tries=5 --waitretry=5 --progress=dot:mega --output-document=codecov.sh https://codecov.io/bash
 chmod +x codecov.sh
 
 COV_FILES=$(find . -name "test*UnitTestCoverage.xml" | xargs -n 1 echo -n " -f ")
-./scripts/retry.sh 5 ./codecov.sh -X gcov -X coveragepy -X xcode ${COV_FILES}
+
+./scripts/retry.sh 5 ./codecov.sh -t "${CODECOV_TOKEN}" -y .github/actions/codecov/codecov.yml -X gcov -X coveragepy -X xcode ${COV_FILES}
