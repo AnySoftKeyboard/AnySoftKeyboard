db254705f181f30d3629a9af6d8fdc83ee8a9583
Menny Even Danan <menny@evendanan.net>
2020-02-06 14:15:10 -0500

    Fix collect-report shell script

--- .github/actions/codecov/upload_coverage.sh
+++ .github/actions/codecov/upload_coverage.sh
@@ -6,6 +6,15 @@ CODECOV_TOKEN="${1}"
 wget --tries=5 --waitretry=5 --progress=dot:mega --output-document=codecov.sh https://codecov.io/bash
 chmod +x codecov.sh
 
-COV_FILES=$(find . -name "test*UnitTestCoverage.xml" | xargs -n 1 echo -n " -f ")
+readarray -d '' COV_FILES < <(find . -name "test*UnitTestCoverage.xml" -print0)
 
-./scripts/retry.sh 5 ./codecov.sh -t "${CODECOV_TOKEN}" -y .github/actions/codecov/codecov.yml -X gcov -X coveragepy -X xcode ${COV_FILES}
+rm -rf /tmp/coverage || true
+mkdir /tmp/coverage
+
+for cov_file in "${COV_FILES[@]}"
+do
+  echo "file: ${cov_file}"
+  cp "${cov_file}" "/tmp/coverage/${RANDOM}-${RANDOM}.xml"
+done
+
+./scripts/retry.sh 5 ./codecov.sh -t "${CODECOV_TOKEN}" -y .github/actions/codecov/codecov.yml -X search -X gcov -X coveragepy -X xcode -f '/tmp/coverage/*.xml'
