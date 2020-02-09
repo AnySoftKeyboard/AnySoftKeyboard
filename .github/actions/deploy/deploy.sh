b1b7f829151ab0a0ef43d851020b951e3b658330
Menny Even Danan <menny@evendanan.net>
2020-02-08 23:27:53 -0500

    Merging deploy scripts and flow

--- .github/actions/deploy/deploy.sh
+++ .github/actions/deploy/deploy.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-GITHUB_REF="${1}"
-shift
 DEPLOY_TARGET="${1}"
 shift
 export ANYSOFTKEYBOARD_CRASH_REPORT_EMAIL="${1}"
@@ -26,22 +24,22 @@ if [[ -z "${KEYSTORE_FILE_URL}" ]]; then
     cp ./.github/actions/deploy/debug.keystore /root/.android/ || exit 1
 fi
 
-DEPLOY_TASKS=()
+DEPLOY_TASKS=( "-PwithAutoVersioning" ":generateFdroidYamls" )
 case "${DEPLOY_TARGET}" in
   dry-run)
-    DEPLOY_TASKS=( "-DdeployChannel=alpha" "assembleRelease" "assembleCanary" "verifyReleaseResources" "generateReleasePlayResources" "generateCanaryPlayResources" ":generateFdroidYamls" )
+    DEPLOY_TASKS+=( "-DdeployChannel=alpha" "assembleRelease" "assembleCanary" "verifyReleaseResources" "generateReleasePlayResources" "generateCanaryPlayResources" )
     ;;
 
   app_alpha)
-    DEPLOY_TASKS=( "-DdeployChannel=alpha" "ime:app:assembleCanary" "ime:app:publishCanary" )
+    DEPLOY_TASKS+=( "-DdeployChannel=alpha" "ime:app:assembleCanary" "ime:app:publishCanary" )
     ;;
 
   app_beta)
-    DEPLOY_TASKS=( "-DdeployChannel=beta" "ime:app:assembleRelease" "ime:app:publishRelease" ":generateFdroidYamls" )
+    DEPLOY_TASKS+=( "-DdeployChannel=beta" "ime:app:assembleRelease" "ime:app:publishRelease")
     ;;
 
   addons_alpha)
-    DEPLOY_TASKS=( "-DdeployChannel=alpha" "assembleRelease" "publishRelease" "-x" "ime:app:assembleRelease" "-x" "ime:app:publishRelease" ":generateFdroidYamls" )
+    DEPLOY_TASKS+=( "-DdeployChannel=alpha" "assembleRelease" "publishRelease" "-x" "ime:app:assembleRelease" "-x" "ime:app:publishRelease" )
     ;;
 
   *)
@@ -51,4 +49,27 @@ case "${DEPLOY_TARGET}" in
 esac
 
 echo "Counter is ${BUILD_COUNT_FOR_VERSION}, DEPLOY_TARGET: ${DEPLOY_TARGET}, crash email: ${ANYSOFTKEYBOARD_CRASH_REPORT_EMAIL}, and tasks: ${DEPLOY_TASKS[*]}"
-./scripts/ci/ci_deploy.sh "${KEYSTORE_FILE_URL}" "${PUBLISH_CERT_FILE_URL}" "${DEPLOY_TASKS[@]}"
+
+if [[ "${DEPLOY_TASKS[*]}" == *"publish"* ]]; then
+    echo "Downloading signature files..."
+
+    if [[ -z "${KEYSTORE_FILE_URL}" ]]; then
+        echo "Could not find secure env variable KEYSTORE_FILE_URL. Can not deploy."
+        exit 1
+    fi
+
+    if [[ -z "${PUBLISH_CERT_FILE_URL}" ]]; then
+        echo "Could not find secure env variable PUBLISH_CERT_FILE_URL. Can not deploy."
+        exit 1
+    fi
+
+    wget --tries=5 --waitretry=5 "${KEYSTORE_FILE_URL}" -q -O /tmp/anysoftkeyboard.keystore
+    stat /tmp/anysoftkeyboard.keystore
+    wget --tries=5 --waitretry=5 "${PUBLISH_CERT_FILE_URL}" -q -O /tmp/apk_upload_key.p12
+    stat /tmp/apk_upload_key.p12
+fi
+
+# shellcheck disable=SC2086
+./gradlew "${DEPLOY_TASKS[@]}"
+
+[[ -n "${GITHUB_ACTIONS}" ]] && chmod -R a+rwx .
