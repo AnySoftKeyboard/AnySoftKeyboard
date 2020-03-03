de671137f0845125f86dd21191937db2a43df2a6
matttttt <18152455+martholomew@users.noreply.github.com>
2020-03-03 13:50:35 -0700

    Added Hindi

---
+++ addons/languages/hindi/pack/src/main/java/com/anysoftkeyboard/languagepack/hindi/PackBroadcastReceiver.java
@@ -0,0 +1,7 @@
+package com.anysoftkeyboard.languagepack.hindi;
+
+import com.anysoftkeyboard.addon.PackBroadcastReceiverBase;
+
+public class PackBroadcastReceiver extends PackBroadcastReceiverBase {
+
+}
