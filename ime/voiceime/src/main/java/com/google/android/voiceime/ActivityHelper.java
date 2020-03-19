5ebea281ba96c89f79e733c2463de9d5c32d2837
Menny Even Danan <menny@evendanan.net>
2020-03-18 22:45:11 -0400

    extracting voiceime out of app

--- ime/voiceime/src/main/java/com/google/android/voiceime/ActivityHelper.java
+++ ime/voiceime/src/main/java/com/google/android/voiceime/ActivityHelper.java
@@ -24,10 +24,12 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
-import com.menny.android.anysoftkeyboard.BuildConfig;
+
 import java.util.ArrayList;
 
-/** Helper activity used for triggering the Intent recognition, and for collecting the results. */
+/**
+ * Helper activity used for triggering the Intent recognition, and for collecting the results.
+ */
 public class ActivityHelper extends Activity {
 
     @SuppressWarnings("unused")
@@ -46,7 +48,7 @@ public class ActivityHelper extends Activity {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(
                 "calling_package" /*RecognizerIntent.EXTRA_CALLING_PACKAGE*/,
-                BuildConfig.APPLICATION_ID);
+                getApplicationContext().getPackageName());
         intent.putExtra(
                 RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 5);
