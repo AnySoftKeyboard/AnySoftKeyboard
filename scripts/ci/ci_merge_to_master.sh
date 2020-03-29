522dab6089ce2642ee43d39df92f2c5cdd3ba2c0
Menny Even Danan <menny@evendanan.net>
2020-03-29 10:41:58 -0400

    a more specific merge command

--- scripts/ci/ci_merge_to_master.sh
+++ scripts/ci/ci_merge_to_master.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
+set -e
+set -x
 
 REF="${1}"
 LOCAL_REF="$(echo refs/heads/release-branch-addons-v4.0-r1 | cut -d'/' -f 3)"
 
-echo "Creating merge commit from ${REF}, as local ${LOCAL_REF}"
+echo "Fetching from ${REF}, as local ${LOCAL_REF}:"
 git fetch origin "${REF}"
-git merge "${LOCAL_REF}"
+echo "Merging:"
+git merge "origin/${LOCAL_REF}"
