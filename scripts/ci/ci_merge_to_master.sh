5ab1569fdc95971f09f04116e94872194c0892d4
Menny Even Danan <menny@evendanan.net>
2020-04-01 20:08:53 -0400

    identifying as polyglot

--- scripts/ci/ci_merge_to_master.sh
+++ scripts/ci/ci_merge_to_master.sh
@@ -5,6 +5,12 @@ set -x
 REF="${1}"
 LOCAL_REF="$(echo refs/heads/release-branch-addons-v4.0-r1 | cut -d'/' -f 3)"
 
+echo "setting git user details"
+echo "email"
+git config --global user.email "ask@evendanan.net"
+echo "name"
+git config --global user.name "Polyglot"
+  
 echo "Fetching from ${REF}, as local ${LOCAL_REF}:"
 git fetch origin "${REF}"
 echo "Merging:"
