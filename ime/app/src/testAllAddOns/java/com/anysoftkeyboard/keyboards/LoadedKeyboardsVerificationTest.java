a4bdb891bf3155778fb2d0c807f3b8c9ab7fd8e0
Menny Even Danan <menny@evendanan.net>
2020-04-20 11:29:07 -0400

    ensure tabs are not used

---
+++ ime/app/src/testAllAddOns/java/com/anysoftkeyboard/keyboards/LoadedKeyboardsVerificationTest.java
@@ -0,0 +1,16 @@
+package com.anysoftkeyboard.keyboards;
+
+import com.anysoftkeyboard.AnySoftKeyboardRobolectricTestRunner;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AnySoftKeyboardRobolectricTestRunner.class)
+public class LoadedKeyboardsVerificationTest {
+
+    @Test
+    public void testAllKeysAreOnScreen() throws Exception {
+        Assert.fail();
+    }
+}
