f5793f629109fa4802a510c65924c45208e5cc41
Menny Even Danan <menny@evendanan.net>
2020-05-22 23:12:14 -0400

    Update changelog with clipboard feature

--- ime/app/src/main/java/com/anysoftkeyboard/keyboards/views/MiniKeyboardKeyDetector.java
+++ ime/app/src/main/java/com/anysoftkeyboard/keyboards/views/MiniKeyboardKeyDetector.java
@@ -18,7 +18,6 @@ package com.anysoftkeyboard.keyboards.views;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-
 import com.anysoftkeyboard.keyboards.Keyboard.Key;
 
 class MiniKeyboardKeyDetector extends KeyDetector {
