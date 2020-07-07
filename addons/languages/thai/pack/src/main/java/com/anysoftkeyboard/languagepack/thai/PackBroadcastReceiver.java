5f50aadb034deb39aed629534b55bdf50cb89d60
Wisha Wa <wisha.wa@yandex.com>
2020-07-07 04:27:37 +0000

    Add Thai language

---
+++ addons/languages/thai/pack/src/main/java/com/anysoftkeyboard/languagepack/thai/PackBroadcastReceiver.java
@@ -0,0 +1,5 @@
+package com.anysoftkeyboard.languagepack.thai;
+
+import com.anysoftkeyboard.addon.PackBroadcastReceiverBase;
+
+public class PackBroadcastReceiver extends PackBroadcastReceiverBase {}
