7137cc876f5390d58787654002ba8a1f116eeb3e
Menny Even Danan <menny@evendanan.net>
2020-10-15 21:57:41 -0400

    trying to fix codecov config

--- .github/actions/codecov/upload_coverage.sh
+++ .github/actions/codecov/upload_coverage.sh
@@ -6,15 +6,8 @@ CODECOV_TOKEN="${1}"
 wget --tries=5 --waitretry=5 --progress=dot:mega --output-document=codecov.sh https://codecov.io/bash
 chmod +x codecov.sh
 
-readarray -d '' COV_FILES < <(find . -name "test*UnitTestCoverage.xml" -print0)
+TARGET_FOLDER="${PWD}/build/jacoco"
 
-rm -rf /tmp/coverage || true
-mkdir /tmp/coverage
+ls -al "${TARGET_FOLDER}"
 
-for cov_file in "${COV_FILES[@]}"
-do
-  echo "file: ${cov_file}"
-  cp "${cov_file}" "/tmp/coverage/${RANDOM}-${RANDOM}.xml"
-done
-
-./scripts/retry.sh 5 ./codecov.sh -t "${CODECOV_TOKEN}" -y .github/actions/codecov/codecov.yml -X search -X gcov -X coveragepy -X xcode -f '/tmp/coverage/*.xml'
+./scripts/retry.sh 5 ./codecov.sh -t "${CODECOV_TOKEN}" -X coveragepy -X xcode -X gcov -s "${TARGET_FOLDER}"
