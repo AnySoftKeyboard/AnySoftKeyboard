ddb148bae18d96e21982a4d04cd5064a688e0ee1
somini <dev@somini.xyz>
2020-12-14 00:37:23 +0000

    New Language: Portuguese
    Sourced from the old repository.
    
    - Updated the dictionaries to the latest AOSP and LineageOS
    - Used a new flag from: https://commons.wikimedia.org/wiki/File:Flag_of_Portugal.svg
    
    https://github.com/AnySoftKeyboard/LanguagePack/tree/Portuguese

---
+++ addons/languages/portuguese2/pack/src/main/java/com/anysoftkeyboard/languagepack/portuguese2/PackBroadcastReceiver.java
@@ -0,0 +1,5 @@
+package com.anysoftkeyboard.languagepack.portuguese2;
+
+import com.anysoftkeyboard.addon.PackBroadcastReceiverBase;
+
+public class PackBroadcastReceiver extends PackBroadcastReceiverBase {}
