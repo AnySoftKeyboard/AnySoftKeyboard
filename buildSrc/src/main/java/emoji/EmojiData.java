b180cc3a4a39089950a32e079f361b420386f464
Menny Even Danan <menny@evendanan.net>
2021-06-22 14:11:00 -0400

    Adding marine-food to real animals.
    https://github.com/AnySoftKeyboard/AnySoftKeyboard/issues/2716

--- buildSrc/src/main/java/emoji/EmojiData.java
+++ buildSrc/src/main/java/emoji/EmojiData.java
@@ -3,6 +3,7 @@ package emoji;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 class EmojiData {
     public final int position;
@@ -25,5 +26,18 @@ class EmojiData {
 
     public List<String> getVariants() {
         return Collections.unmodifiableList(mVariants);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EmojiData emojiData = (EmojiData) o;
+        return Objects.equals(output, emojiData.output);
+    }
+
+    @Override
+    public int hashCode() {
+        return output.hashCode();
     }
 }
