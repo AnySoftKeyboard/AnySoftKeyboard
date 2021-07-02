33254db2ba71fb5adca7ffb313ac26d72c1af14a
John Oberhauser <john.oberhauser@live.com>
2021-07-02 16:59:02 -0500

    cleanup

--- ime/app/src/main/java/com/anysoftkeyboard/keyboards/views/CandidateView.java
+++ ime/app/src/main/java/com/anysoftkeyboard/keyboards/views/CandidateView.java
@@ -31,7 +31,6 @@ import android.text.StaticLayout;
 import android.text.TextPaint;
 import android.text.TextUtils;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
