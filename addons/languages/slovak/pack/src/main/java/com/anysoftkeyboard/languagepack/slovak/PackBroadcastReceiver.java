4ce39980a4f74762b44c9500bc79d4cff3e8f898
Aiq0 <66842415+Aiq0@users.noreply.github.com>
2021-10-03 19:31:24 +0200

    Added Slovak Language Pack

---
+++ addons/languages/slovak/pack/src/main/java/com/anysoftkeyboard/languagepack/slovak/PackBroadcastReceiver.java
@@ -0,0 +1,5 @@
+package com.anysoftkeyboard.languagepack.slovak;
+
+import com.anysoftkeyboard.addon.PackBroadcastReceiverBase;
+
+public class PackBroadcastReceiver extends PackBroadcastReceiverBase {}
