e3518769d070b8ffa62ceb62cc9705e49bae8244
Leo Arias <elopio@openzeppelin.com>
2021-12-25 06:15:02 +0000

    Initial implementation of the bribri layout

---
+++ addons/languages/bribri/pack/src/main/java/com/anysoftkeyboard/languagepack/bribri/PackBroadcastReceiver.java
@@ -0,0 +1,5 @@
+package com.anysoftkeyboard.languagepack.bribri;
+
+import com.anysoftkeyboard.addon.PackBroadcastReceiverBase;
+
+public class PackBroadcastReceiver extends PackBroadcastReceiverBase {}
