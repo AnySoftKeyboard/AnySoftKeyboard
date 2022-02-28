d9edbb1edb7f43ec217ccc8a7ad79a39153f28f9
Menny Even Danan <menny@evendanan.net>
2022-02-28 13:48:49 -0500

    Automating AGP bumps

---
+++ scripts/get_latest_maven_artifact_version.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#https://dl.google.com/dl/android/maven2
+MAVEN_URL="$1"
+#com.android.tools.build
+GROUP_ID="$2"
+#gradle
+ARTIFACT_ID="$3"
+
+VERSIONS_URL="${MAVEN_URL}/${GROUP_ID//[.]/\/}/group-index.xml"
+
+VERSIONS=$(curl --silent "$VERSIONS_URL" | xmllint --format --xpath "string(/${GROUP_ID}/${ARTIFACT_ID}/@versions)" -)
+#echo "For ${GROUP_ID}:${ARTIFACT_ID} we have: $VERSIONS"
+
+VERSIONS_ARRAY=(${VERSIONS//,/ })
+readarray -t SORTED_VERSIONS_ARRAY < <(for v in "${VERSIONS_ARRAY[@]}"; do echo "$v"; done | sort -r)
+for index in "${!SORTED_VERSIONS_ARRAY[@]}" ; do [[ ${SORTED_VERSIONS_ARRAY[$index]} =~ alpha ]] && unset -v 'SORTED_VERSIONS_ARRAY[$index]' ; done
+for index in "${!SORTED_VERSIONS_ARRAY[@]}" ; do [[ ${SORTED_VERSIONS_ARRAY[$index]} =~ beta ]] && unset -v 'SORTED_VERSIONS_ARRAY[$index]' ; done
+SORTED_VERSIONS_ARRAY=("${SORTED_VERSIONS_ARRAY[@]}")
+echo "${SORTED_VERSIONS_ARRAY[0]}"
