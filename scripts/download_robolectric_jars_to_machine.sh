522ffb262934c7a489e34ad0f626684618e5de3f
Menny Even Danan <menny@evendanan.net>
2022-04-20 12:27:56 -0400

    Allowing the bot to also redefine robolectric jars

--- scripts/download_robolectric_jars_to_machine.sh
+++ scripts/download_robolectric_jars_to_machine.sh
@@ -1,29 +1,18 @@
 #!/bin/bash
 set -e
 
-TARGET_FOLDER=".robolectric-android-all-jars/"
+TARGET_FOLDER=".robolectric-android-all-jars"
 rm -rf "${TARGET_FOLDER}" || true
 mkdir -p "${TARGET_FOLDER}"
 
 DOWNLOAD_BASE_URL="https://repo1.maven.org/maven2/org/robolectric/android-all-instrumented"
-#artifacts taken from: https://github.com/robolectric/robolectric/blob/robolectric-4.7.3/robolectric/src/main/java/org/robolectric/plugins/DefaultSdkProvider.java
-ARTIFACTS=( \
-  "12-robolectric-7732740" \
-  "11-robolectric-6757853" \
-  "10-robolectric-5803371" \
-  "9-robolectric-4913185-2" \
-  "8.1.0-robolectric-4611349" \
-  "8.0.0_r4-robolectric-r1" \
-  "7.1.0_r7-robolectric-r1" \
-  "7.0.0_r1-robolectric-r1" \
-  "6.0.1_r3-robolectric-r1" \
-  "5.1.1_r9-robolectric-r2" \
-  "5.0.2_r3-robolectric-r0" \
-  "4.4_r1-robolectric-r2" \
-  "4.3_r2-robolectric-r1" \
-  "4.2.2_r1.2-robolectric-r1" \
-  "4.1.2_r1-robolectric-r1" \
-  )
+#artifacts taken from: https://github.com/robolectric/robolectric/blob/robolectric-VERSION/robolectric/src/main/java/org/robolectric/plugins/DefaultSdkProvider.java
+mapfile -t VERSIONS_ARRAY < scripts/robolectric_jars_versions.txt
+ARTIFACTS=()
+for (( idx=0; idx<${#VERSIONS_ARRAY[@]} ; idx=idx+2 )) ; do
+  ARTIFACT="${VERSIONS_ARRAY[${idx}]}-robolectric-${VERSIONS_ARRAY[${idx}+1]}"
+  ARTIFACTS+=( "$ARTIFACT" )
+done
 
 for artifact in "${ARTIFACTS[@]}"
 do
