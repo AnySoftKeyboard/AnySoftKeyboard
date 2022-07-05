b0c80af67bd7186318451322bac53dcc314ae453
Menny Even Danan <menny@evendanan.net>
2022-07-05 15:24:41 +0300

    Ensuring we have latest yaml-lint

--- scripts/ci/ci_check_yamls.sh
+++ scripts/ci/ci_check_yamls.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-if ! command -v yamllint; then
-  sudo apt-get install -y --allow-remove-essential --allow-change-held-packages yamllint
-fi
-yamllint -c config/yamllint.yml -f github . | tee build/yamllint.log
+python3 -m pip install --upgrade pip setuptools wheel
+python3 -m pip install "yamllint"
+
+yamllint -c config/yamllint.yml -f github .
 
 if command -v go; then
   go install github.com/rhysd/actionlint/cmd/actionlint@latest
-  actionlint -no-color -ignore SC2215| tee build/actionlint.log
+  actionlint -no-color -ignore SC2215
 fi
