1335d360e4e6a1cd6252f52f828524c1f22537a2
Menny Even Danan <menny@evendanan.net>
2023-07-07 15:32:26 -0400

    Add support for multiple strings files from crowdin

--- scripts/update_strings_from_crowdin_api.sh
+++ scripts/update_strings_from_crowdin_api.sh
@@ -4,6 +4,7 @@ set -e
 TEMP_EXTRACT_FOLDER="${TMPDIR:-/tmp}/ask_crowdin/"
 TEMP_OUTPUT_FOLDER="${TMPDIR:-/tmp}/ask_crowdin_file/"
 TEMP_OUTPUT_FILE=all.zip
+REPO_ROOT="$PWD"
 
 if [ -z "${CROWDIN_API}" ]; then
     echo "Could not find crowdin API environment variable at CROWDIN_API."
@@ -27,47 +28,67 @@ mkdir "${TEMP_OUTPUT_FOLDER}"
 wget --tries=5 --waitretry=5 -O "${TEMP_OUTPUT_FOLDER}${TEMP_OUTPUT_FILE}" "https://api.crowdin.com/api/project/anysoftkeyboard/download/all.zip?key=${CROWDIN_API}"
 unzip -o "${TEMP_OUTPUT_FOLDER}${TEMP_OUTPUT_FILE}" -d "${TEMP_EXTRACT_FOLDER}"
 
+echo "output folder: ${TEMP_EXTRACT_FOLDER}"
+
 pushd "${TEMP_EXTRACT_FOLDER}" || exit 1
 for f in *; do mv "$f" "values-$f"; done
 popd || exit 1
 
-APP_RES_FOLDER=ime/app/src/main/res
-echo "will copy from ${TEMP_EXTRACT_FOLDER} to ${APP_RES_FOLDER}"
-for f in "${TEMP_EXTRACT_FOLDER}"*; do cp -R "$f" "${APP_RES_FOLDER}"; done
+copy_script="${PWD}/scripts/copy_translations_from_crowdin_to_module.sh"
+function copy_translations() {
+  local TARGET_FOLDER="$1"
+  local SOURCE_FILE="$2"
 
-echo "fixing file locations a bit..."
-rm -rf "${APP_RES_FOLDER}/values-en-PT"
-mv "${APP_RES_FOLDER}/values-es-ES/strings.xml" "${APP_RES_FOLDER}/values-es/"
-rm -rf "${APP_RES_FOLDER}/values-es-AR" || true
-rm -rf "${APP_RES_FOLDER}/values-es-ES" || true
-mv "${APP_RES_FOLDER}/values-he/strings.xml" "${APP_RES_FOLDER}/values-iw/"
-rm -rf "${APP_RES_FOLDER}/values-he" || true
-mv "${APP_RES_FOLDER}/values-yi/strings.xml" "${APP_RES_FOLDER}/values-ji/"
-rm -rf "${APP_RES_FOLDER}/values-yi" || true
-mv "${APP_RES_FOLDER}/values-hy-AM/strings.xml" "${APP_RES_FOLDER}/values-hy/"
-rm -rf "${APP_RES_FOLDER}/values-hy-AM" || true
-mv "${APP_RES_FOLDER}/values-sv-SE/strings.xml" "${APP_RES_FOLDER}/values-se/"
-rm -rf "${APP_RES_FOLDER}/values-sv-SE/" || true
-mv "${APP_RES_FOLDER}/values-pt-PT/strings.xml" "${APP_RES_FOLDER}/values-pt/"
-rm -rf "${APP_RES_FOLDER}/values-pt-PT/" || true
-mv "${APP_RES_FOLDER}/values-pt-BR/strings.xml" "${APP_RES_FOLDER}/values-pt-rBR/"
-rm -rf "${APP_RES_FOLDER}/values-pt-BR/" || true
-mv "${APP_RES_FOLDER}/values-zh-CN/strings.xml" "${APP_RES_FOLDER}/values-zh-rCN/"
-rm -rf "${APP_RES_FOLDER}/values-zh-CN/" || true
-mv "${APP_RES_FOLDER}/values-tlh-AA/strings.xml" "${APP_RES_FOLDER}/values-tlh/"
-rm -rf "${APP_RES_FOLDER}/values-tlh-AA" || true
-mv "${APP_RES_FOLDER}/values-es-MX/strings.xml" "${APP_RES_FOLDER}/values-es-rMX/"
-rm -rf "${APP_RES_FOLDER}/values-es-MX/" || true
-mv "${APP_RES_FOLDER}/values-ml-IN/strings.xml" "${APP_RES_FOLDER}/values-ml-rIN/"
-rm -rf "${APP_RES_FOLDER}/values-ml-IN/" || true
-mv "${APP_RES_FOLDER}/values-bn-IN/strings.xml" "${APP_RES_FOLDER}/values-bn-rIN/"
-rm -rf "${APP_RES_FOLDER}/values-bn-IN/" || true
-mv "${APP_RES_FOLDER}/values-si-LK/strings.xml" "${APP_RES_FOLDER}/values-si-rLK/"
-rm -rf "${APP_RES_FOLDER}/values-si-LK/" || true
-#copying generic strings to en
-cp "${APP_RES_FOLDER}/values/strings.xml" "${APP_RES_FOLDER}/values-en/strings.xml"
+  echo "will copy ${SOURCE_FILE} from ${TEMP_EXTRACT_FOLDER} to ${TARGET_FOLDER}"
+  for f in **/"${SOURCE_FILE}"; do
+    echo "file $f"
+    local TARGET_RES_FOLDER="${TARGET_FOLDER}/$(dirname $f)"
+    echo "target folder $TARGET_RES_FOLDER"
+    mkdir -p "${TARGET_RES_FOLDER}"
+    cp "$f" "${TARGET_RES_FOLDER}/strings.xml"
+  done
 
-echo "fixing ellipsis character..."
-find "${APP_RES_FOLDER}" -type f -name "strings.xml" -exec sed -i 's/\.\.\./…/g' {} \;
+  echo "fixing file locations a bit..."
+  rm -rf "${TARGET_FOLDER}/values-en-PT"
+  mv "${TARGET_FOLDER}/values-es-ES/strings.xml" "${TARGET_FOLDER}/values-es/"
+  rm -rf "${TARGET_FOLDER}/values-es-AR" || true
+  rm -rf "${TARGET_FOLDER}/values-es-ES" || true
+  mv "${TARGET_FOLDER}/values-he/strings.xml" "${TARGET_FOLDER}/values-iw/"
+  rm -rf "${TARGET_FOLDER}/values-he" || true
+  mv "${TARGET_FOLDER}/values-yi/strings.xml" "${TARGET_FOLDER}/values-ji/"
+  rm -rf "${TARGET_FOLDER}/values-yi" || true
+  mv "${TARGET_FOLDER}/values-hy-AM/strings.xml" "${TARGET_FOLDER}/values-hy/"
+  rm -rf "${TARGET_FOLDER}/values-hy-AM" || true
+  mv "${TARGET_FOLDER}/values-sv-SE/strings.xml" "${TARGET_FOLDER}/values-se/"
+  rm -rf "${TARGET_FOLDER}/values-sv-SE/" || true
+  mv "${TARGET_FOLDER}/values-pt-PT/strings.xml" "${TARGET_FOLDER}/values-pt/"
+  rm -rf "${TARGET_FOLDER}/values-pt-PT/" || true
+  mv "${TARGET_FOLDER}/values-pt-BR/strings.xml" "${TARGET_FOLDER}/values-pt-rBR/"
+  rm -rf "${TARGET_FOLDER}/values-pt-BR/" || true
+  mv "${TARGET_FOLDER}/values-zh-CN/strings.xml" "${TARGET_FOLDER}/values-zh-rCN/"
+  rm -rf "${TARGET_FOLDER}/values-zh-CN/" || true
+  mv "${TARGET_FOLDER}/values-tlh-AA/strings.xml" "${TARGET_FOLDER}/values-tlh/"
+  rm -rf "${TARGET_FOLDER}/values-tlh-AA" || true
+  mv "${TARGET_FOLDER}/values-es-MX/strings.xml" "${TARGET_FOLDER}/values-es-rMX/"
+  rm -rf "${TARGET_FOLDER}/values-es-MX/" || true
+  mv "${TARGET_FOLDER}/values-ml-IN/strings.xml" "${TARGET_FOLDER}/values-ml-rIN/"
+  rm -rf "${TARGET_FOLDER}/values-ml-IN/" || true
+  mv "${TARGET_FOLDER}/values-bn-IN/strings.xml" "${TARGET_FOLDER}/values-bn-rIN/"
+  rm -rf "${TARGET_FOLDER}/values-bn-IN/" || true
+  mv "${TARGET_FOLDER}/values-si-LK/strings.xml" "${TARGET_FOLDER}/values-si-rLK/"
+  rm -rf "${TARGET_FOLDER}/values-si-LK/" || true
+  #copying generic strings to en
+  cp "${TARGET_FOLDER}/values/strings.xml" "${TARGET_FOLDER}/values-en/strings.xml"
 
+  echo "fixing ellipsis character..."
+  find "${TARGET_FOLDER}" -type f -name "strings.xml" -exec sed -i 's/\.\.\./…/g' {} \;
+}
+
+pushd "${TEMP_EXTRACT_FOLDER}"
+
+copy_translations "${REPO_ROOT}/ime/app/src/main/res" strings.xml
+copy_translations "${REPO_ROOT}/ime/remote/src/main/res" remote_strings.xml
+copy_translations "${REPO_ROOT}/ime/addons/src/main/res" addons_strings.xml
+
+popd
 echo "done"
