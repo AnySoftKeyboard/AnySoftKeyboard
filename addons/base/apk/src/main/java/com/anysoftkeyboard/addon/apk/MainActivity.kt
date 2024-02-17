595eb6ee367660985174358c52c4c71cf645120f
Menny Even Danan <menny@evendanan.net>
2024-02-16 22:59:18 -0500

    Adding base launcher activity for add ons

---
+++ addons/base/apk/src/main/java/com/anysoftkeyboard/addon/apk/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.anysoftkeyboard.addon.apk
+
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.anysoftkeyboard.addon.base.apk.R
+import com.anysoftkeyboard.addon.base.apk.databinding.ActivityMainBinding
+
+
+const val ASK_PACKAGE_NAME = "com.menny.android.anysoftkeyboard"
+abstract class MainActivityBase(
+    @StringRes private val addOnName: Int,
+    @StringRes private val addOnDescription: Int,
+    @DrawableRes private val screenshot: Int) : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.appScreenshot.setImageResource(screenshot)
+        binding.welcomeDescription.text = getString(R.string.welcome_subtitle_template, getText(addOnName))
+        binding.packDescription.text = getText(addOnDescription)
+
+        if (isAnySoftKeyboardInstalled()) {
+            binding.actionDescription.setText(R.string.ask_installed)
+            binding.actionButton.setText(R.string.open_ask_main_settings)
+            binding.actionButton.setOnClickListener {
+                try {
+                    packageManager.getLaunchIntentForPackage(ASK_PACKAGE_NAME)?.let { intent ->
+                        it.context.startActivity(intent)
+                    }
+                } catch (ex: Exception) {
+                    Log.e("ASK_ADD_ON", "Could not launch Store search!", ex)
+                }
+            }
+        } else {
+            binding.actionDescription.setText(R.string.ask_is_missing_need_install)
+            binding.actionButton.setText(R.string.open_ask_in_vending)
+            binding.actionButton.setOnClickListener {
+                try {
+                    val search = Intent(Intent.ACTION_VIEW)
+                    val uri = Uri.Builder()
+                        .scheme("market")
+                        .authority("search")
+                        .appendQueryParameter("q", ASK_PACKAGE_NAME)
+                        .build()
+                    search.setData(uri)
+                    it.context.startActivity(search)
+                } catch (ex: Exception) {
+                    Log.e("ASK_ADD_ON", "Could not launch Store search!", ex)
+                }
+            }
+        }
+    }
+
+    private fun isAnySoftKeyboardInstalled(): Boolean {
+        //TODO: we need to query for a broadcast-receiver, or something
+        return try {
+            val services = packageManager.getPackageInfo(
+                ASK_PACKAGE_NAME,
+                PackageManager.GET_SERVICES
+            )
+            services.services.any { it.name == "com.menny.android.anysoftkeyboard.SoftKeyboard" }
+        } catch (e: Exception) {
+            false
+        }
+    }
+}
---
+++ addons/base/apk/src/test/java/com/anysoftkeyboard/addon/apk/MainActivityBaseTest.kt
@@ -0,0 +1,144 @@
+package com.anysoftkeyboard.addon.apk
+
+import android.content.ComponentName
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageInfo
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.lifecycle.Lifecycle
+import androidx.test.core.app.ActivityScenario
+import com.anysoftkeyboard.AnySoftKeyboardRobolectricTestRunner
+import org.junit.Test
+import com.anysoftkeyboard.addon.base.apk.R
+import org.junit.Assert
+import org.junit.runner.RunWith
+import org.robolectric.RuntimeEnvironment
+import org.robolectric.Shadows
+
+@RunWith(AnySoftKeyboardRobolectricTestRunner::class)
+class MainActivityBaseTest {
+
+    @Test
+    fun testActivityShowsAddOnDetails() {
+        ActivityScenario.launch(TestMainActivity::class.java).use { scenario ->
+            scenario
+                .moveToState(Lifecycle.State.RESUMED)
+                .onActivity { activity ->
+                    activity.findViewById<TextView>(R.id.welcome_description).let {
+                        Assert.assertEquals(
+                            "Thank you for installing Test Add On App Name for AnySoftKeyboard.",
+                            it.text
+                        )
+                    }
+                    activity.findViewById<ImageView>(R.id.app_screenshot).let {
+                        Assert.assertEquals(
+                            R.drawable.test_screenshot,
+                            Shadows.shadowOf(it.drawable).createdFromResId
+                        )
+                    }
+                    activity.findViewById<TextView>(R.id.pack_description).let {
+                        Assert.assertEquals(
+                            "This is a test add on description, it can be anything",
+                            it.text
+                        )
+                    }
+                }
+        }
+    }
+
+    @Test
+    fun testInstallAnySoftKeyboardFlow() {
+        Shadows.shadowOf(RuntimeEnvironment.getApplication().packageManager)
+            .deletePackage(ASK_PACKAGE_NAME)
+
+        ActivityScenario.launch(TestMainActivity::class.java).use { scenario ->
+            scenario
+                .moveToState(Lifecycle.State.RESUMED)
+                .onActivity { activity ->
+                    activity.findViewById<TextView>(R.id.action_description).run {
+                        Assert.assertEquals(
+                            "AnySoftKeyboard is not installed on your device.\nIn order to use this expansion pack, you must first install AnySoftKeyboard.",
+                            text
+                        )
+                    }
+                    activity.findViewById<Button>(R.id.action_button).run {
+                        Assert.assertEquals(
+                            "Go to Play Store",
+                            text
+                        )
+                        Shadows.shadowOf(this).onClickListener.onClick(this)
+                        Shadows.shadowOf(RuntimeEnvironment.getApplication()).let { app ->
+                            app.nextStartedActivity.let { searchIntent ->
+                                Assert.assertEquals(Intent.ACTION_VIEW, searchIntent.action)
+                                Assert.assertEquals("market", searchIntent.data!!.scheme)
+                                Assert.assertEquals("search", searchIntent.data!!.authority)
+                                Assert.assertEquals(
+                                    "q=com.menny.android.anysoftkeyboard",
+                                    searchIntent.data!!.query
+                                )
+                            }
+                        }
+                    }
+
+                }
+        }
+    }
+
+    @Test
+    fun testAlreadyInstalledAnySoftKeyboardFlow() {
+        Shadows.shadowOf(RuntimeEnvironment.getApplication().packageManager).let { pm ->
+            PackageInfo().let { info ->
+                info.packageName = ASK_PACKAGE_NAME
+                pm.installPackage(info)
+            }
+            pm.addServiceIfNotPresent(
+                ComponentName(
+                    ASK_PACKAGE_NAME,
+                    "${ASK_PACKAGE_NAME}.SoftKeyboard"
+                )
+            )
+            ComponentName(ASK_PACKAGE_NAME, "${ASK_PACKAGE_NAME}.MainActivity").let { info ->
+                pm.addActivityIfNotPresent(info)
+                pm.addIntentFilterForActivity(
+                    info,
+                    IntentFilter().apply {
+                        addAction(Intent.ACTION_MAIN)
+                        addCategory(Intent.CATEGORY_LAUNCHER)
+                    }
+                )
+            }
+        }
+
+        ActivityScenario.launch(TestMainActivity::class.java).use { scenario ->
+            scenario
+                .moveToState(Lifecycle.State.RESUMED)
+                .onActivity { activity ->
+                    activity.findViewById<TextView>(R.id.action_description).run {
+                        Assert.assertEquals(
+                            "AnySoftKeyboard is installed. You may need to set it up to start using this expansion pack.",
+                            text
+                        )
+                    }
+                    activity.findViewById<Button>(R.id.action_button).run {
+                        Assert.assertEquals(
+                            "Open AnySoftKeyboard",
+                            text
+                        )
+                        Shadows.shadowOf(this).onClickListener.onClick(this)
+                        Shadows.shadowOf(RuntimeEnvironment.getApplication()).let { app ->
+                            app.nextStartedActivity.let { launcherIntent ->
+                                Assert.assertEquals(ASK_PACKAGE_NAME, launcherIntent.`package`)
+                            }
+                        }
+                    }
+                }
+        }
+    }
+}
+
+class TestMainActivity: MainActivityBase(
+    R.string.test_app_name,
+    R.string.test_add_on_description,
+    R.drawable.test_screenshot)
