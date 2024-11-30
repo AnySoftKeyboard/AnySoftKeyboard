24860f42ea5986961e3e29c5def0709c84b4f3a8
Menny Even Danan <menny@evendanan.net>
2024-11-30 11:37:52 -0500

    yamllint is already installed in the docker image

--- scripts/ci/ci_check_yamls.sh
+++ scripts/ci/ci_check_yamls.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-python3 -m pip install --upgrade pip setuptools wheel
-python3 -m pip install "yamllint"
-
 yamllint -c config/yamllint.yml -f github .
 
 if command -v go; then
