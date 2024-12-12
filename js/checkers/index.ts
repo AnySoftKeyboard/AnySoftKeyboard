974167ee82b15d132c3456b482bab0e5bb31f893
Menny Even Danan <menny@evendanan.net>
2024-12-11 21:18:33 -0500

    A small code re-order

--- js/checkers/index.ts
+++ js/checkers/index.ts
@@ -3,14 +3,13 @@ import type { Checker } from './checker.js';
 import { Command } from 'commander';
 import { exit } from 'process';
 
-const program = new Command();
-program.name('checkers').description('CLI to run various checkers on the codebase.').version('0.0.1');
-
 function skip_predicate(dirname: string): boolean {
   const lowerCase = dirname.toLowerCase();
   return lowerCase.indexOf('build') >= 0 ||lowerCase.indexOf('.git') >= 0 || lowerCase.indexOf('node_modules') >= 0 || lowerCase.indexOf('bazel-') >= 0;
 }
 
+const program = new Command();
+program.name('checkers').description('CLI to run various checkers on the codebase.').version('0.0.1');
 program.requiredOption('--root_dir <path>', 'Path to the root of the repo.').action(async (options) => {
   const checkers: Checker[] = [new UniqueAddOnIdChecker()];
 
