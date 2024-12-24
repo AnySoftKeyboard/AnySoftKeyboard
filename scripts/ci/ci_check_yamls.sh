c066a3d3df0df6f328188ea8a255767cc9cf8131
Menny Even Danan <menny@evendanan.net>
2024-12-24 09:49:51 -0500

    Add all formatter from bazel

--- scripts/ci/ci_check_yamls.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-yamllint -c config/yamllint.yml -f github .
-
-if command -v go; then
-  go install github.com/rhysd/actionlint/cmd/actionlint@latest
-  actionlint -no-color -ignore SC2215
-fi
