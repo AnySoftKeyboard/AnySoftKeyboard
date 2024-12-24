2b0836e89a31542209b891bfaa7942a192f5ecf3
Menny Even Danan <menny@evendanan.net>
2024-12-24 00:20:33 -0500

    Add ktfmt to bazel

--- buildSrc/src/main/kotlin/net/evendanan/versiongenerator/VersionGenerator.kt
+++ buildSrc/src/main/kotlin/net/evendanan/versiongenerator/VersionGenerator.kt
@@ -4,18 +4,19 @@ package net.evendanan.versiongenerator
 
 abstract class VersionGenerator(val name: String) {
 
-    abstract fun isValidForEnvironment(): Boolean
+  abstract fun isValidForEnvironment(): Boolean
 
-    open fun generate(generationData: GenerationData): VersionData {
-        return VersionData(getVersionCode(generationData), getVersionName(generationData), this)
-    }
+  open fun generate(generationData: GenerationData): VersionData {
+    return VersionData(getVersionCode(generationData), getVersionName(generationData), this)
+  }
 
-    protected abstract fun getVersionCode(generationData: GenerationData): Int
+  protected abstract fun getVersionCode(generationData: GenerationData): Int
 
-    protected open fun getVersionName(generationData: GenerationData): String {
-        return "%d.%d.%d".format(
-                generationData.major,
-                generationData.minor,
-                getVersionCode(generationData) + generationData.patchOffset)
-    }
+  protected open fun getVersionName(generationData: GenerationData): String {
+    return "%d.%d.%d"
+        .format(
+            generationData.major,
+            generationData.minor,
+            getVersionCode(generationData) + generationData.patchOffset)
+  }
 }
--- buildSrc/src/main/kotlin/net/evendanan/versiongenerator/VersionGeneratorFactory.kt
+++ buildSrc/src/main/kotlin/net/evendanan/versiongenerator/VersionGeneratorFactory.kt
@@ -7,27 +7,40 @@ import net.evendanan.versiongenerator.generators.GitBuildVersionGenerator
 import net.evendanan.versiongenerator.generators.StaticVersionGenerator
 
 open class VersionGeneratorFactory {
-    open fun generateVersion(major: Int, minor: Int, patchOffset: Int, generators: Iterable<VersionGenerator>): VersionData {
-        val generationData = GenerationData(major, minor, patchOffset)
+  open fun generateVersion(
+      major: Int,
+      minor: Int,
+      patchOffset: Int,
+      generators: Iterable<VersionGenerator>
+  ): VersionData {
+    val generationData = GenerationData(major, minor, patchOffset)
 
-        for (generator in generators) {
-            if (generator.isValidForEnvironment()) {
-                return generator.generate(generationData)
-            }
-        }
-
-        throw IllegalStateException("Could not find any valid VersionGenerator for this environment!")
+    for (generator in generators) {
+      if (generator.isValidForEnvironment()) {
+        return generator.generate(generationData)
+      }
     }
 
-    open fun generateVersion(major: Int, minor: Int, generators: Iterable<VersionGenerator>): VersionData {
-        return generateVersion(major, minor, 0, generators)
-    }
+    throw IllegalStateException("Could not find any valid VersionGenerator for this environment!")
+  }
 
-    open fun generateVersion(major: Int, minor: Int, patchOffset: Int): VersionData {
-        return generateVersion(major, minor, patchOffset,
-                listOf(EnvBuildVersionGenerator.CircleCi(),
-                        EnvBuildVersionGenerator.Shippable(),
-                        GitBuildVersionGenerator(),
-                        StaticVersionGenerator()))
-    }
+  open fun generateVersion(
+      major: Int,
+      minor: Int,
+      generators: Iterable<VersionGenerator>
+  ): VersionData {
+    return generateVersion(major, minor, 0, generators)
+  }
+
+  open fun generateVersion(major: Int, minor: Int, patchOffset: Int): VersionData {
+    return generateVersion(
+        major,
+        minor,
+        patchOffset,
+        listOf(
+            EnvBuildVersionGenerator.CircleCi(),
+            EnvBuildVersionGenerator.Shippable(),
+            GitBuildVersionGenerator(),
+            StaticVersionGenerator()))
+  }
 }
