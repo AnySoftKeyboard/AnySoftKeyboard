243ff28e3c5310088961fabb1aca7b246ca243f5
Aditya Wibisana <aditya.wibisana@gmail.com>
2025-05-03 16:07:29 +0700

    Fix error related to "Constant expression required"
    Resource IDs cannot be used in a switch statement in Android library modules

--- ime/app/src/main/java/com/anysoftkeyboard/quicktextkeys/ui/FrameKeyboardViewClickListener.java
+++ ime/app/src/main/java/com/anysoftkeyboard/quicktextkeys/ui/FrameKeyboardViewClickListener.java
@@ -17,39 +17,34 @@ import com.menny.android.anysoftkeyboard.R;
 
   @Override
   public void onClick(View v) {
-    switch (v.getId()) {
-      case R.id.quick_keys_popup_close:
-        mKeyboardActionListener.onKey(KeyCodes.CANCEL, null, 0, null, true);
-        break;
-      case R.id.quick_keys_popup_backspace:
-        mKeyboardActionListener.onKey(KeyCodes.DELETE, null, 0, null, true);
-        break;
-      case R.id.quick_keys_popup_quick_keys_insert_media:
-        mKeyboardActionListener.onKey(KeyCodes.IMAGE_MEDIA_POPUP, null, 0, null, true);
-        break;
-      case R.id.quick_keys_popup_delete_recently_used_smileys:
-        mKeyboardActionListener.onKey(KeyCodes.CLEAR_QUICK_TEXT_HISTORY, null, 0, null, true);
-        // re-show
-        mKeyboardActionListener.onKey(KeyCodes.QUICK_TEXT_POPUP, null, 0, null, true);
-        break;
-      case R.id.quick_keys_popup_quick_keys_settings:
-        Intent startSettings =
-            new Intent(
-                Intent.ACTION_VIEW,
-                Uri.parse(v.getContext().getString(R.string.deeplink_url_quick_text)),
-                v.getContext(),
-                MainSettingsActivity.class);
-        startSettings.setFlags(
-            Intent.FLAG_ACTIVITY_NEW_TASK
-                | Intent.FLAG_ACTIVITY_NO_HISTORY
-                | Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
-        v.getContext().startActivity(startSettings);
-        // and closing keyboard
-        mKeyboardActionListener.onKey(KeyCodes.CANCEL, null, 0, null, true);
-        break;
-      default:
-        throw new IllegalArgumentException(
-            "Failed to handle view id " + v.getId() + " in FrameKeyboardViewClickListener");
+    int id = v.getId();
+    if (id == R.id.quick_keys_popup_close) {
+      mKeyboardActionListener.onKey(KeyCodes.CANCEL, null, 0, null, true);
+    } else if (id == R.id.quick_keys_popup_backspace) {
+      mKeyboardActionListener.onKey(KeyCodes.DELETE, null, 0, null, true);
+    } else if (id == R.id.quick_keys_popup_quick_keys_insert_media) {
+      mKeyboardActionListener.onKey(KeyCodes.IMAGE_MEDIA_POPUP, null, 0, null, true);
+    } else if (id == R.id.quick_keys_popup_delete_recently_used_smileys) {
+      mKeyboardActionListener.onKey(KeyCodes.CLEAR_QUICK_TEXT_HISTORY, null, 0, null, true);
+      // re-show
+      mKeyboardActionListener.onKey(KeyCodes.QUICK_TEXT_POPUP, null, 0, null, true);
+    } else if (id == R.id.quick_keys_popup_quick_keys_settings) {
+      Intent startSettings =
+              new Intent(
+                      Intent.ACTION_VIEW,
+                      Uri.parse(v.getContext().getString(R.string.deeplink_url_quick_text)),
+                      v.getContext(),
+                      MainSettingsActivity.class);
+      startSettings.setFlags(
+              Intent.FLAG_ACTIVITY_NEW_TASK
+                      | Intent.FLAG_ACTIVITY_NO_HISTORY
+                      | Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+      v.getContext().startActivity(startSettings);
+      // and closing keyboard
+      mKeyboardActionListener.onKey(KeyCodes.CANCEL, null, 0, null, true);
+    } else {
+      throw new IllegalArgumentException(
+              "Failed to handle view id " + v.getId() + " in FrameKeyboardViewClickListener");
     }
   }
 
