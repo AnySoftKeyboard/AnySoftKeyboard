d13c60e9fac5dc03c7adb8608da630fc84bfd979
Aditya Wibisana <aditya.wibisana@gmail.com>
2025-05-03 14:03:53 +0700

    Add new (other) app module

---
+++ vhk/src/main/java/aditya/wibisana/vhk/MainActivity.kt
@@ -0,0 +1,20 @@
+package aditya.wibisana.vhk
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
