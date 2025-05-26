d2c13f4c61a3415578830307693fdeed891a0786
Menny Even Danan <menny@evendanan.net>
2025-05-26 17:11:27 -0400

    Correctly detect BOT accounts for contributions

--- js/contributors/contributors.test.ts
+++ js/contributors/contributors.test.ts
@@ -22,17 +22,17 @@ test.describe('Contributors', () => {
     { login: 'e_user', contributions: 1 },
   ];
 
-  test.describe('sorting correctly', () => {
+  test('sorting correctly', () => {
     const result = sortContributors(listOfContributors);
     assert.deepEqual(result, listOfContributorsSorted);
   });
 
-  test.describe('generate markdown list correctly', () => {
+  test('generate markdown list correctly', () => {
     const result = generateMarkdownList(listOfContributorsSorted);
     assert.equal(
       result,
       `1. [d_user](https://github.com/d_user) (1.1k)
-1. [anysoftkeyboard-bot](https://github.com/anysoftkeyboard-bot) (0.7k) 🤖
+1. [anysoftkeyboard-bot](https://github.com/anysoftkeyboard-bot) (0.73k) 🤖
 1. [a_user](https://github.com/a_user) (123)
 1. [b_user](https://github.com/b_user) (123)
 1. [c_user](https://github.com/c_user) (12)
@@ -41,7 +41,24 @@ test.describe('Contributors', () => {
     );
   });
 
-  test.describe('contructs markdown file', () => {
+  test('correctly detects bots', () => {
+    const withBots: Contributor[] = [
+      { login: 'd_user', contributions: 1123 },
+      { login: 'anysoftkeyboard-bot', contributions: 1733 },
+      { login: 'some[bot]', contributions: 123 },
+      { login: '[another[bot]]', contributions: 1 },
+    ];
+    const result = generateMarkdownList(withBots);
+    assert.equal(
+      result,
+      `1. [d_user](https://github.com/d_user) (1.1k)
+1. [anysoftkeyboard-bot](https://github.com/anysoftkeyboard-bot) (1.7k) 🤖
+1. [some](https://github.com/some) (0.12k) 🤖
+1. [another](https://github.com/another) (1) 🤖`,
+    );
+  });
+
+  test('contructs markdown file', () => {
     const result = constructContributorsContext('1.line1\n1.line2');
     assert.equal(
       result,
