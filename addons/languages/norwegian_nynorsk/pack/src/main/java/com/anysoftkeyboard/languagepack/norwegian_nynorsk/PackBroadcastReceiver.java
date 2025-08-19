cf6ec213c45b524cdf23738846027a57194a523b
eirik <eirik@garatun.com>
2025-08-19 18:50:27 +0200

    Added Norwegian (Nynorsk)

---
+++ addons/languages/norwegian_nynorsk/pack/src/main/java/com/anysoftkeyboard/languagepack/norwegian_nynorsk/PackBroadcastReceiver.java
@@ -0,0 +1,5 @@
+package com.anysoftkeyboard.languagepack.norwegian_nynorsk;
+
+import com.anysoftkeyboard.addon.PackBroadcastReceiverBase;
+
+public class PackBroadcastReceiver extends PackBroadcastReceiverBase {}
