f36fb0dd4c0228cf1716e4cbea69bc7fbd24cfd7
Luke Wilkinson <wilkinsonluke@proton.me>
2026-08-16 16:17:55 +0100

    Add Scottish Gaelic (Gàidhlig) language pack
    - Adds QWERTY layout with necessary accents shown on long press
    - Adds gaelic dictionary of 10000 words

---
+++ addons/languages/scottishgaelic/pack/src/main/java/com/anysoftkeyboard/languagepack/scottishgaelic/PackBroadcastReceiver.java
@@ -0,0 +1,5 @@
+package com.anysoftkeyboard.languagepack.scottishgaelic;
+
+import com.anysoftkeyboard.addon.PackBroadcastReceiverBase;
+
+public class PackBroadcastReceiver extends PackBroadcastReceiverBase {}
